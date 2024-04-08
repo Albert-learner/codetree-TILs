dd4a258d79c8fac93fc9de55bd6f337af367f1d6
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-08 14:20:35 +0900

    Title: 함수를 이용한 소수 판별, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240408/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -4,12 +4,15 @@ def is_prime_number(x):
     for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             return False
-
+    
     return True
 
-prime_sum = 0
-for num in range(A, B + 1):
-    if is_prime_number(num):
-        prime_sum += num
+if A == 1 and B == 1:
+    print(0)
+else:
+    prime_sum = 0
+    for num in range(A, B + 1):
+        if is_prime_number(num):
+            prime_sum += num
 
-print(prime_sum)
+    print(prime_sum)
