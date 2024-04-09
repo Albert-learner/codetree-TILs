c77dec93ea7e24b05b85ca729a4f7d3252857d25
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 00:22:58 +0900

    Title: 줄 세우기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/줄 세우기/line-up-students.py
+++ 240410/줄 세우기/line-up-students.py
@@ -5,6 +5,6 @@ students = []
 for student_idx, (height, weight) in enumerate(students_input, 1):
     students.append([height, weight, student_idx])
 
-students.sort(key = lambda x: (-x[0], -x[1], -x[2]))
+students.sort(key = lambda x: (-x[0], -x[1], x[2]))
 for student in students:
     print(*student)
