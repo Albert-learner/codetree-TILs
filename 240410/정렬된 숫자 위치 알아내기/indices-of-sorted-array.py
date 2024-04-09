98b2fef953d37d297ffc7ebc6c3cb0a254b2c865
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 01:03:51 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 304ms, Memory: 26MB, Status: Passed - CodeTree

--- 240410/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 240410/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -1,15 +1,16 @@
+from functools import cmp_to_key
+
 N = int(input())
 n_lst = list(map(int, input().split()))
 
 answer = []
-n_pairs = []
 sort_n_lst = sorted(n_lst)
-for n_idx, n in enumerate(sort_n_lst, 1):
-    n_pairs.append([n_idx, n])
+n_pairs = list(enumerate(sort_n_lst, start = 1))
 
 for n in n_lst:
     if n in list(zip(*n_pairs))[1]:
         n_idx = list(zip(*n_pairs))[1].index(n)
         answer.append(n_pairs[n_idx][0])
+        n_pairs.pop(n_idx)
 
 print(*answer)
