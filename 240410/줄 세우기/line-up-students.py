270dbdaa3f7fb4f880f9561fa892bcda73cfa49b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 00:20:09 +0900

    Title: 줄 세우기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/줄 세우기/line-up-students.py
@@ -0,0 +1,10 @@
+N = int(input())
+students_input = [list(map(int, input().split()))for _ in range(N)]
+
+students = []
+for student_idx, (height, weight) in enumerate(students_input, 1):
+    students.append([height, weight, student_idx])
+
+students.sort(key = lambda x: -x[-1])
+for student in students:
+    print(*student)
