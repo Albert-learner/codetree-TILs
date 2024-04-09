8bad6a99d5f8989315ee514fb41b6458c3f222d7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-09 10:43:02 +0900

    Title: 그 계절, 그 날, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240409/그 계절, 그 날/that-season-that-day.py
+++ 240409/그 계절, 그 날/that-season-that-day.py
@@ -6,99 +6,26 @@ def is_leap_year(y):
     
     return False
 
-def is_right_day(y, m, d):
-    if not is_leap_year(y):
-        if m in [12, 1, 2]:
-            if m in [12, 1]:
-                if d in range(1, 32):
-                    return "Winter"
-                else:
-                    return -1
-            else:
-                if d in range(1, 32):
-                    return "Winter"
-                else:
-                    return -1
-        elif m in [3, 4, 5]:
-            if m in [3, 5]:
-                if d in range(1, 32):
-                    return "Spring"
-                else:
-                    return -1
-            else:
-                if d in range(1, 31):
-                    return "Spring"
-                else:
-                    return -1
-        elif m in [6, 7, 8]:
-            if m in [7, 8]:
-                if d in range(1, 32):
-                    return "Summer"
-                else:
-                    return -1
-            else:
-                if d in range(1, 31):
-                    return "Summer"
-                else:
-                    return -1
-        elif m in [9, 10, 11]:
-            if m == 10:
-                if d in range(1, 32):
-                    return "Fall"
-                return -1
-            else:
-                if d in range(1, 31):
-                    return "Fall"
-                else:
-                    return -1
-        else:
-            return -1
+def is_wrong_day(y, m, d):
+    if m in [4, 6, 9, 11] and d > 30:
+        return True
+    elif m == 2 and is_leap_year(y) and d > 29:
+        return True
+    elif m == 2 and is_leap_year(y) == False and d > 28:
+        return True
     else:
-        if m != 2:
-            if m in [12, 1]:
-                if d in range(1, 32):
-                    return "Winter"
-                else:
-                    return -1
-            elif m in [3, 4, 5]:
-                if m in [3, 5]:
-                    if d in range(1, 32):
-                        return "Spring"
-                    else:
-                        return -1
-                else:
-                    if d in range(1, 31):
-                        return "Spring"
-                    else:
-                        return -1
-            elif m in [6, 7, 8]:
-                if m in [7, 8]:
-                    if d in range(1, 32):
-                        return "Summer"
-                    else:
-                        return -1
-                else:
-                    if d in range(1, 31):
-                        return "Summer"
-                    else:
-                        return -1
-            elif m in [9, 10, 11]:
-                if m == 10:
-                    if d in range(1, 32):
-                        return "Fall"
-                    else:
-                        return -1
-                else:
-                    if d in range(1, 31):
-                        return "Fall"
-                    else:
-                        return -1
-            else:
-                return -1
-        else:
-            if d in range(1, 30):
-                return "Winter"
-            else:
-                return False
-    
-print(is_right_day(Y, M, D))
+        return False
+
+if is_wrong_day(Y, M, D):
+    print(-1)
+else:
+    if M in [12, 1, 2]:
+        print("Winter")
+    elif M in [3, 4, 5]:
+        print("Spring")
+    elif M in [6, 7, 8]:
+        print("Summer")
+    elif M in [9, 10, 11]:
+        print("Fall")
+    else:
+        print(-1)
