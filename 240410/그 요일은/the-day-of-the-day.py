b3f67af1aaf029096dda458f5e530f383ab89549
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 11:30:51 +0900

    Title: 그 요일은, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/그 요일은/the-day-of-the-day.py
+++ 240410/그 요일은/the-day-of-the-day.py
@@ -1,16 +1,25 @@
 m1, d1, m2, d2 = map(int, input().split())
 find_day = input()
 
-month_days = [(1, 31), (2, 28), (3, 31), (4, 30), (5, 31), (6, 30), (7, 31), (8, 31), (9, 30), (10, 31), (11, 30), (12, 31)]
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-start_date = sum([days for month, days in month_days if month <= m1 - 1]) + d1
-end_date = sum([days for month, days in month_days if month <= m2 - 1]) + d2
+def get_num(f_day):
+    for day_idx, day in enumerate(days):
+        if day == f_day:
+            return day_idx
 
-days = {"Mon": 0, "Tue": 1, "Wed": 2, "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6}
-find_day_num = days[find_day]
+day_num = get_num(find_day)
+d1 += day_num
 
-quota, rest = divmod(end_date - start_date + 1, 7)
-if rest < find_day_num:
-    print(quota)
-else:
-    print(quota + 1)
+def total_days(m, d):
+    t_days = 0
+    for i in range(m):
+        t_days += months[i]
+
+    t_days += d
+
+    return t_days
+
+diff_days = total_days(m2, d2) - total_days(m1, d1)
+print(diff_days // 7 + 1)
