dc29eb1a5e3e4d65c4251b503b5c8bfb9462eb7c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 10:46:41 +0900

    Title: 그 요일은, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,18 @@
+m1, d1, m2, d2 = map(int, input().split())
+find_day = input()
+
+month_days = [(1, 31), (2, 28), (3, 31), (4, 30), (5, 31), (6, 30), (7, 31), (8, 31), (9, 30), (10, 31), (11, 30), (12, 31)]
+
+start_date = sum([days for month, days in month_days if month <= m1 - 1]) + d1
+end_date = sum([days for month, days in month_days if month <= m2 - 1]) + d2
+
+days = {"Mon": 0, "Tue": 1, "Wed": 2, "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6}
+find_day_num = days[find_day]
+
+cnts = 0
+day = 0
+while 7 * day + find_day_num <= end_date - start_date + 1:
+    cnts += 1
+    day += 1
+
+print(cnts)
