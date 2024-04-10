1a9a041bf8b76637e027f3bc18edb43c11f6ba80
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 09:37:10 +0900

    Title: DateTime to DateTime, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/DateTime to DateTime/datetime-to-datetime.py
+++ 240410/DateTime to DateTime/datetime-to-datetime.py
@@ -1,6 +1,6 @@
 A, B, C = map(int, input().split())
 
-if A >= 11 and B >= 11 and C >= 11:
+if A >= 11 or B >= 11 or C >= 11:
     day_diff, hour_diff, minute_diff = A - 11, B - 11, C - 11
     print(day_diff * 24 * 60 + hour_diff * 60 + minute_diff)
 else:
