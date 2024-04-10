c8a11cf96ecd965d531070c694ce41bb85a9516e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 14:23:09 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240410/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240410/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,10 +1,15 @@
 N = int(input())
 areas_lst = [list(map(int, input().split())) for _ in range(N)]
 
-max_len = max(list(zip(*areas_lst))[-1])
-checked = [0] * max_len
+start_lst = list(zip(*areas_lst))[0]
+offset = min(sorted(start_lst)) if min(sorted(start_lst)) < 0 else 0
+max_len = max(list(zip(*areas_lst))[1])
 
+checked = [0] * max_len
 for start, end in areas_lst:
+    start += offset
+    end += offset
+
     for idx in range(start, end):
         checked[idx] += 1
 
