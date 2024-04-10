da0169d4b72bb6bb1eca9335b1a29e5d0f9c05f9
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 09:45:10 +0900

    Title: DateTime to DateTime, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/DateTime to DateTime/datetime-to-datetime.py
+++ 240410/DateTime to DateTime/datetime-to-datetime.py
@@ -3,4 +3,7 @@ A, B, C = map(int, input().split())
 start_time = (11 - 1) * 24 * 60 + 11 * 60 + 11
 end_time = (A - 1) * 24 * 60 + B * 60 + C
 
-print(end_time - start_time)
+if end_time - start_time < 0:
+    print(-1)
+else:
+    print(end_time - start_time)
