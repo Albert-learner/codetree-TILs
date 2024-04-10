50f3573ef3bdfbd06995048aa1bb529fa11e911d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 23:57:54 +0900

    Title: 사각형의 총 넓이 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240410/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,34 +1,35 @@
+# 내 풀이(음수 좌표를 고려해야 함)
 N = int(input())
+rectangles = [list(map(lambda x: int(x) + 100, input().split())) for _ in range(N)]
 
-arr = [[0] * 201 for _ in range(201)]
-for _ in range(N):
-    x1, y1, x2, y2 = map(int, input().split())
-    x1 += 100
-    y1 += 100
-    x2 += 100
-    y2 += 100
+total_areas = 0
+overlap_areas = (min(list(zip(*rectangles))[2]) - max(list(zip(*rectangles))[0])) * (min(list(zip(*rectangles))[3]) - max(list(zip(*rectangles))[1]))
+for x1, y1, x2, y2 in rectangles:
+    area = (x2 - x1) * (y2 - y1)
+    total_areas += area
 
-    for row in range(x1, x2):
-        for col in range(y1, y2):
-            arr[row][col] = 1
+total_areas -= overlap_areas
+print(total_areas)
 
-cnts = 0
-for row in range(len(arr)):
-    for col in range(len(arr[0])):
-        if arr[row][col] == 1:
-            cnts += 1
-
-print(cnts)
-
-# # 내 풀이(음수 좌표를 고려해야 함)
+# # 남의 풀이
 # N = int(input())
-# rectangles = [list(map(int, input().split())) for _ in range(N)]
 
-# total_areas = 0
-# overlap_areas = (min(list(zip(*rectangles))[2]) - max(list(zip(*rectangles))[0])) * (min(list(zip(*rectangles))[3]) - max(list(zip(*rectangles))[1]))
-# for x1, y1, x2, y2 in rectangles:
-#     area = (x2 - x1) * (y2 - y1)
-#     total_areas += area
+# arr = [[0] * 201 for _ in range(201)]
+# for _ in range(N):
+#     x1, y1, x2, y2 = map(int, input().split())
+#     x1 += 100
+#     y1 += 100
+#     x2 += 100
+#     y2 += 100
 
-# total_areas -= overlap_areas
-# print(total_areas)
+#     for row in range(x1, x2):
+#         for col in range(y1, y2):
+#             arr[row][col] = 1
+
+# cnts = 0
+# for row in range(len(arr)):
+#     for col in range(len(arr[0])):
+#         if arr[row][col] == 1:
+#             cnts += 1
+
+# print(cnts)
