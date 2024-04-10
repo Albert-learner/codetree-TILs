8393b755ce21d125fe1a0054a3ef1378321bcbaa
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-10 10:28:30 +0900

    Title: 요일 맞추기, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240410/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,10 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+month_days = [(1, 31), (2, 28), (3, 31), (4, 30), (5, 31), (6, 30), (7, 31), (8, 31), (9, 30), (10, 31), (11, 30), (12, 31)]
+
+start_date = sum([days for month, days in month_days if month <= m1 - 1]) + d1
+end_date = sum([days for month, days in month_days if month <= m2 - 1]) + d2
+
+days = {0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
+
+print(days[(end_date - start_date) % 7])
