4e3c5f3e474bda6ddc0bf96bda62f93d8f664f42
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-11 14:42:56 +0900

    Title: 벌금은 누구에게, Time: 310ms, Memory: 26MB, Status: Passed - CodeTree

--- 240411/벌금은 누구에게/who-will-pay.py
+++ 240411/벌금은 누구에게/who-will-pay.py
@@ -1,14 +1,15 @@
 N, M, K = map(int, input().split())
-students = {n: 0 for n in range(1, N + 1)}
+students = [[n, 0] for n in range(1, N + 1)]
 penalties = [int(input()) for _ in range(M)]
 
-for penalty in penalties:
-    if penalty in students:
-        students[penalty] += 1
-
 answer = -1
-for student, penalty in students.items():
-    if penalty >= K:
-        answer = student
+for penalty in penalties:
+    if penalty in list(zip(*students))[0]:
+        idx = list(zip(*students))[0].index(penalty)
+        students[idx][1] += 1
+
+    if students[idx][1] >= K:
+        answer = students[idx][0]
+        break
 
 print(answer)
