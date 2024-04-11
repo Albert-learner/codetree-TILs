324bee005d21611b91c538f22c32a5b08e47a21f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-11 15:32:17 +0900

    Title: 선두를 지켜라, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240411/선두를 지켜라/keep-the-lead.py
+++ 240411/선두를 지켜라/keep-the-lead.py
@@ -2,25 +2,19 @@ N, M = map(int, input().split())
 a_moves = [list(map(int, input().split())) for _ in range(N)]
 b_moves = [list(map(int, input().split())) for _ in range(M)]
 
-
-a_pos, b_pos = [0], [0]
-for (a_vel, a_times), (b_vel, b_times) in zip(a_moves, b_moves):
-    a_dist, b_dist = a_vel * a_times, b_vel * b_times
-    a_pos.append(a_dist)
-    b_pos.append(b_dist)
-
+a_first, b_first = a_moves[0], b_moves[0]
 change_cnts = 0
-front_runner = 'A' if a_pos[1] > b_pos[1] else 'B' if a_pos[1] < b_pos[1] else ''
-for a_dist, b_dist in zip(a_pos[1:], b_pos[1:]):
-    if a_dist > b_dist and front_runner == 'A':
+front_runner = 'A' if a_first[0] > b_first[0] else 'B' if a_first[0] < b_first[0] else ''
+for (a_vel, _), (b_vel, _) in zip(a_moves[1:], b_moves[1:]):
+    if a_vel > b_vel and front_runner == 'A':
         continue
-    elif a_dist < b_dist and front_runner == 'A':
+    elif a_vel < b_vel and front_runner == 'A':
         change_cnts += 1
         front_runner = 'B'
-    elif a_dist > b_dist and front_runner == 'B':
+    elif a_vel > b_vel and front_runner == 'B':
         change_cnts += 1
         front_runner = 'A'
-    elif a_dist < b_dist and front_runner == 'B':
+    elif a_vel < b_vel and front_runner == 'B':
         continue
 
 print(change_cnts)
