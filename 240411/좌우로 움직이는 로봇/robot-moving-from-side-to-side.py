25a8c63ed6e63108937b85e8d2d7f07fffe6c290
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-11 20:03:11 +0900

    Title: 좌우로 움직이는 로봇, Time: 267ms, Memory: 62MB, Status: Wrong Answer - CodeTree

--- 240411/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240411/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,6 +1,6 @@
+# 내 풀이
 N, M = map(int, input().split())
-diff = N - M if N > M else M - N
-robot_A, robot_B = [], []
+robot_A, robot_B = [0], [0]
 
 cur_a, cur_b = 0, 0
 for _ in range(N):
@@ -8,11 +8,11 @@ for _ in range(N):
     size = int(size)
 
     if mv_dir == 'R':
-        for i in range(cur_a, cur_a + size):
+        for i in range(robot_A[-1] + 1, robot_A[-1] + size + 1):
             robot_A.append(i)
-        cur_a += size 
+        cur_a += size
     else:
-        for i in range(cur_a, cur_a - size, -1):
+        for i in range(robot_A[-1] - 1, robot_A[-1] - size - 1, -1):
             robot_A.append(i)
         cur_a -= size
 
@@ -21,14 +21,18 @@ for _ in range(M):
     size = int(size)
 
     if mv_dir == 'R':
-        for i in range(cur_b, cur_b + size):
+        for i in range(robot_B[-1] + 1, robot_B[-1] + size + 1):
             robot_B.append(i)
         cur_b += size
     else:
-        for i in range(cur_b, cur_b - size, -1):
+        for i in range(robot_B[-1] - 1, robot_B[-1] - size - 1, -1):
             robot_B.append(i)
         cur_b -= size
 
+robot_A = robot_A[1:]
+robot_B = robot_B[1:]
+
+diff = abs(len(robot_A) - len(robot_B))
 if len(robot_A) > len(robot_B):
     for _ in range(diff):
         robot_B.append(robot_B[-1])
@@ -36,11 +40,8 @@ else:
     for _ in range(diff):
         robot_A.append(robot_A[-1])
 
-print(robot_A)
-print(robot_B)
-
 answer = 0
-for (a_elem_idx, a_elem), (b_elem_idx, b_elem) in zip(enumerate(robot_A[1:]), enumerate(robot_B[1:])):
+for (a_elem_idx, a_elem), (b_elem_idx, b_elem) in zip(enumerate(robot_A), enumerate(robot_B)):
     if a_elem == b_elem and robot_A[a_elem_idx - 1] != robot_B[b_elem_idx - 1]:
         answer += 1
 
