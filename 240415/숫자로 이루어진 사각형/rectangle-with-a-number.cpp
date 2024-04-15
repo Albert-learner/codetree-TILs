617dcce4fedf85cfec0e7a0df543efa1f1372627
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-15 16:25:59 +0900

    Title: 숫자로 이루어진 사각형, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240415/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
+++ 240415/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 void print_rect_numbers(int n)
 {
-    for (int i = 1; i <= int(pow(n, 2)); i++)
-    {
-        if (i % n == 0)
-        {
-            if (i > 10)
-                cout << i - 9 << endl;
-            else
-                cout << i << endl;
-        }
-        else
-        {
-            if (i < 10)
-            {
-                cout << i << ' ';
-            }
-            else
-            {
-                cout << i - 9 << ' ';
-            }
-        }
-    }
+	int quota = (n * n) / 9;
+	int rest = (n * n) % 9;
+
+	vector<int> nums_lst;
+	for (int i = 1; i <= quota; ++i)
+	{
+		for (int j = 1; j <= 9; ++j)
+			nums_lst.push_back(j);
+	}
+
+	for (int i = 1; i <= rest; ++i)
+	{
+		nums_lst.push_back(i);
+	}
+
+	for (int i = 0; i < nums_lst.size(); ++i)
+	{
+		if ((i + 1) % n == 0)
+			cout << nums_lst[i] << endl;
+		else
+			cout << nums_lst[i] << ' ';
+	}
 }
 
-int main() {
-    int N;
-    cin >> N;
+int main()
+{
+	int N;
+	cin >> N;
 
-    print_rect_numbers(N);
-    return 0;
+	print_rect_numbers(N);
+	return 0;
 }
