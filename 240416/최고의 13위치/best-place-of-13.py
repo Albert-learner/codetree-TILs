5b9cf8ddf6834eed72d16b7e965819f8181ec843
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-16 11:12:10 +0900

    Title: 최고의 13위치, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/최고의 13위치/best-place-of-13.py
+++ 240416/최고의 13위치/best-place-of-13.py
@@ -4,6 +4,6 @@ board = [list(map(int, input().split())) for _ in range(N)]
 max_cnts = 0
 for i in range(N):
     for j in range(N - 2):
-        max_cnts = max(max_cnts, board[i][j] + board[i][j + 2])
+        max_cnts = max(max_cnts, board[i][j] + board[i][j + 1] + board[i][j + 2])
 
 print(max_cnts)
