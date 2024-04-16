70d57b66388bdee5a909ca75148917d0fb5b230f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-16 14:41:29 +0900

    Title: 이상한 진수 2, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/이상한 진수 2/awkward-digits-2.py
+++ 240416/이상한 진수 2/awkward-digits-2.py
@@ -1,14 +1,17 @@
 a_bin = input()
 
-zero_cnts = a_bin.count('0') if a_bin.count('0') > 1 else 1
-zero_cnts -= 1
+if a_bin == '1':
+    print(0)
+else:
+    zero_cnts = a_bin.count('0') 
+    zero_cnts -= 1
 
-bin_lst = [1 for _ in range(len(a_bin))]
-for i in range(zero_cnts):
-    bin_lst[-i - 1] = 0
+    bin_lst = [1 for _ in range(len(a_bin))]
+    for i in range(zero_cnts):
+        bin_lst[-i - 1] = 0
 
-num = 0
-for idx in range(len(bin_lst)):
-    num = num * 2 + bin_lst[idx]
+    num = 0
+    for idx in range(len(bin_lst)):
+        num = num * 2 + bin_lst[idx]
 
-print(num)
+    print(num)
