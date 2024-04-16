676dc1d88a9f401b0198bdf3a9c0e25dbd3aaffe
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-16 10:34:30 +0900

    Title: 모이자, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240416/모이자/gather.py
@@ -0,0 +1,12 @@
+N = int(input())
+n_lst = list(map(int, input().split()))
+
+moves = []
+for i in range(N):
+    diff_lst = [int(abs(j - i)) for j in range(N)]
+    move = 0
+    for n, diff in zip(n_lst, diff_lst):
+        move += n * diff
+    moves.append(move)
+
+print(min(moves))
