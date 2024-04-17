8b7ba971f5e26814bca63b007f9d84e5c15e9e42
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-17 22:11:57 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240417/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int compare(int a, int b, int c)
+{
+    return min(a, min(b, c));
+}
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    cout << compare(x, y, z);
+    return 0;
+}
