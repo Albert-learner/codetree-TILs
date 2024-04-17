b126e73b13f68c313f39155cb4f5c05a1977585f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-17 15:14:22 +0900

    Title: Carry 피하기 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240417/Carry 피하기 2/escaping-carry-2.py
+++ 240417/Carry 피하기 2/escaping-carry-2.py
@@ -1,36 +1,22 @@
 N = int(input())
 n_lst = [int(input()) for _ in range(N)]
 
-
 def find_carry(x, y, z):
-    x_lst, y_lst, z_lst = list(map(int, str(x))), list(map(int, str(y))), list(map(int, str(z)))
-    max_sum = 0
-
-    if len(x_lst) < 5:
-        for _ in range(5 - len(x_lst)):
-            x_lst.insert(0, 0)
-    if len(y_lst) < 5:
-        for _ in range(5 - len(y_lst)):
-            y_lst.insert(0, 0)
-    if len(z_lst) < 5:
-        for _ in range(5 - len(z_lst)):
-            z_lst.insert(0, 0)
-
-    for x_elem, y_elem, z_elem in zip(x_lst, y_lst, z_lst):
-        if x_elem + y_elem + z_elem > 9:
+    while x > 0 or y > 0 or z > 0:
+        if (x % 10 + y % 10 + z % 10) > 9:
             return False
-        else:
-            max_sum = max(max_sum, x_elem + y_elem + z_elem)
 
-    return max_sum
+        x //= 10
+        y //= 10
+        z //= 10
 
+    return True
 
-
-answer = 0
+answer = -1
 for i in range(N):
     for j in range(i + 1, N):
         for k in range(j + 1, N):
             if find_carry(n_lst[i], n_lst[j], n_lst[k]):
-                answer = n_lst[i] + n_lst[j] + n_lst[k]
+                answer = max(answer, n_lst[i] + n_lst[j] + n_lst[k])
 
 print(answer)
