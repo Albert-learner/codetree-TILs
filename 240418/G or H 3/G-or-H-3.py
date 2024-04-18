e056e6b31dbf42b1d90a69c1017531cc60ffbef5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-18 11:46:00 +0900

    Title: G or H 3, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240418/G or H 3/G-or-H-3.py
+++ 240418/G or H 3/G-or-H-3.py
@@ -13,7 +13,7 @@ for pos, alpha in people:
         line[pos] = 2
 
 max_sum = 0
-for i in range(1, 1000 - K + 1):
+for i in range(1, 1000 - K + 2):
     max_sum = max(max_sum, sum(line[i:i + K + 1]))
 
 print(max_sum)
