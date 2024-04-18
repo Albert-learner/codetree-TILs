cfc51c33216d15b94a47aa7cdc2652c4484efef4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-18 13:18:24 +0900

    Title: G or H 3, Time: 178ms, Memory: 28MB, Status: Passed - CodeTree

--- 240418/G or H 3/G-or-H-3.py
+++ 240418/G or H 3/G-or-H-3.py
@@ -5,24 +5,45 @@ for _ in range(N):
     pos = int(pos)
     people.append([pos, alpha])
 
-scale = 0
-K += 1
-line = [0] * 20001
+line = [0] * 10001
 for pos, alpha in people:
     if alpha == 'G':
         line[pos] = 1
     else:
         line[pos] = 2
+
+max_sum = 0
+for i in range(10000 - K + 1):
+    max_sum = max(max_sum, sum(line[i:i + K + 1]))
+
+print(max_sum)
+
+# 남의 풀이 참고
+# N, K = map(int, input().split())
+# people = []
+# for _ in range(N):
+#     pos, alpha = input().split()
+#     pos = int(pos)
+#     people.append([pos, alpha])
+
+# scale = 0
+# K += 1
+# line = [0] * 10001
+# for pos, alpha in people:
+#     if alpha == 'G':
+#         line[pos] = 1
+#     else:
+#         line[pos] = 2
     
-    scale = max(scale, pos)
+#     scale = max(scale, pos)
 
-result = 0
-for i in range(1, K + 1):
-    result += line[i]
+# result = 0
+# for i in range(1, K + 1):
+#     result += line[i]
 
-cur = result
-for i in range(2, scale + 1):
-    cur = cur - line[i - 1] + line[i + K - 1]
-    result = max(result, cur)
+# cur = result
+# for i in range(2, scale + 1):
+#     cur = cur - line[i - 1] + line[i + K - 1]
+#     result = max(result, cur)
 
-print(result)
+# print(result)
