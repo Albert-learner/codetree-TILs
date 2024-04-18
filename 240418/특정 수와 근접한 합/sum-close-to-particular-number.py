bd6d28e678351578d499447f340ff19011123902
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-18 10:06:10 +0900

    Title: 특정 수와 근접한 합, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240418/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -0,0 +1,19 @@
+N, S = map(int, input().split())
+n_lst = list(map(int, input().split()))
+
+def exclude_two_elements(lst, i, j):
+    result = [x for idx, x in enumerate(lst) if idx != i and idx != j]
+
+    return result
+
+def generate_all_exclusions(lst):
+    exclusions = []
+    for i in range(N):
+        for j in range(i + 1, N):
+            exclusion = exclude_two_elements(lst, i, j)
+            exclusions.append(sum(exclusion))
+
+    return exclusions
+
+diffs = sorted(generate_all_exclusions(n_lst), key = lambda x: abs(S - x))
+print(diffs[0] - S)
