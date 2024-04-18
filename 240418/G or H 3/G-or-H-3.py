3cfec9e5bd513dc77563d90ca1085523b7589da4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-18 11:34:44 +0900

    Title: G or H 3, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240418/G or H 3/G-or-H-3.py
+++ 240418/G or H 3/G-or-H-3.py
@@ -2,7 +2,7 @@ N, K = map(int, input().split())
 people = []
 for _ in range(N):
     pos, alpha = input().split()
-    pos = int(pos) - 1
+    pos = int(pos)
     people.append([pos, alpha])
 
 line = [0] * (max(list(zip(*people))[0]) + 1)
@@ -13,7 +13,7 @@ for pos, alpha in people:
         line[pos] = 2
 
 max_sum = 0
-for i in range(len(line) - K):
+for i in range(1, len(line) - K):
     max_sum = max(max_sum, sum(line[i:i + K + 1]))
 
 print(max_sum)
