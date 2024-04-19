022f962f41a5a669ec51e7e9a82b4442d1e3af74
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-19 09:11:51 +0900

    Title: 특정 구간의 원소 평균값, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

--- 240419/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240419/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -1,11 +1,22 @@
 N = int(input())
 n_lst = list(map(int, input().split()))
 
+exist = False
 answer = 0
 for i in range(N):
-    for j in range(i + 1, N):
-        areas = n_lst[i:j + 1]
-        if sum(areas) // len(areas) in areas:
+    for j in range(i, N):
+        sum_val = 0
+        for k in range(i, j + 1):
+            sum_val += n_lst[k]
+        
+
+        avg = sum_val / (j - i + 1)
+        for k in range(i, j + 1):
+            if n_lst[k] == avg:
+                exist = True
+
+        if exist:
             answer += 1
+            exist = False
 
 print(answer)
