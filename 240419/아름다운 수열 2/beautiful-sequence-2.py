39dfd8ae6e1d75f9edf75fbff10e8c1e4648b284
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-19 10:50:02 +0900

    Title: 아름다운 수열 2, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240419/아름다운 수열 2/beautiful-sequence-2.py
+++ 240419/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,25 +2,18 @@ N, M = map(int, input().split())
 a_seq = list(map(int, input().split()))
 b_seq = list(map(int, input().split()))
 
-def permute_iter(lst):
-    stack = [(0, [])]
-    result = []
-
-    while stack:
-        index, path = stack.pop()
-        if index == len(lst):
-            result.append(path)
-        else:
-            for i in range(len(lst)):
-                if lst[i] not in path:
-                    stack.append((index + 1, path + [lst[i]]))
-
-    return result
-
 answer = 0
-beautiful_seq = permute_iter(b_seq)
-for a_idx in range(N - M + 1):
-    if a_seq[a_idx:a_idx + M] in beautiful_seq:
+for i in range(N - M + 1):
+    check = True
+    find = b_seq[:]
+    for j in range(M):
+        if a_seq[i + j] in find:
+            find.pop(find.index(a_seq[i + j]))
+        else:
+            check = False
+            break
+
+    if check:
         answer += 1
 
 print(answer)
