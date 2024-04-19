e353498277caa7396c084219a84f60af44ce3d02
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-19 15:05:13 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240419/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240419/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -6,7 +6,7 @@ answer = N ** 3
 for i in range(1, N + 1):
     for j in range(1, N + 1):
         for k in range(1, N + 1):
-            if i - a > 2 and j - b > 2 and k - c > 2:
+            if i - a >= 3 and j - b >= 3 and k - c >= 3:
                 answer -= 1
 
 print(answer)
