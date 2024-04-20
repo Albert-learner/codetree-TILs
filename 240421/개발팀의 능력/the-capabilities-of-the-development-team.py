93222384b3001ab8cbe1788b5020032e1bf29e28
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-21 00:57:47 +0900

    Title: 개발팀의 능력, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240421/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240421/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -1,10 +1,33 @@
 developers = list(map(int, input().split()))
-developers.sort()
 
-if developers[0] + developers[2] == developers[1] + developers[3] or developers[1] + developers[3] == developers[4] or developers[4] == developers[0] + developers[2]:
+def get_diff(i, j, k, w):
+    sum1 = developers[i] + developers[j]
+    sum2 = developers[k] + developers[w]
+    sum3 = sum(developers) - (sum1 + sum2)
+
+    if sum1 == sum2 or sum2 == sum3 or sum3 == sum1:
+        return -1
+
+    result = abs(sum1 - sum2)
+    result = max(result, abs(sum1 - sum3))
+    result = max(result, abs(sum2 - sum3))
+
+    return result
+
+min_cst = 1000001
+flag = True
+for i in range(5):
+    for j in range(i + 1, 5):
+        for k in range(5):
+            for w in range(k + 1, 5):
+                if i == k or i == w or j == k or j == w:
+                    continue
+
+                if get_diff(i, j, k, w) != -1:
+                    min_cst = min(min_cst, get_diff(i, j, k, w))
+                    flag = False
+
+if flag:
     print(-1)
 else:
-    max_team = max(developers[0] + developers[2], developers[1] + developers[3], developers[4])
-    min_team = min(developers[0] + developers[2], developers[1] + developers[3], developers[4])
-
-    print(max_team - min_team)
+    print(min_cst)
