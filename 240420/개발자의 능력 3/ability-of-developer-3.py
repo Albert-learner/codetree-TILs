787cabc1dbf06aeafcb302b9f55b30c21e7d498c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-20 14:50:03 +0900

    Title: 개발자의 능력 3, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240420/개발자의 능력 3/ability-of-developer-3.py
+++ 240420/개발자의 능력 3/ability-of-developer-3.py
@@ -1,14 +1,15 @@
 abilities = list(map(int, input().split()))
 
-def get_diff(i, j):
-    sum1 = abilities[i] + abilities[j]
+def get_diff(i, j, k):
+    sum1 = abilities[i] + abilities[j] + abilities[k]
     sum2 = sum(abilities) - sum1
 
     return abs(sum1 - sum2)
 
 min_diff = 1000001
-for i in range(0, len(abilities)):
+for i in range(len(abilities)):
     for j in range(i + 1, len(abilities)):
-        min_diff = min(min_diff, get_diff(i, j))
+        for k in range(j + 1, len(abilities)):
+            min_diff = min(min_diff, get_diff(i, j, k))
 
 print(min_diff)
