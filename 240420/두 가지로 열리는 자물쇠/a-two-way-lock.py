18ad1b4f9701b5863fdfd14e580a46c0b1d53ff3
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-20 16:19:10 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 238ms, Memory: 26MB, Status: Passed - CodeTree

--- 240420/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240420/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -2,26 +2,29 @@ N = int(input())
 a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 
-a1_lst = [i if 1 <= i <= N else i - N if i > N else i + N for i in range(a1 - 2, a1 + 3)]
-b1_lst = [i if 1 <= i <= N else i - N if i > N else i + N for i in range(b1 - 2, b1 + 3)]
-c1_lst = [i if 1 <= i <= N else i - N if i > N else i + N for i in range(c1 - 2, c1 + 3)]
+cnts, overlaps = 0, 0
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        for k in range(1, N + 1):
+            if (abs(a1 - i) <= 2 or abs(a1 - i) >= N - 2) and \
+                (abs(b1 - j) <= 2 or abs(b1 - j) >= N -2) and \
+                (abs(c1 - k) <= 2 or abs(c1 - k) >= N -2):
+                cnts += 1
+            
+            if (abs(a2 - i) <= 2 or abs(a2 - i) >= N -2) and\
+                (abs(b2 - j) <= 2 or abs(b2 - j) >= N -2) and\
+                (abs(c2 - k) <= 2 or abs(c2 - k) >= N -2):
+                cnts += 1
+            
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        for k in range(1, N + 1):
+            if (abs(a1 - i) <= 2 or abs(a1 - i) >= N -2) and\
+                (abs(b1 - j) <= 2 or abs(b1 - j) >= N -2) and\
+                (abs(c1 - k) <= 2 or abs(c1 - k) >= N -2) and\
+                (abs(a2 - i) <= 2 or abs(a2 - i) >= N -2) and\
+                (abs(b2 - j) <= 2 or abs(b2 - j) >= N -2) and\
+                (abs(c2 - k) <= 2 or abs(c2 - k) >= N -2):
+                overlaps += 1
 
-a2_lst = [i if 1 <= i <= N else i - N if i > N else i + N for i in range(a2 - 2, a2 + 3)]
-b2_lst = [i if 1 <= i <= N else i - N if i > N else i + N for i in range(b2 - 2, b2 + 3)]
-c2_lst = [i if 1 <= i <= N else i - N if i > N else i + N for i in range(c2 - 2, c2 + 3)]
-
-first_comb = set()
-for a_one in a1_lst:
-    for b_one in b1_lst:
-        for c_one in c1_lst:
-            first_comb.add((a_one, b_one, c_one))
-first_comb = sorted(first_comb)
-
-second_comb = set()
-for a_two in a2_lst:
-    for b_two in b2_lst:
-        for c_two in c2_lst:
-            second_comb.add((a_two, b_two, c_two))
-second_comb = sorted(second_comb)
-
-print(len(set(first_comb + second_comb)))
+print(cnts - overlaps)
