608023f353e5df7f60e25401f3d0fabdc8170558
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-22 10:00:34 +0900

    Title: 스승의 은혜 2, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240422/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,17 @@
+N, B = map(int, input().split())
+prices = [int(input()) for _ in range(N)]
+
+max_cnts = 0
+for i in range(N):
+    cnts = 0
+    satisfy = B
+    for j in range(N):
+        price = prices[j] // 2 if j == i else prices[j]
+
+        if satisfy >= price:
+            satisfy -= price
+            cnts += 1
+
+    max_cnts = max(max_cnts, cnts)
+
+print(max_cnts)
