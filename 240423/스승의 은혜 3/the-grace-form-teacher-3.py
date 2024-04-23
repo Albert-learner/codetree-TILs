a3f7102ac51cb2cc8d4f8469a5f36bd4b1df0e87
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-23 19:42:49 +0900

    Title: 스승의 은혜 3, Time: 292ms, Memory: 25MB, Status: Passed - CodeTree

--- 240423/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240423/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,21 +1,24 @@
-N, B = map(int, input().split())
-gifts_info = [list(map(int, input().split())) for _ in range(N)]
+n, b = map(int, input().split())
+wishes = [tuple(map(int, input().split())) for _ in range(n)]
 
-max_students = 0
-for i in range(N):
-    gifts_info[i][0] //= 2
-    prices = [(gifts_info[j][0] + gifts_info[j][1]) for j in range(N)]
+ans = 0
+for i in range(n):
+    tmp = [list(wishes[j]) for j in range(n)]
+
+    tmp[i][0] /= 2
+
+    prices = [(tmp[k][0] + tmp[k][1]) for k in range(n)]
     prices.sort()
 
-    students, cur_prices = 0, 0
+    student = 0
+    cnt = 0
 
-    for j in range(N):
-        if cur_prices + prices[j] > B:
+    for x in range(n):
+        if cnt + prices[x] > b:
             break
+        cnt += prices[x]
+        student += 1
 
-        cur_prices += prices[j]
-        students += 1
+    ans = max(ans, student)
 
-    max_students = max(max_students, students)
-
-print(max_students)
+print(ans)
