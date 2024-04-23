0e5b57a2c090d7aff86c41a4d4079af722309b04
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-23 11:18:57 +0900

    Title: 이상한 폭탄 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240423/이상한 폭탄 2/strange-bomb-2.py
+++ 240423/이상한 폭탄 2/strange-bomb-2.py
@@ -2,10 +2,10 @@ N, K = map(int, input().split())
 nums = [int(input()) for _ in range(N)]
 
 answer = -1
-for i in range(N - K + 1):
+for i in range(N):
     bomb_num = -1
     for j in range(i + 1, i + K + 1):
-        if abs(i - j) > K or j > len(nums) - 1:
+        if j - i > K or j > len(nums) - 1:
             continue
 
         if nums[i] == nums[j]:
