59d29765db7e010f5114a16d53317e39aac5a94b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-23 19:47:40 +0900

    Title: 스승의 은혜 3, Time: 190ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240423/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240423/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,24 +1,48 @@
-n, b = map(int, input().split())
-wishes = [tuple(map(int, input().split())) for _ in range(n)]
+N, B = map(int, input().split())
+gifts_info = [list(map(int, input().split())) for _ in range(N)]
 
-ans = 0
-for i in range(n):
-    tmp = [list(wishes[j]) for j in range(n)]
-
-    tmp[i][0] /= 2
-
-    prices = [(tmp[k][0] + tmp[k][1]) for k in range(n)]
+max_students = 0
+for i in range(N):
+    gifts_info[i][0] //= 2
+    prices = [gifts_info[j][0] + gifts_info[j][1] for j in range(N)]
     prices.sort()
 
-    student = 0
-    cnt = 0
+    students, cur_prices = 0, 0
 
-    for x in range(n):
-        if cnt + prices[x] > b:
+    for j in range(N):
+        if cur_prices + prices[j] > B:
             break
-        cnt += prices[x]
-        student += 1
 
-    ans = max(ans, student)
+        cur_prices += prices[j]
+        students += 1
 
-print(ans)
+    max_students = max(max_students, students)
+
+print(max_students)
+
+
+# # 답안
+# n, b = map(int, input().split())
+# wishes = [tuple(map(int, input().split())) for _ in range(n)]
+
+# ans = 0
+# for i in range(n):
+#     tmp = [list(wishes[j]) for j in range(n)]
+
+#     tmp[i][0] /= 2
+
+#     prices = [(tmp[k][0] + tmp[k][1]) for k in range(n)]
+#     prices.sort()
+
+#     student = 0
+#     cnt = 0
+
+#     for x in range(n):
+#         if cnt + prices[x] > b:
+#             break
+#         cnt += prices[x]
+#         student += 1
+
+#     ans = max(ans, student)
+
+# print(ans)
