64c09dbee04064846e91f4e86444c27d52156a0d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-23 19:15:38 +0900

    Title: 스승의 은혜 3, Time: 188ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240423/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240423/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -3,7 +3,7 @@ gifts_info = [list(map(int, input().split())) for _ in range(N)]
 
 max_students = 0
 for i in range(N):
-    gifts_info[i][0] /= 2
+    gifts_info[i][0] //= 2
     prices = [(gifts_info[j][0] + gifts_info[j][1]) for j in range(N)]
     prices.sort()
 
