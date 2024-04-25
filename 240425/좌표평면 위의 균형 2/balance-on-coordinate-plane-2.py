c767a4a734e1af47331714f612c36ebe6ab844d6
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-25 16:04:09 +0900

    Title: 좌표평면 위의 균형 2, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240425/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 240425/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -1,20 +1,33 @@
+import sys
+
 N = int(input())
 points = [tuple(map(int, input().split())) for _ in range(N)]
-x_lst, y_lst = sorted(list(list(zip(*points))[0])), sorted(list(list(zip(*points))[1]))
+x_lst, y_lst = list(list(zip(*points))[0]), list(list(zip(*points))[1])
 
-mid_point = ((x_lst[0] + x_lst[-1]) // 2, (y_lst[0] + y_lst[-1]) // 2)
+min_cnts = sys.maxsize
+for i in range(101):
+    if i % 2 == 1:
+        continue
 
-four_areas = [[] for _ in range(4)]
+    for j in range(101):
+        if j % 2 == 1:
+            continue
 
-for x, y in points:
-    if x < mid_point[0] and y > mid_point[1]:
-        four_areas[0].append((x, y))
-    elif x < mid_point[0] and y < mid_point[1]:
-        four_areas[1].append((x, y))
-    elif x > mid_point[0] and y < mid_point[1]:
-        four_areas[2].append((x, y))
-    elif x > mid_point[0] and y > mid_point[1]:
-        four_areas[3].append((x, y))
+        n1, n2, n3, n4 = 0, 0, 0, 0
+        for k in range(N):
+            if x_lst[k] < i and y_lst[k] > j:
+                n1 += 1
+            elif x_lst[k] > i and y_lst[k] > j:
+                n2 += 1
+            elif x_lst[k] > i and y_lst[k] < j:
+                n3 += 1
+            else:
+                n4 += 1
 
-four_areas = sorted(four_areas, key = lambda x: len(x), reverse = True)
-print(len(four_areas[0]))
+        max_cnt = max(n1, n2)
+        max_cnt = max(max_cnt, n3)
+        max_cnt = max(max_cnt, n4)
+
+        min_cnts = min(min_cnts, max_cnt)
+
+print(min_cnts)
