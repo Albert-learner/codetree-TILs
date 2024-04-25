0f97ab0487a3a8080a0772df6b22c032fac0af43
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-25 18:13:42 +0900

    Title: 팀으로 하는 틱택토 2, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,33 @@
+board = [list(input()) for _ in range(3)]
+
+teams = set()
+for i in range(3):
+    chk = set()
+    for j in range(3):
+        chk.add(board[i][j])
+    
+    if len(chk) == 2:
+        teams.add(tuple(sorted(chk)))
+
+for j in range(3):
+    chk = set()
+    for i in range(3):
+        chk.add(board[j][i])
+    
+    if len(chk) == 2:
+        teams.add(tuple(sorted(chk)))
+
+chk = set()
+for i in range(3):
+    chk.add(board[i][i])
+
+if len(chk) == 2:
+    teams.add(tuple(sorted(chk)))
+
+chk = set()
+for i in range(3):
+    chk.add(board[i][2 - i])
+if len(chk) == 2:
+    teams.add(tuple(sorted(chk)))
+
+print(len(teams))
