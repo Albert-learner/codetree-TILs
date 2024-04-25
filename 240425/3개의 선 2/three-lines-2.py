de022ba9a0914028b1f4575d416bca83b970648b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-25 11:32:08 +0900

    Title: 3개의 선 2, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240425/3개의 선 2/three-lines-2.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+N = int(input())
+points = [tuple(map(int, input().split())) for _ in range(N)]
+x_lst, y_lst = list(list(zip(*points))[0]), list(list(zip(*points))[1])
+
+cntr_x = Counter(x_lst)
+cntr_y = Counter(y_lst)
+
+answer = 0
+lines = 0
+for x, x_cnt in cntr_x.items():
+    if x_cnt >= 2:
+        lines += 1
+
+for y, y_cnt in cntr_y.items():
+    if y_cnt >= 2:
+        lines += 1
+
+answer = 1 if lines >= 3 else 0
+print(answer)
