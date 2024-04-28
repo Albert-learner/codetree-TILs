237d241788b7bc689eaaaa1437cdbf3a1a9f84d5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-28 12:50:11 +0900

    Title: 이상한 폭탄 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240428/이상한 폭탄 3/strange-bomb-3.py
+++ 240428/이상한 폭탄 3/strange-bomb-3.py
@@ -1,13 +1,19 @@
 N, K = map(int, input().split())
 bombs = [int(input()) for _ in range(N)]
 
-max_explosions = 0
-for f_idx in range(N):
-    explosions = 0
+explosions = dict.fromkeys(set(bombs), 0)
+for f_idx in range(N - K + 1):
     for r_idx in range(f_idx + 1, N):
-        if abs(r_idx - f_idx) <= K and bombs[f_idx] == bombs[r_idx]:
-            explosions += 1
+        if abs(r_idx - f_idx) <= K and bombs[f_idx] == bombs[r_idx] and not explosions[bombs[f_idx]]:
+            explosions[bombs[f_idx]] = 1
+        elif abs(r_idx - f_idx) <= K and bombs[f_idx] == bombs[r_idx] and explosions[bombs[f_idx]] >= 1:
+            explosions[bombs[f_idx]] += 1
 
-    max_explosions = max(max_explosions, explosions)
+max_explosion = max(explosions.values())
+explosions_sort = dict(sorted(explosions.items(), key=lambda x: x[1], reverse=True))
+max_bombs = []
+for bomb, explosion_cnts in explosions_sort.items():
+    if explosion_cnts == max_explosion:
+        max_bombs.append(bomb)
 
-print(max_explosions)
+print(sorted(max_bombs, reverse=True)[0])
