fa81bfa1cd2d927138aa12abf4008be04767a1f5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-28 14:48:28 +0900

    Title: 이상한 폭탄 3, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240428/이상한 폭탄 3/strange-bomb-3.py
+++ 240428/이상한 폭탄 3/strange-bomb-3.py
@@ -1,19 +1,21 @@
 N, K = map(int, input().split())
 bombs = [int(input()) for _ in range(N)]
 
-explosions = dict.fromkeys(set(bombs), 0)
-for f_idx in range(N - K + 1):
-    for r_idx in range(f_idx + 1, N):
-        if abs(r_idx - f_idx) <= K and bombs[f_idx] == bombs[r_idx] and not explosions[bombs[f_idx]]:
-            explosions[bombs[f_idx]] = 1
-        elif abs(r_idx - f_idx) <= K and bombs[f_idx] == bombs[r_idx] and explosions[bombs[f_idx]] >= 1:
-            explosions[bombs[f_idx]] += 1
+last_idx = {}
+explosions = {}
 
-max_explosion = max(explosions.values())
-explosions_sort = dict(sorted(explosions.items(), key=lambda x: x[1], reverse=True))
-max_bombs = []
-for bomb, explosion_cnts in explosions_sort.items():
-    if explosion_cnts == max_explosion:
-        max_bombs.append(bomb)
+for i in range(N):
+    if bombs[i] in last_idx:
+        if i - last_idx[bombs[i]] <= K:
+            explosions[bombs[i]] = explosions.get(bombs[i], 0) + 1
 
-print(sorted(max_bombs, reverse=True)[0])
+    last_idx[bombs[i]] = i
+
+max_explosion = float("inf")
+for idx in explosions.keys():
+    if max_explosion == float("inf") or explosions[max_explosion] < explosions[idx]:
+        max_explosion = idx
+    elif explosions[max_explosion] == explosions[idx] and max_explosion < idx:
+        max_explosion = idx
+
+print(max_explosion if max_explosion != float("inf") else 0)
