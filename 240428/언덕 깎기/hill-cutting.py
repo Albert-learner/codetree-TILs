4bcab96630056e43fdff4fddf9b26b06512eacbb
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-28 16:14:15 +0900

    Title: 언덕 깎기, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240428/언덕 깎기/hill-cutting.py
@@ -0,0 +1,15 @@
+N = int(input())
+hills = [int(input()) for _ in range(N)]
+hills.sort()
+
+costs = 0
+half_hills = len(hills) // 2
+for i in range(half_hills):
+    for j in range(len(hills) - 1, 0, -1):
+        if hills[j] - hills[i] > 17:
+            costs += (hills[i + 1] - hills[i]) ** 2
+            costs += (hills[j] - hills[j - 1]) ** 2
+            hills[i] = hills[i + 1]
+            hills[j] = hills[j - 1]
+            
+print(costs)
