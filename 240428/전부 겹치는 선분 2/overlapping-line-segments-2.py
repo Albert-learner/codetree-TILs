752f4dc07fa2932e1a154ab64b2c5a92225ccb86
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-28 21:52:44 +0900

    Title: 전부 겹치는 선분 2, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240428/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240428/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,21 +1,17 @@
 N = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(N)]
 
-total_line = [0] * 100
+total_line = [0] * 101
 for x, y in lines:
     for i in range(x, y + 1):
         total_line[i] += 1
 
-answer = "No"
-for x, y in lines:
-    for i in range(x, y + 1):
-        total_line[i] -= 1
+cnts = 0
+for i in range(101):
+    if total_line[i] == N - 1:
+        cnts += 1
 
-    if N - 1 in total_line:
-        answer = "Yes"
-        break
-
-    for i in range(x, y + 1):
-        total_line[i] += 1
-
-print(answer)
+if cnts > 0:
+    print("Yes")
+else:
+    print("No")
