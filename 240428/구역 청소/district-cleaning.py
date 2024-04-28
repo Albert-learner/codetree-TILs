1ffc820c48cbdadcebde628b37d179698355d1f8
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-28 21:26:48 +0900

    Title: 구역 청소, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240428/구역 청소/district-cleaning.py
+++ 240428/구역 청소/district-cleaning.py
@@ -1,6 +1,9 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
+if a > c and b > d:
+    a, b, c, d = c, d, a, b
+
 if b >= c:
     a_areas = list(range(a, b + 1))
     b_areas = list(range(c, d + 1))
@@ -8,7 +11,6 @@ if b >= c:
     total_areas = sorted(list(set(a_areas + b_areas)))
     print(total_areas[-1] - total_areas[0])
 else:
-    a_areas = list(range(a + 1, b + 1))
-    b_areas = list(range(c + 1, d + 1))
-
+    a_areas = list(range(a, b))
+    b_areas = list(range(c, d))
     print(len(a_areas) + len(b_areas))
