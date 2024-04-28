a9ad3efbc9da29274975e9173f652f1845c4c864
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-28 21:12:41 +0900

    Title: 두 직사각형, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240428/두 직사각형/two-rectangles.py
+++ 240428/두 직사각형/two-rectangles.py
@@ -1,10 +1,24 @@
 x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
-first_rectangle = [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]
-second_rectangle = [(a1, b1), (a1, b2), (a2, b1), (a2, b2)]
+board = [[0] * 101 for _ in range(101)]
 
-if (a1 in range(x1, x2 + 1) and b1 in range(y1, y2 + 1)) or (a1 in range(x1, x2 + 1) and b2 in range(y1, y2 + 1)) or (a2 in range(x1, x2 + 1) and b1 in range(y1, y2 + 1)) or (a2 in range(x1, x2 + 1) and b2 in range(y1, y2 + 1)):
+for i in range(x1, x2 + 1):
+    for j in range(y1, y2 + 1):
+        board[i][j] += 1
+
+for i in range(a1, a2 + 1):
+    for j in range(b1, b2 + 1):
+        board[i][j] += 1
+
+confirm = False
+for row in range(len(board)):
+    for col in range(len(board[0])):
+        if board[row][col] >= 2:
+            confirm = True
+            break
+
+if confirm:
     print("overlapping")
 else:
     print("nonoverlapping")
