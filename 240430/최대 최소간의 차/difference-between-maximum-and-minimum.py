aeb255dd652bfeeba5760ed34fdcb1fd290e272d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-04-30 14:29:38 +0900

    Title: 최대 최소간의 차, Time: 270ms, Memory: 26MB, Status: Passed - CodeTree

--- 240430/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240430/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -33,3 +33,38 @@ while True:
             cst += 1
 
 print(cst)
+
+# # 모범답안
+# import sys
+
+# INT_MAX = sys.maxsize
+# MAX_K = 10000
+
+# # 변수 선언 및 입력:
+# n, k = tuple(map(int, input().split()))
+# arr = list(map(int, input().split()))
+
+
+# def get_cost(low, high):
+#     cost = 0
+#     # 각 수에 대해 low ~ high 사이로 바꾸는데 드는 cost를 계산해 줍니다.
+#     for elem in arr:
+#         # low보다 작은 경우 low로 만들어 주는 게 최소 cost입니다.
+#         if elem < low: 
+#             cost += low - elem
+#         # high보다 큰 경우 high로 만들어 주는게 최소 cost입니다.
+#         if elem > high: 
+#             cost += elem - high
+#         # 그 외의 경우 이미 구간 안에 있기 때문에 cost가 필요하지 않습니다.
+
+#     return cost
+
+   
+# ans = INT_MAX
+# # 모든 구간 쌍 (num, num + k)를 잡아보며
+# # 그 구간으로 만들기 위한 비용을 계산하여
+# # 그 중 최솟값을 계산합니다.
+# for num in range(1, MAX_K + 1):
+#     ans = min(ans, get_cost(num, num + k))
+
+# print(ans)
