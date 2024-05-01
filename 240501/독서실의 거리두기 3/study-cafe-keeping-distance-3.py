36c1c49cb7a51ac1a9385f4cc4624f45f989870e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-01 20:14:09 +0900

    Title: 독서실의 거리두기 3, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240501/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,15 @@
+N = int(input())
+seats = list(map(int, input()))
+
+ones = [idx for idx, cst in enumerate(seats) if cst == 1]
+
+max_dst, start, end = 0, 0, 0
+for idx, cst in enumerate(ones):
+    if idx == len(ones) - 1:
+        break
+
+    if ones[idx + 1] - ones[idx] > max_dst:
+        max_dst = ones[idx + 1] - ones[idx]
+        start, end = ones[idx], ones[idx + 1]
+
+print((start + end) // 2)
