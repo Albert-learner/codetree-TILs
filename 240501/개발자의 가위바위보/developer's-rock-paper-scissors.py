cff40dd5ebd63fcd5473a4b41e61fd05290c2c87
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-01 21:35:23 +0900

    Title: 개발자의 가위바위보, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/개발자의 가위바위보/developer's-rock-paper-scissors.py
+++ 240501/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -1,34 +1,61 @@
-# 변수 선언 및 입력
-n = int(input())
-arr = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+N = int(input())
+games = [list(map(int, input().split())) for _ in range(N)]
 
-max_win = 0
-
-# Case 1. 1이 2를 이기고, 2가 3을 이기고 3이 1을 이기는 경우
-win = 0
-for a, b in arr:
+max_wins = 0
+wins = 0
+for a, b in games:
     if a == 1 and b == 2:
-        win += 1
+        wins += 1
     elif a == 2 and b == 3:
-        win += 1
+        wins += 1
     elif a == 3 and b == 1:
-        win += 1
+        wins += 1
 
-max_win = max(max_win, win)
-
-# Case 2. 1이 3을 이기고, 3이 2를 이기고 2가 1을 이기는 경우
-win = 0
-for a, b in arr:
+max_wins = max(max_wins, wins)
+wins = 0
+for a, b in games:
     if a == 1 and b == 3:
-        win += 1
+        wins += 1
     elif a == 3 and b == 2:
-        win += 1
+        wins += 1
     elif a == 2 and b == 1:
-        win += 1
+        wins += 1
 
-max_win = max(max_win, win)
+max_wins = max(max_wins, wins)
+print(max_wins)
 
-print(max_win)
+# # 모범답안
+# # 변수 선언 및 입력
+# n = int(input())
+# arr = [
+#     list(map(int, input().split()))
+#     for _ in range(n)
+# ]
+
+# max_win = 0
+
+# # Case 1. 1이 2를 이기고, 2가 3을 이기고 3이 1을 이기는 경우
+# win = 0
+# for a, b in arr:
+#     if a == 1 and b == 2:
+#         win += 1
+#     elif a == 2 and b == 3:
+#         win += 1
+#     elif a == 3 and b == 1:
+#         win += 1
+
+# max_win = max(max_win, win)
+
+# # Case 2. 1이 3을 이기고, 3이 2를 이기고 2가 1을 이기는 경우
+# win = 0
+# for a, b in arr:
+#     if a == 1 and b == 3:
+#         win += 1
+#     elif a == 3 and b == 2:
+#         win += 1
+#     elif a == 2 and b == 1:
+#         win += 1
+
+# max_win = max(max_win, win)
+
+# print(max_win)
