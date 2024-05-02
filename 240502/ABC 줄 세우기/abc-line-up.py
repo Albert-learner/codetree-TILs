70bc10f992af1cdd1a0a67242e9028f8094e191c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-02 13:23:18 +0900

    Title: ABC 줄 세우기, Time: 115ms, Memory: 28MB, Status: Passed - CodeTree

--- 240502/ABC 줄 세우기/abc-line-up.py
+++ 240502/ABC 줄 세우기/abc-line-up.py
@@ -13,3 +13,26 @@ def bubble_sort(lst):
 
 total_cnts, sort_line = bubble_sort(line)
 print(total_cnts)
+
+# # 모범답안
+# # 변수 선언 및 입력:
+# n = int(input())
+# arr = [0] + list(input().split())
+# ans = 0
+
+# # 'A' 부터 알파벳 순서대로 찾아 가장 앞으로 이동합니다.
+# for i in range(1, n + 1):
+#     x = chr(ord('A') + i - 1)
+
+#     # i번째 알파벳을 찾아 idx에 저장합니다.
+#     idx = 0
+#     for j in range(1, n + 1):
+#         if arr[j] == x:
+#             idx = j
+    
+#     # idx번째 알파벳을 i번째 위치까지 swap합니다.
+#     for j in range(idx - 1, i - 1, -1):
+#         arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#         ans += 1
+
+# print(ans)
