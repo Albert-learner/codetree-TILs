fa80fc25ad9db7b66dca848a46989ce9d493c2c3
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-02 14:33:15 +0900

    Title: 순위 경쟁2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,43 @@
+N = int(input())
+changes = []
+for _ in range(N):
+    c, s = input().split()
+    ch_score = int(s)
+    changes.append([c, ch_score])
+
+a_score, b_score = 0, 0
+change_cnts = 0
+prev_winner, winner = '', ''
+for ch_idx, (player, ch_score) in enumerate(changes):
+    if player == 'A':
+        a_score += ch_score
+    elif player == 'B':
+        b_score += ch_score
+
+    if ch_idx != 0:
+        if player == 'A':
+            a_score += ch_score
+        elif player == 'B':
+            b_score += ch_score
+
+        if a_score > b_score:
+            winner = 'A'
+        elif a_score < b_score:
+            winner = 'B'
+        else:
+            change_cnts += 1
+
+        if prev_winner != winner:
+            change_cnts += 1
+            prev_winner, winner = winner, ''
+    else:
+        if player == 'A':
+            a_score += ch_score
+            prev_winner, winner = player, player
+        elif player == 'B':
+            b_score += ch_score
+            prev_winner, winner = player, player
+
+        change_cnts += 1
+
+print(change_cnts)
