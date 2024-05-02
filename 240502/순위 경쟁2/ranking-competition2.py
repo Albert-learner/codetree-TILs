1e09a3cd0188146cd02b223acc836ee6aa6027c5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-02 15:01:22 +0900

    Title: 순위 경쟁2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240502/순위 경쟁2/ranking-competition2.py
+++ 240502/순위 경쟁2/ranking-competition2.py
@@ -8,13 +8,15 @@ for _ in range(N):
     ch_score = int(s)
     idx += 1
 
+    if a_scores[idx] == a_scores[idx - 1] and b_scores[idx] == b_scores[idx - 1]:
+        a_scores[idx], b_scores[idx] = a_scores[idx - 1], b_scores[idx - 1]
+
     if c == 'A':
         a_scores[idx] = a_scores[idx - 1] + ch_score
     elif c == 'B':
         b_scores[idx] = b_scores[idx - 1] + ch_score
 
-    if a_scores[idx] == a_scores[idx - 1] and b_scores[idx] == b_scores[idx - 1]:
-        a_scores[idx], b_scores[idx] = a_scores[idx - 1], b_scores[idx - 1]
+    
 
 for i in range(N):
     if (a_scores[i] == b_scores[i] and a_scores[i + 1] > b_scores[i + 1]) or \
