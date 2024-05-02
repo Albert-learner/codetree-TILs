54146b68fa020ebe17c8cdce3f4913b711f3645d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-02 16:01:02 +0900

    Title: 두 번째로 작은 수의 위치, Time: 184ms, Memory: 24MB, Status: Passed - CodeTree

--- 240502/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240502/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -2,9 +2,13 @@ N = int(input())
 n_lst = list(map(int, input().split()))
 
 uniq_n_lst = sorted(list(set(n_lst[:])))
-twos = n_lst.count(uniq_n_lst[1])
 
-if twos > 1:
+if len(uniq_n_lst) == 1:
     print(-1)
-elif twos == 1:
-    print(n_lst.index(uniq_n_lst[1]) + 1)
+else:
+    twos = n_lst.count(uniq_n_lst[1])
+
+    if twos > 1:
+        print(-1)
+    elif twos == 1:
+        print(n_lst.index(uniq_n_lst[1]) + 1)
