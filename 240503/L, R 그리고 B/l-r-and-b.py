e648200fb57a33712041686f311fe4f073c49377
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-03 11:27:32 +0900

    Title: L, R 그리고 B, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240503/L, R 그리고 B/l-r-and-b.py
+++ 240503/L, R 그리고 B/l-r-and-b.py
@@ -1,32 +1,53 @@
-grids = [list(input()) for _ in range(10)]
+from collections import deque
 
-start_x, start_y, target_x, target_y, disturb_x, disturb_y = 0, 0, 0, 0, 0, 0
-for row in range(10):
-    for col in range(10):
-        if grids[row][col] == 'L':
-            start_x, start_y = row, col
-        elif grids[row][col] == 'B':
-            target_x, target_y = row, col
-        elif grids[row][col] == 'R':
-            disturb_x, disturb_y = row, col
+class Push:
+    def __init__(self, row, col):
+        self.r = row
+        self.c = col
 
-routes_pos = []
-min_dists = 0
-sx, sy, tx, ty = start_x, start_y, target_x, target_y
-if tx < sx and ty < sy:
-    sx, sy, tx, ty = tx, ty, sx, sy
+def index_ok(r, c):
+    return 0 <= r < 10 and 0 <= c < 10
 
-while sx != tx and sy != ty:
-    sy += 1
-    routes_pos.append((sx, sy))
-    sx += 1
-    routes_pos.append((sx, sy))
+def bfs():
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
 
-if (disturb_x, disturb_y) in routes_pos:
-    min_dists += 1
+    while q:
+        p = q.popleft()
 
-if (start_x == target_x and start_x == disturb_x) or (start_y == target_y and start_y == disturb_y):
-    min_dists += 4
+        for i in range(4):
+            next_r = p.r + dr[i]
+            next_c = p.c + dc[i]
 
-min_dists = abs(target_x - start_x) + abs(target_y - start_y) - 1
-print(min_dists)
+            if index_ok(next_r, next_c) and arr[next_r][next_c] != 'R' and dp[next_r][next_c] == 0:
+                dp[next_r][next_c] = dp[p.r][p.c] + 1
+                p2 = Push(next_r, next_c)
+                q.append(p2)
+
+if __name__ == "__main__":
+    arr = [['' for _ in range(10)] for _ in range(10)]
+    dp = [[0 for _ in range(10)] for _ in range(10)]
+    q = deque()
+
+    str_i = -1
+    str_j = -1
+    las_i = -1
+    las_j = -1
+
+    for i in range(10):
+        row = input()
+        for j in range(10):
+            arr[i][j] = row[j]
+            if arr[i][j] == 'B' or arr[i][j] == 'L':
+                if str_i != -1:
+                    las_i = i
+                    las_j = j
+                else:
+                    str_i = i
+                    str_j = j
+
+    p = Push(str_i, str_j)
+    q.append(p)
+    bfs()
+
+    print(dp[las_i][las_j] - 1)
