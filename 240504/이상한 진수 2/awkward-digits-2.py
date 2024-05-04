f935a2c08f8de89b2e3e039466d557ebf323ca0f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-04 20:23:55 +0900

    Title: 이상한 진수 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240504/이상한 진수 2/awkward-digits-2.py
+++ 240504/이상한 진수 2/awkward-digits-2.py
@@ -1,16 +1,18 @@
 ch_bin_num = input()
 ch_bin_num_lst = list(ch_bin_num)
 
-if ch_bin_num == '1':
-    print(0)
+max_cst = 0
+if '0' not in ch_bin_num_lst:
+    ch_bin_num_lst[-1] = '0'
+
+    
+
 else:
     for b_n_idx, bin_n in enumerate(ch_bin_num_lst):
         if bin_n == '0':
             ch_bin_num_lst[b_n_idx] = '1'
             break
 
-    max_cst = 0
-    for base_num, bin_n in enumerate(ch_bin_num_lst[::-1]):
+for base_num, bin_n in enumerate(ch_bin_num_lst[::-1]):
         max_cst += int(bin_n) * (2 ** base_num)
-
-    print(max_cst)
+print(max_cst)
