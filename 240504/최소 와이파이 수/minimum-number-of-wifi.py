3cf5a9f15146e73bae654366b393edfb4f9af0c2
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-04 18:57:03 +0900

    Title: 최소 와이파이 수, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240504/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240504/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,36 +1,51 @@
+# 모범답안
 N, M = map(int, input().split())
 people = list(map(int, input().split()))
 
-def new_idx(x, n_idx):
-    for i in range(N):
-        if i >= n_idx and people[i] == 1:
-            return i
-        elif i == N - 1:
-            return n_idx
-        else:
-            continue
-
-def is_range(x):
-    if x >= N:
-        return False
-    elif people[x] == 0:
-        return True
+cnts, i = 0, 0
+while i < N:
+    if people[i] == 1:
+        cnts += 1
+        i += 2 * M + 1
     else:
-        return False
+        i += 1
 
-cnts = 0
-if 1 not in people:
-    print(cnts)
-else:
-    idx = 0
-    while idx < N:
-        if people[idx: idx + 2 * M + 1].count(1) != 0:
-            cnts += 1
+print(cnts)
 
-        if people[idx: idx + 2 * M + 1].count(1) == 0 or is_range(idx + 2 * M + 1):
-            now_idx = idx + 2 * M + 1
-            idx = new_idx(people, now_idx)
-        else:
-            idx += 2 * M + 1
+# # 풀이
+# N, M = map(int, input().split())
+# people = list(map(int, input().split()))
 
-    print(cnts)
+# def new_idx(x, n_idx):
+#     for i in range(N):
+#         if i >= n_idx and people[i] == 1:
+#             return i
+#         elif i == N - 1:
+#             return n_idx
+#         else:
+#             continue
+
+# def is_range(x):
+#     if x >= N:
+#         return False
+#     elif people[x] == 0:
+#         return True
+#     else:
+#         return False
+
+# cnts = 0
+# if 1 not in people:
+#     print(cnts)
+# else:
+#     idx = 0
+#     while idx < N:
+#         if people[idx: idx + 2 * M + 1].count(1) != 0:
+#             cnts += 1
+
+#         if people[idx: idx + 2 * M + 1].count(1) == 0 or is_range(idx + 2 * M + 1):
+#             now_idx = idx + 2 * M + 1
+#             idx = new_idx(people, now_idx)
+#         else:
+#             idx += 2 * M + 1
+
+#     print(cnts)
