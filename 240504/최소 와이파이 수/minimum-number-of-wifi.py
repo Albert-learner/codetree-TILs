9c0583bd847648cb4d3fee538b0fe08e84925f9a
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-04 18:54:58 +0900

    Title: 최소 와이파이 수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240504/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240504/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,9 +1,36 @@
 N, M = map(int, input().split())
 people = list(map(int, input().split()))
 
-if M == 0:
-    print(N - 1)
-elif 0 < M and M * 2 + 1 < N:
-    print(N // (M * 2 + 1))
-elif N < M * 2 + 1:
-    print((M * 2 + 1) // N)
+def new_idx(x, n_idx):
+    for i in range(N):
+        if i >= n_idx and people[i] == 1:
+            return i
+        elif i == N - 1:
+            return n_idx
+        else:
+            continue
+
+def is_range(x):
+    if x >= N:
+        return False
+    elif people[x] == 0:
+        return True
+    else:
+        return False
+
+cnts = 0
+if 1 not in people:
+    print(cnts)
+else:
+    idx = 0
+    while idx < N:
+        if people[idx: idx + 2 * M + 1].count(1) != 0:
+            cnts += 1
+
+        if people[idx: idx + 2 * M + 1].count(1) == 0 or is_range(idx + 2 * M + 1):
+            now_idx = idx + 2 * M + 1
+            idx = new_idx(people, now_idx)
+        else:
+            idx += 2 * M + 1
+
+print(cnts)
