c1f00b06e28c2d25e375a590b961ae03fbf382a7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-04 18:39:52 +0900

    Title: 최소 와이파이 수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240504/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240504/최소 와이파이 수/minimum-number-of-wifi.py
@@ -3,5 +3,7 @@ people = list(map(int, input().split()))
 
 if M == 0:
     print(N - 1)
-else:
+elif 0 < M and M * 2 + 1 < N:
     print(N // (M * 2 + 1))
+elif N < M * 2 + 1:
+    print((M * 2 + 1) // N)
