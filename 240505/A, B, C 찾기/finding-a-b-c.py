1a8ec51051aa140ad9e737963f78fbdfd46b9a49
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-05 14:23:52 +0900

    Title: A, B, C 찾기, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/A, B, C 찾기/finding-a-b-c.py
+++ 240505/A, B, C 찾기/finding-a-b-c.py
@@ -10,3 +10,22 @@ for i in range(len(nums)):
                 break
 
 print(a, b, c)
+
+# # 모범답안
+# n = 7
+
+# # 변수 선언 및 입력
+# arr = list(map(int, input().split()))
+
+# # 오름차순으로 정렬을 진행합니다.
+# arr.sort()
+
+# # 오름차순으로 정렬했을 때,
+# # 가장 작은 숫자는 A,
+# # 두 번째로 작은 숫자는 항상 B가 됩니다.
+# a , b = arr[0], arr[1]
+# # 또한, 가장 큰 숫자는 항상 A + B + C가 되므로
+# # C 는 끝 숫자 - A - B가 됩니다
+# c = arr[-1] - a - b
+
+# print(a, b, c)
