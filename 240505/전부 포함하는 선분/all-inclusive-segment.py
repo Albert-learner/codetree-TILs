093411af52f77cf18091a083820c0ce00b9caacb
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-05 15:57:55 +0900

    Title: 전부 포함하는 선분, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240505/전부 포함하는 선분/all-inclusive-segment.py
+++ 240505/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,10 +1,12 @@
+import sys
+
 N = int(input())
-lines = [tuple(map(int, input().split())) for _ in range(N)]
-lines.sort(key = lambda x: abs(x[0] - x[1]))
+lines = [list(map(int, input().split())) for _ in range(N)]
 
-p_lines = lines[:-1]
-min_x1, max_x1 = min(list(zip(*p_lines))[0]), max(list(zip(*p_lines))[0])
-min_x2, max_x2 = min(list(zip(*p_lines))[1]), max(list(zip(*p_lines))[1])
+min_line = sys.maxsize
+for i in range(N):
+    excludeds = sorted(lines[:i] + lines[i + 1:])
+    start, end = min(list(zip(*excludeds))[0]), max(list(zip(*excludeds))[1])
+    min_line = min(min_line, end - start)
 
-min_start, max_end = min(min_x1, min_x2), max(max_x1, max_x2)
-print(abs(max_end - min_start))
+print(min_line)
