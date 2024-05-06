244c6fbeaa5a261f83d63c9127aff96ce1295294
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-06 14:42:53 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 240506/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240506/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,25 +1,9 @@
 a_str = input()
-n = len(a_str)
 
-cnts, result = 0, 0
-for i in range(n - 1, 0, -1):
-    if a_str[i] == ')' and a_str[i - 1] == ')':
-        cnts += 1
-    elif a_str[i] == '(' and a_str[i - 1] == '(':
-        result += cnts
+answer = 0
+for i in range(len(a_str) - 1):
+    for j in range(i + 1, len(a_str) - 1):
+        if a_str[i] == '(' and a_str[i + 1] == '(' and a_str[j] == ')' and a_str[j + 1] == ')':
+            answer += 1
 
-print(result)
-
-# # 모범답안
-# # 변수 선언 및 입력
-# string = input()
-# n = len(string)
-
-# # 모든 쌍을 다 잡아봅니다.
-# cnt = 0
-# for i in range(n - 1):
-#     for j in range(i + 1, n - 1):
-#         if string[i] == '(' and string[i + 1] == '(' and string[j] == ')' and string[j + 1] == ')':
-#             cnt += 1
-            
-# print(cnt)
+print(answer)
