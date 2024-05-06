5d339a4101ef2695aa7daa5f1878037144a39455
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-06 14:17:25 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240506/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240506/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -8,14 +8,10 @@ for chr_idx, a_chr in enumerate(a_str):
         rear_idxs.append(chr_idx)
 
 answer = 0
-front_cases, rear_cases = 0, 0
 for i in range(len(front_idxs) - 1):
     if abs(front_idxs[i + 1] - front_idxs[i]) == 1:
-        front_cases += 1
+        for j in range(i + 1, len(rear_idxs) - 1):
+            if abs(rear_idxs[j + 1] - rear_idxs[j]) == 1:
+                answer += 1
 
-for i in range(len(rear_idxs) - 1):
-    if abs(rear_idxs[i + 1] - rear_idxs[i]) == 1:
-        rear_cases += 1
-
-answer = front_cases * rear_cases
 print(answer)
