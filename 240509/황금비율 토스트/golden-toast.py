237bd5c948f5d761bd5d875aab987e3aed62ccd9
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-09 17:59:38 +0900

    Title: 황금비율 토스트, Time: 205ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 240509/황금비율 토스트/golden-toast.py
+++ 240509/황금비율 토스트/golden-toast.py
@@ -5,17 +5,18 @@ codes = [input().split() for _ in range(M)]
 
 itr = N
 for code in codes:
-    if code[0] == 'L':
-        if itr != 0:
-            itr -= 1
-    elif code[0] == 'R':
-        if itr != N:
+    if itr in range(N + 1):
+        if code[0] == 'L':
+            if itr != 0:
+                itr -= 1
+        elif code[0] == 'R':
+            if itr != N:
+                itr += 1
+        elif code[0] == 'D':
+            if itr != 0 or itr != N:
+                word_lst.pop(itr)
+        elif code[0] == 'P':
+            word_lst.insert(itr, code[1])
             itr += 1
-    elif code[0] == 'D':
-        if itr != 0 or itr != N:
-            word_lst.pop(itr)
-    elif code[0] == 'P':
-        word_lst.insert(itr, code[1])
-        itr += 1
 
 print("".join(word_lst))
