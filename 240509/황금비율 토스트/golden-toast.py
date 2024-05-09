b1a8cd9709e2fc6f351f94b73bf75111b107cde6
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-09 11:29:18 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240509/황금비율 토스트/golden-toast.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+word = input()
+word_lst = list(word)
+codes = [input().split() for _ in range(M)]
+
+itr = N
+for code in codes:
+    if code[0] == 'L':
+        if itr != 0:
+            itr -= 1
+    elif code[0] == 'R':
+        if itr != N:
+            itr += 1
+    elif code[0] == 'D':
+        if itr == 0 or itr == N:
+            word_lst.pop(itr)
+        else:
+            word_lst.pop(itr + 1)
+    elif code[0] == 'P':
+        word_lst.insert(itr, code[1])
+
+print("".join(word_lst))
