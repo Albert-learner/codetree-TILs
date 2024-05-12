c82d8b1e66f01949ee175efcd5f0c2545827b748
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-12 12:17:01 +0900

    Title: 힙 정렬 구현, Time: 352ms, Memory: 34MB, Status: Passed - CodeTree

--- 240512/힙 정렬 구현/implement-heap-sort.py
+++ 240512/힙 정렬 구현/implement-heap-sort.py
@@ -1,24 +1,33 @@
+import heapq as hq
+
 N = int(input())
 n_lst = list(map(int, input().split()))
 
-def heapify(lst, lst_len):
-    last_parent = lst_len // 2 - 1
-    for cur in range(last_parent, -1, -1):
-        while cur <= last_parent:
-            child = cur * 2 + 1
-            sibling = child + 1
-            if sibling < lst_len and lst[child] < lst[sibling]:
-                child = sibling
-            if lst[cur] < lst[child]:
-                lst[cur], lst[child] = lst[child], lst[cur]
-                cur = child
-            else:
-                break
-    
-def heap_sort(lst):
-    for lst_len in range(len(lst), 1, -1):
-        heapify(lst, lst_len)
-        lst[lst_len - 1], lst[0] = lst[0], lst[lst_len - 1]
+sort_nums = []
+hq.heapify(n_lst)
+while n_lst:
+    sort_nums.append(hq.heappop(n_lst))
 
-heap_sort(n_lst)
-print(*n_lst)
+print(*sort_nums)
+
+# def heapify(lst, lst_len):
+#     last_parent = lst_len // 2 - 1
+#     for cur in range(last_parent, -1, -1):
+#         while cur <= last_parent:
+#             child = cur * 2 + 1
+#             sibling = child + 1
+#             if sibling < lst_len and lst[child] < lst[sibling]:
+#                 child = sibling
+#             if lst[cur] < lst[child]:
+#                 lst[cur], lst[child] = lst[child], lst[cur]
+#                 cur = child
+#             else:
+#                 break
+    
+# def heap_sort(lst):
+#     for lst_len in range(len(lst), 1, -1):
+#         heapify(lst, lst_len)
+#         lst[lst_len - 1], lst[0] = lst[0], lst[lst_len - 1]
+
+# heap_sort(n_lst)
+# print(*n_lst)
