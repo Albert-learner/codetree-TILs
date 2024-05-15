32f5032c20458313ef64859629d26d06fa97c46e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-15 09:34:54 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 169ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240515/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,16 @@
+brackets = input()
+
+stack = []
+for bracket in brackets:
+    if bracket == '(':
+        stack.append(bracket)
+    else:
+        if len(stack) == 0:
+            print("No")
+            break
+        stack.pop()
+
+if len(stack) != 0:
+    print("No")
+else:
+    print("Yes")
