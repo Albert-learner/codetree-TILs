e303ff44bb631a5b8b55a221759dc7991add333d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-24 23:50:34 +0900

    Title: 오목, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240524/오목/O-mok.py
+++ 240524/오목/O-mok.py
@@ -1,28 +1,27 @@
-board = [list(map(int, input().split())) for _ in range(19)]
-
-winner = 0
-mid_coord = None
-
-move_dirs = [(-1, 0), (-1, 1), (0, 1), (1, 1)]
-def DFS(x, y, cnts, color, dx, dy):
-    global winner, mid_coord
-
+def DFS(board, x, y, cnts, color, dx, dy):
     if cnts == 5:
-        winner = color
-        mid_coord = (x - dx * 2, y - dy * 2)
-        return
+        return color, (x - dx * 2, y - dy * 2)
     
     mx, my = x + dx, y + dy
     if 0 <= mx < 19 and 0 <= my < 19 and board[mx][my] == color:
-        DFS(mx, my, cnts + 1, color, dx, dy)
+        return DFS(board, mx, my, cnts + 1, color, dx, dy)
     else:
-        return
+        return None
 
-for i in range(len(board)):
-    for j in range(len(board[0])):
-        if board[i][j] == 1 or board[i][j] == 2:
-            for dx, dy in move_dirs:
-                DFS(i, j, 1, board[i][j], dx, dy)
+def find_winner(board):
+    move_dirs = [(-1, 0), (-1, 1), (0, 1), (1, 1)]
+    
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 1 or board[i][j] == 2:
+                for dx, dy in move_dirs:
+                    result = DFS(board, i, j, 1, board[i][j], dx, dy)
+                    if result:
+                        return result
+    return 0, None
+
+board = [list(map(int, input().split())) for _ in range(19)]
+winner, mid_coord = find_winner(board)
 
 if winner != 0:
     print(winner)
