4b554dd0decce9e5b92cb4aeaf84b77d4c24c70c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-26 09:23:46 +0900

    Title: 알파벳에 따른 평가, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240526/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,3 @@
+input_str = input()
+
+print("Superior" if input_str == 'S' else "Excellent" if input_str == 'A' else "Good" if input_str == 'B' else "Usually" if input_str == 'C' else "Effort" if input_str == 'D' else "Failure")
