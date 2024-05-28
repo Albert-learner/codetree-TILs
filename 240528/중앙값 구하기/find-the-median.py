79d273270296e51cd5661b93d3041a8535fc04ee
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-05-28 09:43:27 +0900

    Title: 중앙값 구하기, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240528/중앙값 구하기/find-the-median.py
+++ 240528/중앙값 구하기/find-the-median.py
@@ -1,5 +1,26 @@
 a, b, c = map(int, input().split())
 
-lst = sorted([a, b, c])
+# lst = sorted([a, b, c])
 
-print(lst[1])
+# print(lst[1])
+
+if b > c:
+    if b > a and a > c:
+        print(a)
+else:
+    if c > a and a > b:
+        print(a)
+
+if a > c:
+    if a > b and b > c:
+        print(b)
+else:
+    if b > a and c > b:
+        print(b)
+
+if a > b:
+    if a > c and c > b:
+        print(c)
+else:
+    if b > c and c > a:
+        print(c)
