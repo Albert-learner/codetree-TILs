ffce06168e3c86a91927fcd8bdcee38ec45660d5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-05 10:22:05 +0900

    Title: 별표 출력하기 13, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240605/별표 출력하기 13/print-start-13.py
+++ 240605/별표 출력하기 13/print-start-13.py
@@ -1,30 +1,44 @@
-N = int(input())
+# N = int(input())
 
-plus, minus = 1, N
-for i in range(1, N + 1):
-    if i % 2 == 0:
-        print("* " * plus)
-        plus += 1
-    else:
-        print("* " * minus)
-        minus -= 1
+# plus, minus = 1, N
+# for i in range(1, N + 1):
+#     if i % 2 == 0:
+#         print("* " * plus)
+#         plus += 1
+#     else:
+#         print("* " * minus)
+#         minus -= 1
 
-plus = (N // 2 + 1)
-minus = N // 2
+# plus = (N // 2 + 1)
+# minus = N // 2
 
-if N % 2 == 0:
-    for i in range(1, N + 1):
-        if i % 2 == 0:
-            print("* " * plus)
-            plus += 1
-        else:
-            print("* " * minus)
-            minus -= 1
-else:
-    for i in range(1, N + 1):
-        if i % 2 == 0:
-            print("* " * minus)
-            minus -= 1
-        else:
-            print("* " * plus)
-            plus += 1
+# if N % 2 == 0:
+#     for i in range(1, N + 1):
+#         if i % 2 == 0:
+#             print("* " * plus)
+#             plus += 1
+#         else:
+#             print("* " * minus)
+#             minus -= 1
+# else:
+#     for i in range(1, N + 1):
+#         if i % 2 == 0:
+#             print("* " * minus)
+#             minus -= 1
+#         else:
+#             print("* " * plus)
+#             plus += 1
+
+# 모범답안
+# 변수 선언 및 입력
+n = int(input())
+
+# i가 홀수인 경우 별을 1 + (i // 2)개, 짝수인 경우 n - (i // 2)개 출력합니다
+for i in range(2 * n):
+	if i % 2 == 1:
+		for _ in range(1 + i // 2):
+			print("* ", end="")
+	else:
+		for _ in range(n - i // 2):
+			print("* ", end="")
+	print()
