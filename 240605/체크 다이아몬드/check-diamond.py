2f3eb61463751988dbd7ef4b1482a79d878acb80
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-05 09:53:49 +0900

    Title: 체크 다이아몬드, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240605/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N):
+    for j in range(N, i + 1, -1):
+        print(' ', end = '')
+    for j in range(i + 1):
+        print("* ", end = '')
+    print()
+
+for i in range(1, N):
+    for j in range(i):
+        print(' ', end = '')
+    for j in range(N, i, -1):
+        print("* ", end = '')
+    print()
