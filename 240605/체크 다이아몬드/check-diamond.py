9a9b1907561b709a35d632dcec36681596bd3bec
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-05 09:54:22 +0900

    Title: 체크 다이아몬드, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240605/체크 다이아몬드/check-diamond.py
+++ 240605/체크 다이아몬드/check-diamond.py
@@ -13,3 +13,23 @@ for i in range(1, N):
     for j in range(N, i, -1):
         print("* ", end = '')
     print()
+
+# # 모범답안
+# # 변수 선언 및 입력
+# n = int(input())
+
+# # 모양에 맞게 위쪽 별을 출력합니다.
+# for i in range(n):
+# 	for _ in range(n - i - 1):
+# 		print(" ", end="")
+# 	for _ in range(i + 1):
+# 		print("* ", end="")
+# 	print()
+
+# # 모양에 맞게 아래쪽 별을 출력합니다.
+# for i in range(n-2, -1, -1):
+# 	for _ in range(n - i - 1):
+# 		print(" ", end="")
+# 	for _ in range(i + 1):
+# 		print("* ", end="")
+# 	print()
