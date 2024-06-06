d58591424ca11fec886015314aa4a499c541dc92
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-06 17:39:14 +0900

    Title: 홀수로 이루어진 정사각형, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240606/홀수로 이루어진 정사각형/square-with-odd-numbers.py
+++ 240606/홀수로 이루어진 정사각형/square-with-odd-numbers.py
@@ -4,3 +4,13 @@ for i in range(0, 2 * N, 2):
     for j in range(11, N * 2 - 1 + 11, 2):
         print(i + j, end = ' ')
     print()
+
+# # 모범답안
+# # 변수 선언 및 입력
+# n = int(input())
+	
+# # n칸의 정사각형에 올바른 숫자를 출력합니다.
+# for i in range(n):
+# 	for j in range(n):
+# 		print(i * 2 + j * 2 + 11, end=" ")
+# 	print()
