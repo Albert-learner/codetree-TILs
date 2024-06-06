f3982ac8a54184b0395cb4b7ae9828d777dc6ea9
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-06 19:18:38 +0900

    Title: 좌우로 반복하며 출력, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240606/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ 240606/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -2,6 +2,10 @@ N = int(input())
 
 for i in range(1, N + 1):
     if i % 2 == 1:
-        print(*[j for j in range(1, N + 1)], end = '')
+        for j in range(1, N + 1):
+            print(j, end = '')
+        print()
     else:
-        print(*[j for j in range(N, 0, -1)], end = '')
+        for j in range(N, 0, -1):
+            print(j, end = '')
+        print()
