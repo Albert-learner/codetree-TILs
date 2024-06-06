6f5bd305dfe9e9d61e290081447e7b79ab77322c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-06 19:02:12 +0900

    Title: 1-9 정사각형, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240606/1-9 정사각형/1-9-square.py
+++ 240606/1-9 정사각형/1-9-square.py
@@ -8,3 +8,17 @@ for i in range(N):
         if cnts > 9:
             cnts = 1
     print()
+
+# # 모범답안
+# # 변수 선언 및 입력
+# n = int(input())
+# cnt = 1
+	
+# # cnt를 이용해 n칸의 정사각형에 올바른 숫자를 출력합니다.
+# for _ in range(n):
+# 	for _ in range(n):
+# 		print(cnt, end="")
+# 		cnt += 1
+# 		if cnt == 10:
+# 			cnt = 1
+# 	print()
