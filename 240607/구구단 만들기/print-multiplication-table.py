150c8876b27c8caa870a4b8d1e1dbf325e86ee40
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-07 10:09:46 +0900

    Title: 구구단 만들기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240607/구구단 만들기/print-multiplication-table.py
+++ 240607/구구단 만들기/print-multiplication-table.py
@@ -9,3 +9,19 @@ for num in range(1, 10):
                 print(f"{n} * {num} = {n * num} /", end = ' ')
     else:
         print(f"{a} * {num} = {a * num}")
+
+# # 모범답안
+# # 변수 선언 및 입력
+# inp = input()
+# arr = inp.split()
+# a = int(arr[0])
+# b = int(arr[1])
+	
+# # b부터 a까지 감소하며 짝수의 구구단을 출력합니다.
+# for i in range(1, 10):
+# 	for j in range(b, a-1, -1):
+# 		if j % 2 == 0:
+# 			print(f"{j} * {i} = {i * j}", end="")
+# 			if j != a:
+# 				print(" / ", end="");
+# 	print()
