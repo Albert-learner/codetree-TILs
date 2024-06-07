91849adcf98216bb837cec183891ad3551ad9a5a
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-07 10:08:32 +0900

    Title: 구구단 만들기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240607/구구단 만들기/print-multiplication-table.py
+++ 240607/구구단 만들기/print-multiplication-table.py
@@ -1,8 +1,11 @@
 a, b = map(int, input().split())
 
 for num in range(1, 10):
-    for n in range(b, a - 1, -2):
-        if n == 2:
-            print(f"{n} * {num} = {n * num}")
-        else:
-            print(f"{n} * {num} = {n * num} /", end = ' ')
+    if a != b:
+        for n in range(b, a - 1, -2):
+            if n == 2:
+                print(f"{n} * {num} = {n * num}")
+            else:
+                print(f"{n} * {num} = {n * num} /", end = ' ')
+    else:
+        print(f"{a} * {num} = {a * num}")
