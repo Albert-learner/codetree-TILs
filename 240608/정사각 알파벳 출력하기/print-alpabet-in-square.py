4bb687a4a07e9db7e471ec52f05077e4b0974010
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-08 14:11:49 +0900

    Title: 정사각 알파벳 출력하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240608/정사각 알파벳 출력하기/print-alpabet-in-square.py
+++ 240608/정사각 알파벳 출력하기/print-alpabet-in-square.py
@@ -3,6 +3,5 @@ N = int(input())
 start_chr = ord('A')
 for i in range(N):
     for j in range(N):
-        start_chr += j
-        print(chr(start_chr), end = '')
+        print(chr(start_chr + i + j), end = '')
     print()
