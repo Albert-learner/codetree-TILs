640eedda3446f64a7106912e405aaf42efc06b46
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-08 13:24:06 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240608/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
+++ 240608/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -12,3 +12,19 @@ for i in range(N, 0, -1):
             if cnts == 10:
                 cnts = 1
     print()
+
+# # 모범답안
+# # 변수 선언 및 입력
+# n = int(input())
+# cnt = 1;
+	
+# # cnt를 이용해 숫자로 이루어진 삼각형을 출력합니다.
+# for i in range(n):
+# 	for j in range(i):
+# 		print(" ", end=" ")
+# 	for j in range(n - i):
+# 		print(cnt, end=" ")
+# 		cnt += 1
+# 		if cnt == 10:
+# 			cnt = 1
+# 	print()
