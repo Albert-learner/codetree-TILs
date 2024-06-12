94829d25b5b81d84739a4443e5009dcaeadcefa4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-13 00:17:16 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240613/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240613/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -7,19 +7,42 @@
 
 # print(max(dp))
 
-# 모범답안 1
+# # 모범답안 1
+# # 변수 선언 및 입력:
+# n = int(input())
+# price = list(map(int, input().split()))
+
+# # 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+# max_profit = 0
+# for i in range(n):
+#     # 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
+#     for j in range(i + 1, n):
+#         profit = price[j] - price[i]
+
+#         if profit > max_profit:
+#             max_profit = profit
+    
+# print(max_profit)
+
+# 모범답안 2
 # 변수 선언 및 입력:
 n = int(input())
 price = list(map(int, input().split()))
 
-# 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+# 배열을 앞에서부터 순회하며 최소값을 갱신해줍니다.
+# 각 원소에 대하여 해당 시점의 최소값과의 차이가
+# 최대가 될 때를 갱신해줍니다.
 max_profit = 0
+min_price = price[0]
 for i in range(n):
-    # 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
-    for j in range(i + 1, n):
-        profit = price[j] - price[i]
+    profit = price[i] - min_price
 
-        if profit > max_profit:
-            max_profit = profit
+    # 답을 갱신해줍니다.
+    if profit > max_profit:
+        max_profit = profit
+
+    # 지금까지의 최솟값을 갱신해줍니다.
+    if min_price > price[i]:
+        min_price = price[i]
     
 print(max_profit)
