8b09b76526217c61f33bb885d0310c288ca6487c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-12 09:49:38 +0900

    Title: 가장 큰 수 고르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240612/가장 큰 수 고르기/picking-biggest-number.py
+++ 240612/가장 큰 수 고르기/picking-biggest-number.py
@@ -1,3 +1,13 @@
 n_lst = list(map(int, input().split()))
 
-print(max(n_lst))
+# print(max(n_lst))
+
+max_val = arr[0]
+	
+# 10개의 숫자들 중 최댓값을 구합니다.
+for elem in arr:
+	if elem > max_val:
+		max_val = elem
+	
+# 최댓값을 출력합니다.
+print(max_val)
