bcfb7a39197724d9f52a135e85a1fb0cd6510ffc
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-12 10:19:09 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240612/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240612/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -13,32 +13,51 @@
 #     print(idx + 1, end = ' ')
 #     n_lst = n_lst[:idx]
 
-# 모범답안 1
-# 변수 선언 및 입력
+# # 모범답안 1
+# # 변수 선언 및 입력
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# prev_max_idx = n
+
+# # 첫 번째 원소가 최대가 되기 전까지 계속 반복합니다.
+# while True:
+#     # 최대값 후보의 시작은 항상 첫 번째 원소입니다.
+#     max_idx = 0
+
+#     # 두 번째 원소부터 바로 직전 최대로 뽑힌
+#     # 원소 전까지 보면서 그 중 최대 index를 갱신합니다.
+#     # index를 오름차순으로 보기 때문에
+#     # 최댓값이 여러개인 경우 가장 왼쪽에 있는
+#     # 원소가 뽑히게 됩니다.
+#     for i in range(1, prev_max_idx):
+#         if a[i] > a[max_idx]:
+#             max_idx = i
+
+#     print(max_idx + 1, end=" ")
+
+#     # 최대인 원소가 첫 번째 원소라면 종료합니다.
+#     if max_idx == 0:
+#         break
+
+#     #바로 직전 최대 index를 갱신해줍니다.
+#     prev_max_idx = max_idx
+
+# 모범답안 2
+# 변수 선언 및 입력:
 n = int(input())
 a = list(map(int, input().split()))
+indices = list()
 
-prev_max_idx = n
+# 첫 번째 원소는 항상 답이 됩니다.
+indices.append(0)
 
-# 첫 번째 원소가 최대가 되기 전까지 계속 반복합니다.
-while True:
-    # 최대값 후보의 시작은 항상 첫 번째 원소입니다.
-    max_idx = 0
+# 바로 직전에 답으로 추가한 원소보다
+# 현재 원소가 더 큰 경우에만 답으로 추가합니다.
+for i in range(1, n):
+    last_idx = indices[-1]
+    if a[i] > a[last_idx]:
+        indices.append(i)
 
-    # 두 번째 원소부터 바로 직전 최대로 뽑힌
-    # 원소 전까지 보면서 그 중 최대 index를 갱신합니다.
-    # index를 오름차순으로 보기 때문에
-    # 최댓값이 여러개인 경우 가장 왼쪽에 있는
-    # 원소가 뽑히게 됩니다.
-    for i in range(1, prev_max_idx):
-        if a[i] > a[max_idx]:
-            max_idx = i
-
-    print(max_idx + 1, end=" ")
-
-    # 최대인 원소가 첫 번째 원소라면 종료합니다.
-    if max_idx == 0:
-        break
-
-    #바로 직전 최대 index를 갱신해줍니다.
-    prev_max_idx = max_idx
+for idx in indices[::-1]:
+    print(idx + 1, end=' ')
