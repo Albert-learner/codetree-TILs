5516cfec152cfc2565621868ff764e1898b850c6
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-15 15:47:08 +0900

    Title: 최고의 33위치, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240615/최고의 33위치/best-place-of-33.py
+++ 240615/최고의 33위치/best-place-of-33.py
@@ -1,21 +1,12 @@
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-row_diff, col_diff = len(board) - 3, len(board[0]) - 3
 max_coins = 0
-
-if row_diff == 0 and col_diff == 0:
-    for r in range(len(board)):
-        for c in range(len(board[0])):
-            max_coins += board[r][c]
-else:
-    coins_lst = []
-    for r in range(row_diff):
-        for c in range(col_diff):
-            coins = 0
-            for k in range(3):
-                for l in range(3):
-                    coins += board[r + k][c + l]
-            max_coins = max(max_coins, coins)
+for i in range(N - 2):
+    for j in range(N - 2):
+        coins = 0
+        for r in board[i:i + 3]:
+            coins += sum(r[j:j + 3])
+        max_coins = max(max_coins, coins)
 
 print(max_coins)
