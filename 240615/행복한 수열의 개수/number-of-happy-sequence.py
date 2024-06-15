e1e4ca5c7caac8a9a8af059d10eac3d842652fdc
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-15 16:41:54 +0900

    Title: 행복한 수열의 개수, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240615/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240615/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,26 +1,17 @@
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-def has_consecutive_numbers(lst):
-    for i in range(len(lst) - 1):
-        if lst[i] == lst[i + 1]:
-            return True
-    
-    return False
-
-def check_consecutive(matrix):
-    row, col = len(matrix), len(matrix[0])
-    happy_sequences = 0
-
-    for i in range(row):
-        if has_consecutive_numbers(matrix[i]):
+happy_sequences = 0
+for row in board:
+    for i in range(N - M + 1):
+        if row[i:i + M].count(row[i]) == M:
             happy_sequences += 1
+            break
 
-    for j in range(col):
-        column = [matrix[i][j] for i in range(row)]
-        if has_consecutive_numbers(column):
+for col in zip(*board):
+    for i in range(N - M + 1):
+        if col[i:i + M].count(col[i]) == M:
             happy_sequences += 1
+            break
 
-    return happy_sequences
-
-print(check_consecutive(board))
+print(happy_sequences)
