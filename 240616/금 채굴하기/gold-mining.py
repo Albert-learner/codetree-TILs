46871922668352d778cead30beda2b4ddfea1bb7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-16 15:31:13 +0900

    Title: 금 채굴하기, Time: 206ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240616/금 채굴하기/gold-mining.py
+++ 240616/금 채굴하기/gold-mining.py
@@ -1,49 +1,55 @@
 from collections import deque
 
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-
-def get_max_golds(sy, sx, n, m, board):
-    max_golds = 0
+def get_max_gold(start_y, start_x, n, gold_price, board):
+    max_gold = 0
     visited = [[False] * n for _ in range(n)]
-    f_dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
 
     for k in range(n):
         for i in range(n):
             visited[i] = [False] * n
-        
-        visited[sy][sx] = True
-        BFS = deque([(sy, sx, 0)])
-        total_golds = 0
 
-        while BFS:
-            y, x, side = BFS.popleft()
+        visited[start_y][start_x] = True
+        bfs = deque([(start_y, start_x, 0)])
+        total_gold_acquired = 0
 
-            if k < side:
+        while bfs:
+            y, x, step = bfs.popleft()
+
+            if k < step:
                 break
 
-            total_golds += board[y][x]
-            for dy, dx in f_dirs:
-                my, mx = y + dy, x + dx
+            total_gold_acquired += board[y][x]
 
-                if my < 0 or mx < 0 or my >= n or mx >= n:
+            for i in range(4):
+                ny, nx = y + dy[i], x + dx[i]
+
+                if ny < 0 or nx < 0 or ny >= n or nx >= n:
                     continue
 
-                if visited[my][mx]:
+                if visited[ny][nx]:
                     continue
 
-                visited[my][mx] = True
-                BFS.append((my, mx, side + 1))
+                visited[ny][nx] = True
+                bfs.append((ny, nx, step + 1))
 
         cost = k * k + (k + 1) * (k + 1)
-        if cost <= total_golds * m:
-            max_golds = max(max_golds, total_golds)
+        if cost <= total_gold_acquired * gold_price:
+            max_gold = max(max_gold, total_gold_acquired)
 
-    return max_golds
+    return max_gold
 
-max_golds = 0
-for y in range(N):
-    for x in range(N):
-        max_golds = max(max_golds, get_max_golds(y, x, N, M, board))
+def main():
+    n, gold_price = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(n)]
+    max_gold = 0
 
-print(max_golds)
+    for y in range(n):
+        for x in range(n):
+            max_gold = max(max_gold, get_max_gold(y, x, n, gold_price, board))
+
+    print(max_gold)
+
+if __name__ == "__main__":
+    main()
