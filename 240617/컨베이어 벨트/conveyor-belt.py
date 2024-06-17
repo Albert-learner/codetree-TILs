76896030156daba25964fd57f01e0a349604974f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-17 13:06:41 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240617/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,16 @@
+N, T = map(int, input().split())
+belt = [list(map(int, input().split())) for _ in range(2)]
+it_first_col = 0
+
+while T > 0:
+    tmp = belt[0][it_first_col + (N - 1) % N]
+    belt[0][it_first_col + (N - 1) % N] = belt[1][it_first_col + (N - 1) % N]
+    belt[1][it_first_col + (N - 1) % N] = tmp
+
+    it_first_col = (it_first_col - 1 + N) % N
+    T -= 1
+
+for ir in range(2):
+    for ic in range(it_first_col, it_first_col + N):
+        print(belt[ir][ic % N], end = ' ')
+    print()
