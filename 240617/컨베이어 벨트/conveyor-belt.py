3f049e6314cabd773365de278dded025bdf68232
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-17 13:08:08 +0900

    Title: 컨베이어 벨트, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240617/컨베이어 벨트/conveyor-belt.py
+++ 240617/컨베이어 벨트/conveyor-belt.py
@@ -3,9 +3,9 @@ belt = [list(map(int, input().split())) for _ in range(2)]
 it_first_col = 0
 
 while T > 0:
-    tmp = belt[0][it_first_col + (N - 1) % N]
-    belt[0][it_first_col + (N - 1) % N] = belt[1][it_first_col + (N - 1) % N]
-    belt[1][it_first_col + (N - 1) % N] = tmp
+    tmp = belt[0][(it_first_col + (N - 1)) % N]
+    belt[0][(it_first_col + (N - 1)) % N] = belt[1][(it_first_col + (N - 1)) % N]
+    belt[1][(it_first_col + (N - 1)) % N] = tmp
 
     it_first_col = (it_first_col - 1 + N) % N
     T -= 1
