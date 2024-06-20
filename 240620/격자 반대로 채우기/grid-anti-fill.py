4192c929447a2ca26e50028b96817f72f67de471
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-20 12:30:43 +0900

    Title: 격자 반대로 채우기, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/격자 반대로 채우기/grid-anti-fill.py
+++ 240620/격자 반대로 채우기/grid-anti-fill.py
@@ -25,3 +25,31 @@ else:
 
 for row in board:
     print(*row)
+
+# # 모범답안
+# # 변수 선언 및 입력
+# n = int(input())
+# answer = [
+#     [0 for _ in range(n)]
+#     for _ in range(n)
+# ]
+# count = 1
+
+# # 격자를 채워줍니다.
+# for col in range(n - 1, -1, -1):
+#     if (n - 1 - col) % 2 == 0:
+#         # 이 케이스에는 아래에서 위로 값을 채워줍니다.
+#         for row in range(n - 1, -1, -1):
+#             answer[row][col] = count
+#             count += 1
+#     else:
+#         # 이 케이스에는 위에서 아래로 값을 채워줍니다.
+#         for row in range(n):
+#             answer[row][col] = count
+#             count += 1
+
+# # 출력:
+# for row in answer:
+#     for elem in row:
+#         print(elem, end=" ")
+#     print()
