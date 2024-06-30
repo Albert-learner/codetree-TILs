4ede767268ca794283dfe52890c68cedb8975b8b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-06-30 13:24:23 +0900

    Title: 떨어지는 1자 블록, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240630/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240630/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,21 +1,22 @@
-N, M, K = tuple(map(int, input().split()))
-board = [list(map(int, input().split())) for _ in range(N)]
+n, m, k = tuple(map(int, input().split()))
+grid = [list(map(int, input().split())) for _ in range(n)]
 flag = False
-
-for row in range(N):
-    for col in range(K - 1, K - 1 + M):
-        if board[row][col] == 1:
-            for j in range(K - 1, K - 1 + M):
-                board[row - 1][j] = 1
+for row in range(n):
+    for col in range(k - 1, k - 1 + m):
+        if grid[row][col] == 1:
+            for j in range(k - 1, k - 1 + m):
+                grid[row - 1][j] = 1
             flag = True
             break
     if flag:
         break
         
 if not flag:
-    for row in range(N):
-        for col in range(K - 1,K - 1 + M):
-            board[row][col] = 1
+    for row in range(n):
+        for col in range(k-1,k-1+m):
+            grid[row][col] = 1
 
-for row in board:
-    print(*row)
+for row in range(n):
+    for col in range(n):
+        print(grid[row][col], end=' ')
+    print()
