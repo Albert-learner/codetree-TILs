79071b2ed1c8c6b4654d248ad7ca334a9fc0b7a4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-07-04 23:03:20 +0900

    Title: 숫자의 순차적 이동, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/숫자의 순차적 이동/sequential-movement-of-numbers.py
+++ 240704/숫자의 순차적 이동/sequential-movement-of-numbers.py
@@ -1,8 +1,8 @@
 import sys
 
 # 입력
-N, T = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
+n, t = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 # 선언
 dxs = [-1, -1, 0, 1, 1, 1, 0, -1]
@@ -10,17 +10,7 @@ dys = [0, 1, 1, 1, 0, -1, -1, -1]
 
 # 격자 안에 있는 범위 탐색
 def is_in_range(x, y) :
-    return 0 <= x < N and 0 <= y < N
-
-# 교체하기
-def Exchange(max_num, num) :
-    max_row, max_col = Search_point(max_num)
-    row, col = Search_point(num)
-
-    temp = board[max_row][max_col]
-    board[max_row][max_col] = board[row][col]
-    board[row][col] = temp
-    return board
+    return 0 <= x < n and 0 <= y < n
 
 # 8방향 중 최대값 찾기 
 def Search_dirt(row, col) :
@@ -29,23 +19,30 @@ def Search_dirt(row, col) :
         next_x, next_y = row + dx, col + dy
 
         if is_in_range(next_x, next_y) :
-            if board[next_x][next_y] > max_num :
-                max_num = board[next_x][next_y]
+            if arr[next_x][next_y] > max_num :
+                max_num = arr[next_x][next_y]
     return max_num
 
 # 각 값들의 위치(r, c) 찾기 
 def Search_point(num) :
-    for row in range(N) :
-        for col in range(N) :
-            if board[row][col] == num :
+    for row in range(n) :
+        for col in range(n) :
+            if arr[row][col] == num :
                 return row, col
 
-for turn in range(T) :
-    # 1부터 n * n까지의 위치 찾기
-    for num in range(1, (N ** 2) + 1) :
+# 교체하기
+def Exchange(max_num, num, row, col) :
+    max_row, max_col = Search_point(max_num)
+
+    temp = arr[max_row][max_col]
+    arr[max_row][max_col] = arr[row][col]
+    arr[row][col] = temp
+    return arr
+for turn in range(t) :
+    for num in range(1, (n * n) + 1) :
         row, col = Search_point(num) # 숫자의 위치찾기
         max_num = Search_dirt(row, col) # 8방향 중 최대값 찾기
-        arr = Exchange(max_num, num)
+        arr = Exchange(max_num, num, row, col)
 
-    for row in board:
+    for row in arr:
         print(*row)
