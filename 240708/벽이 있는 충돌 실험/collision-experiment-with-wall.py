9928c5c33dd24a2c26d38e09f1dc2f8e35778920
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-07-08 22:09:25 +0900

    Title: 벽이 있는 충돌 실험, Time: 5099ms, Memory: 52MB, Status: Time Limit Exceed - CodeTree

--- 240708/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240708/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,25 +1,19 @@
-T = int(input())
+import sys
 
+# 방향을 나타내는 배열
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 def parse_key(key):
+    # 좌표 문자열을 분리하여 리스트로 반환
     return list(map(int, key.split(',')))
 
 def make_key(x, y):
+    # 좌표를 문자열로 변환하여 키로 사용
     return f"{x},{y}"
 
-def get_direc(direct):
-    if direct == 'U':
-        return 0
-    elif direct == 'D':
-        return 1
-    elif direct == 'L':
-        return 2
-    else:
-        return 3
-
 def turn_direc(direc):
+    # 방향을 반대로 전환
     if direc == 0:
         return 1
     elif direc == 1:
@@ -29,53 +23,81 @@ def turn_direc(direc):
     else:
         return 2
 
-def is_range(x, y):
-    return 0 <= x < N and 0 <= y < N
+def is_range(x, y, n):
+    # 좌표가 유효한 범위인지 확인
+    return 0 <= x < n and 0 <= y < n
 
-results = []
-for _ in range(T):
-    N, M = map(int, input().split())
+def get_direc(direc):
+    # 방향을 숫자로 변환
+    if direc == 'U':
+        return 0
+    elif direc == 'D':
+        return 1
+    elif direc == 'L':
+        return 2
+    else:
+        return 3
+
+def main():
+    input = sys.stdin.read
+    data = input().split()
     
-    map_dict = {}
-    for _ in range(M):
-        x, y, d = input().split()
-        x, y = int(x) - 1, int(y) - 1
-        direct = get_direc(d)
+    idx = 0
+    t = int(data[idx])
+    idx += 1
+    results = []
 
-        map_dict[make_key(y, x)] = direct
+    for _ in range(t):
+        n = int(data[idx])
+        m = int(data[idx + 1])
+        idx += 2
 
-    sec = 0
-    while sec <= 2 * N:
-        sec += 1
-        board = [[0] * N for _ in range(N)]
-        renew_map = {}
+        # 초기 지도 생성
+        map_dict = {}
 
-        for key in map_dict.keys():
-            pos = parse_key(key)
-            x, y = pos
-            direct = map_dict[key]
+        for _ in range(m):
+            r = int(data[idx]) - 1
+            c = int(data[idx + 1]) - 1
+            direc = get_direc(data[idx + 2])
+            idx += 3
 
-            mx, my = x + dx[direct], y + dy[direct]
+            map_dict[make_key(c, r)] = direc
 
-            if is_range(mx, my):
-                board[mx][my] += 1
+        sec = 0
+        while sec <= 2 * n:
+            sec += 1
+            memo = [[0] * n for _ in range(n)]
+            renew_map = {}
 
-                if board[mx][my] > 1:
-                    renew_map.pop(make_key(mx, my), None)
+            for key in map_dict.keys():
+                pos = parse_key(key)
+                x, y = pos
+                direc = map_dict[key]
+
+                nx = x + dx[direc]
+                ny = y + dy[direc]
+
+                if is_range(nx, ny, n):
+                    memo[ny][nx] += 1
+
+                    if memo[ny][nx] > 1:
+                        renew_map.pop(make_key(nx, ny), None)
+                    else:
+                        renew_map[make_key(nx, ny)] = direc
                 else:
-                    renew_map[make_key(mx, my)] = direct
-            else:
-                board[y][x] += 1
+                    memo[y][x] += 1
 
-                if board[y][x] > 1:
-                    renew_map.pop(make_key(x, y), None)
-                else:
-                    renew_map[make_key(x, y)] = turn_direc(direct)
-                    
-        map_dict = renew_map
+                    if memo[y][x] > 1:
+                        renew_map.pop(make_key(x, y), None)
+                    else:
+                        renew_map[make_key(x, y)] = turn_direc(direc)
 
-    answer = len(map_dict)
-    results.append(str(answer))
+            map_dict = renew_map
 
-for result in results:
-    print(result)
+        ans = len(map_dict)
+        results.append(str(ans))
+
+    print("\n".join(results))
+
+if __name__ == "__main__":
+    main()
