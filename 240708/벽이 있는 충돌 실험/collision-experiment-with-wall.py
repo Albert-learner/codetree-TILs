624195de14e7e507c37f1d27fdc8d0bd0b9c4897
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-07-08 22:35:06 +0900

    Title: 벽이 있는 충돌 실험, Time: 2073ms, Memory: 29MB, Status: Passed - CodeTree

--- 240708/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240708/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,103 +1,79 @@
-import sys
+# 입력 처리
+t = int(input())
 
-# 방향을 나타내는 배열
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-def parse_key(key):
-    # 좌표 문자열을 분리하여 리스트로 반환
-    return list(map(int, key.split(',')))
-
-def make_key(x, y):
-    # 좌표를 문자열로 변환하여 키로 사용
-    return f"{x},{y}"
-
-def turn_direc(direc):
-    # 방향을 반대로 전환
-    if direc == 0:
-        return 1
-    elif direc == 1:
-        return 0
-    elif direc == 2:
-        return 3
-    else:
-        return 2
-
-def is_range(x, y, n):
-    # 좌표가 유효한 범위인지 확인
+# 격자 안에 있는 범위 탐색
+def is_in_range(x, y, n):
     return 0 <= x < n and 0 <= y < n
 
-def get_direc(direc):
-    # 방향을 숫자로 변환
-    if direc == 'U':
-        return 0
-    elif direc == 'D':
-        return 1
-    elif direc == 'L':
-        return 2
+# 남은 구슬들 세기
+def count(test_cases, n):
+    cnt = 0
+    for x, y, d in test_cases:
+        if x != n and y != n:
+            cnt += 1
+    return cnt
+
+# 격자에서 벗어나는 경우 - 방향을 전환하는 함수
+def change_dirt(d):
+    return (d + 2) % 4
+
+# 이동하는 함수
+def move(x, y, d, n):
+    next_x = x + dx[d]
+    next_y = y + dy[d]
+    if is_in_range(next_x, next_y, n):
+        return next_x, next_y, d
     else:
-        return 3
+        return x, y, change_dirt(d)
 
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    
-    idx = 0
-    t = int(data[idx])
-    idx += 1
-    results = []
+for _ in range(t):
+    test_cases = []
+    n, m = map(int, input().split())
 
-    for _ in range(t):
-        n = int(data[idx])
-        m = int(data[idx + 1])
-        idx += 2
+    for _ in range(m):
+        x, y, d = input().split()
 
-        # 초기 지도 생성
-        map_dict = {}
+        if d == 'R':
+            d = 0
+        elif d == 'D':
+            d = 1
+        elif d == 'L':
+            d = 2
+        elif d == 'U':
+            d = 3
 
-        for _ in range(m):
-            r = int(data[idx]) - 1
-            c = int(data[idx + 1]) - 1
-            direc = get_direc(data[idx + 2])
-            idx += 3
+        x = int(x) - 1
+        y = int(y) - 1
+        test_cases.append([x, y, d])
 
-            map_dict[make_key(c, r)] = direc
+    for _ in range(n * 2):  # 격자의 *2만큼 반복하면 무한히 반복되는게 확정되려나
+        visited = [[0 for _ in range(n)] for _ in range(n)]
+        conflict_positions = set()
 
-        sec = 0
-        while sec <= 2 * n:
-            sec += 1
-            memo = [[0] * n for _ in range(n)]
-            renew_map = {}
+        # 각 구슬에 대해 이동 처리
+        for i in range(m):
+            x, y, d = test_cases[i]
 
-            for key in map_dict.keys():
-                pos = parse_key(key)
-                x, y = pos
-                direc = map_dict[key]
+            if x >= n or y >= n:  # x 또는 y가 n을 넘어가면 깨진 구슬이라는 의미이므로 패스
+                continue
 
-                nx = x + dx[direc]
-                ny = y + dy[direc]
+            nx, ny, nd = move(x, y, d, n)
+            test_cases[i] = [nx, ny, nd]
 
-                if is_range(nx, ny, n):
-                    memo[ny][nx] += 1
+            visited[nx][ny] += 1
 
-                    if memo[ny][nx] > 1:
-                        renew_map.pop(make_key(nx, ny), None)
-                    else:
-                        renew_map[make_key(nx, ny)] = direc
-                else:
-                    memo[y][x] += 1
+            if visited[nx][ny] > 1:
+                conflict_positions.add((nx, ny))
 
-                    if memo[y][x] > 1:
-                        renew_map.pop(make_key(x, y), None)
-                    else:
-                        renew_map[make_key(x, y)] = turn_direc(direc)
+        # 충돌 처리
+        if conflict_positions:
+            for i in range(m):
+                x, y, _ = test_cases[i]
+                if (x, y) in conflict_positions:
+                    test_cases[i] = [n, n, -1]  # 파괴된 구슬로 설정
 
-            map_dict = renew_map
-
-        ans = len(map_dict)
-        results.append(str(ans))
-
-    print("\n".join(results))
-
-if __name__ == "__main__":
-    main()
+    # 결과 출력
+    print(count(test_cases, n))
