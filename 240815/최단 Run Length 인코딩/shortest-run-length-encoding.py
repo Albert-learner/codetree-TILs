7d2a666328829a30d714480f4f798b7a0574bc42
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-08-15 09:36:43 +0900

    Title: 최단 Run Length 인코딩, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240815/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240815/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -3,30 +3,36 @@ from collections import deque
 a_str = input()
 
 def run_length_encoding(input_str):
-    result = ""
+    ret_str = ""
+    
     indices = [-1]
-    for chr_idx in range(len(input_str) - 1):
-        if input_str[chr_idx] != input_str[chr_idx + 1]:
-            indices.append(chr_idx)
+    for i in range(len(input_str) - 1):
+        if input_str[i] != input_str[i + 1]:
+            indices.append(i)
     indices.append(len(input_str) - 1)
-
+    
     cnts_lst = []
     for i in range(len(indices) - 1):
         diff = indices[i + 1] - indices[i]
         cnts_lst.append(diff)
 
     for chr_idx, cnts in zip(indices[1:], cnts_lst):
-        result += (input_str[chr_idx] + str(cnts))
+        ret_str += (input_str[chr_idx] + str(cnts))
 
-    return result
+    return ret_str
 
 min_length = float("INF")
-for cnts in range(1, len(a_str)):
-    a_str_que = deque(a_str)
-    a_str_que.rotate(cnts)
-    a_str_rotate = "".join(a_str_que)
+cnts = len(a_str)
 
-    encoding = len(run_length_encoding(a_str_rotate))
-    min_length = min(min_length, encoding)
+while cnts > 0:
+    encoding = run_length_encoding(a_str)
+    encoding_len = len(encoding)
+
+    min_length = min(min_length, encoding_len)
+
+    a_str_que = deque(a_str)
+    a_str_que.rotate(1)
+    a_str = "".join(a_str_que)
+    cnts -= 1
 
 print(min_length)
