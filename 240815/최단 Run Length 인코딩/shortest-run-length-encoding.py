00758ddd8250e4a2bd6da0a50b1634beb02f31fc
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2024-08-15 09:40:47 +0900

    Title: 최단 Run Length 인코딩, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 240815/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240815/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -22,17 +22,12 @@ def run_length_encoding(input_str):
     return ret_str
 
 min_length = float("INF")
-cnts = len(a_str)
-
-while cnts > 0:
-    encoding = run_length_encoding(a_str)
-    encoding_len = len(encoding)
-
-    min_length = min(min_length, encoding_len)
-
+for cnts in range(len(a_str)):
     a_str_que = deque(a_str)
-    a_str_que.rotate(1)
-    a_str = "".join(a_str_que)
-    cnts -= 1
+    a_str_que.rotate(cnts)
+    a_str_rotate = "".join(a_str_que)
+
+    encoding = len(run_length_encoding(a_str_rotate))
+    min_length = min(min_length, encoding)
 
 print(min_length)
