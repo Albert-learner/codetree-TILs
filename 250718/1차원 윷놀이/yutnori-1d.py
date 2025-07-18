9e2451ae0522073edfc7e646e14d49a5a192e561
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-07-18 22:36:49 +0900

    Title: 1차원 윷놀이, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250718/1차원 윷놀이/yutnori-1d.py
+++ 250718/1차원 윷놀이/yutnori-1d.py
@@ -1,35 +1,24 @@
-n, m, k = map(int, input().split())
-command = list(map(int, input().split()))
-
-
-#command 순서가 올 때 마다 , 어느 말을 이동시킬 것인지, 결정해야한다.
-horse_status = [1 for _ in range(k)]
-MAX = -1
-
-def calc():
-    global horse_status
-    score = 0
-    for piece in horse_status:
-        score += (piece >= m)
-    return score
-
-def backtracking(curr_idx):
-    global n, m, k, command, horse_status, MAX
-
-    MAX = max(MAX, sum(1 for val in horse_status if val >= m))
-    # MAX = max(MAX, calc()) #해설코드
-
+def backtracking(curr_idx, horse_status, command, n, m, k):
     if curr_idx == n:
-        return
-    for i in range(k): #0부터 k-1 까지의 말을 선택.
+        return sum(1 for val in horse_status if val >= m)
 
+    max_score = 0
+    for i in range(k):
         if horse_status[i] >= m:
             continue
 
-        temp = horse_status[i]
+        # 말 이동
         horse_status[i] += command[curr_idx]
-        backtracking(curr_idx + 1)
-        horse_status[i] -= command[curr_idx]
+        score = backtracking(curr_idx + 1, horse_status, command, n, m, k)
+        max_score = max(max_score, score)
+        horse_status[i] -= command[curr_idx]  # 백트래킹
 
-backtracking(0)
-print(MAX)
+    return max_score
+
+# 입력 및 실행
+n, m, k = map(int, input().split())
+command = list(map(int, input().split()))
+horse_status = [1] * k
+
+result = backtracking(0, horse_status, command, n, m, k)
+print(result)
