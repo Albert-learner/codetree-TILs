082cdc306844141bc934364261598ee6ce1e04fc
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-07-27 16:12:09 +0900

    Title: 강력한 폭발, Time: 70ms, Memory: 17MB, Status: Passed - Codetree

--- 250727/강력한 폭발/strong-explosion.py
+++ 250727/강력한 폭발/strong-explosion.py
@@ -2,65 +2,68 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-import copy
+bomb_type = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+visited = [
+    [False for _ in range(n)]
+    for _ in range(n)
+]
+pts = []
+max_cnt = 0
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            pts.append([i, j])
 
-bomb_places = [(i, j) for i in range(n) for j in range(n) if grid[i][j] == 1]
+def count():
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                visited[i][j] = False
+    for i in range(n):
+        for j in range(n):
+            if bomb_type[i][j]:
+                fill_grid(i, j, bomb_type[i][j])
+    # print_visited()
+    for i in range(n):
+        for j in range(n):
+            if visited[i][j]:
+                cnt += 1
 
-max_bombs = 0
+    global max_cnt
+    max_cnt = max(max_cnt, cnt)
 
-def execute(cnts, grid):
-    global max_bombs
 
-    if cnts == len(bomb_places):
-        matrix = copy.deepcopy(grid)
-        bomb(matrix)
-        max_bombs = max(max_bombs, count(matrix))
+def in_range(i, j):
+    return 0 <= i < n and 0 <= j < n
+
+def choose(n):
+    if n == len(pts):
+        count()
         return
-
-    x, y = bomb_places[cnts]
-
     for i in range(1, 4):
-        grid[x][y] = i
-        execute(cnts + 1, grid)
-        grid[x][y] = 1
+        x, y = pts[n]
+        bomb_type[x][y] = i
+        choose(n + 1)
+        bomb_type[x][y] = 0
 
-def bomb(matrix):
-    N = len(matrix)
-    to_bomb = []
 
-    for i in range(N):
-        for j in range(N):
-            if matrix[i][j] == 1:
-                to_bomb.append((i, j))
-                for d in range(1, 3):
-                    if 0 <= i - d:
-                        to_bomb.append((i - d, j))
-                    if i + d < n:
-                        to_bomb.append((i + d, j))
-            elif matrix[i][j] == 2:
-                to_bomb.extend([
-                    (i, j),
-                    (i - 1, j) if i - 1 >= 0 else None,
-                    (i + 1, j) if i + 1 < n else None,
-                    (i, j - 1) if j - 1 >= 0 else None,
-                    (i, j + 1) if j + 1 < n else None
-                ])
-            elif matrix[i][j] == 3:
-                to_bomb.extend([
-                    (i, j),
-                    (i - 1, j - 1) if i - 1 >= 0 and j - 1 >= 0 else None,
-                    (i - 1, j + 1) if i - 1 >= 0 and j + 1 < n else None,
-                    (i + 1, j - 1) if i + 1 < n and j - 1 >= 0 else None,
-                    (i + 1, j + 1) if i + 1 < n and j + 1 < n else None
-                ])
+def fill_grid(i, j, k):
+    if k == 1:
+            dis, djs = [-2, -1, 1, 2], [0, 0, 0, 0]
+    if k == 2:
+            dis, djs = [-1, 1, 0, 0], [0, 0, -1, 1]
+    if k == 3:
+            dis, djs = [-1, -1, 1, 1], [-1, 1, -1, 1]
+    visited[i][j] = True
+    for di, dj in zip(dis, djs):
+        new_i, new_j = i + di, j + dj
+        if in_range(new_i, new_j):
+            visited[new_i][new_j] = True
 
-    for pos in to_bomb:
-        if pos:
-            x, y = pos
-            matrix[x][y] = -1
+choose(0)
 
-def count(matrix):
-    return sum(row.count(-1) for row in matrix)
-
-execute(0, grid)
-print(max_bombs)
+print(max_cnt)
