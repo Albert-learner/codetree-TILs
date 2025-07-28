7c23ac85ad725c2f064ae0f22480f83b25ec99df
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-07-28 18:03:39 +0900

    Title: 외판원 순회, Time: 271ms, Memory: 23MB, Status: Passed - Codetree

--- 250728/외판원 순회/traveling-salesman-problem.py
+++ 250728/외판원 순회/traveling-salesman-problem.py
@@ -2,26 +2,37 @@ n = int(input())
 A = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-import sys
-from itertools import permutations
+def solve_min_hamiltonian_cost(n, cost):
+    visited = [False] * n
+    x_pos = []
+    y_pos = []
+    ans = [float('inf')]
 
-n_lst = [i for i in range(2, n + 1)]
-perms_lst = []
-min_cst = sys.maxsize
-for perm in permutations(n_lst):
-    path = [1] + list(perm) + [1]
-    perms_lst.append(path)
+    def calc_min_sum(curX):
+        total = 0
+        for i in range(n - 1):
+            total += cost[x_pos[i]][y_pos[i]]
+        if cost[curX][0] == 0:
+            return
+        total += cost[curX][0]
+        ans[0] = min(ans[0], total)
 
-perms_pairs_lst = []
-for perm_lst in perms_lst:
-    perm_pair = [(perm_lst[i], perm_lst[i + 1]) for i in range(len(perm_lst) - 1)]
-    perms_pairs_lst.append(perm_pair)
+    def find_min(cnt, curX):
+        if cnt == n - 1:
+            calc_min_sum(curX)
+            return
+        for i in range(n):
+            if visited[i] or cost[curX][i] == 0:
+                continue
+            visited[curX] = True
+            x_pos.append(curX)
+            y_pos.append(i)
+            find_min(cnt + 1, i)
+            x_pos.pop()
+            y_pos.pop()
+            visited[curX] = False
 
-for perm_pair in perms_pairs_lst:
-    cost = 0
-    for y, x in perm_pair:
-        cost += A[y - 1][x - 1]
+    find_min(0, 0)
+    return ans[0]
 
-    min_cst = min(min_cst, cost)
-
-print(min_cst)
+print(solve_min_hamiltonian_cost(n, A))
