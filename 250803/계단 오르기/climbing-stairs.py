f36e8e8840020e7d1577d0c5faeb3a4138437c19
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-08-03 16:49:06 +0900

    Title: 계단 오르기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250803/계단 오르기/climbing-stairs.py
+++ 250803/계단 오르기/climbing-stairs.py
@@ -1,9 +1,10 @@
 n = int(input())
 
 # Please write your code here.
-climb_stairs = [0, 1, 1] + [0] * 1000
+climb_stairs = [0] * (n + 1)
+climb_stairs[0], climb_stairs[1], climb_stairs[2] = 0, 1, 1
 
-for i in range(3, n):
+for i in range(3, n + 1):
     climb_stairs[i] = climb_stairs[i - 2] + climb_stairs[i - 3]
 
-print(climb_stairs[n - 1])
+print(climb_stairs[n - 1] % 10007)
