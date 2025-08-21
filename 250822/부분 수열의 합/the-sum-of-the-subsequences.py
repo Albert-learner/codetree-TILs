47a925b1300da39948bb30d45f1dae367d5f501c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-08-22 08:48:13 +0900

    Title: 부분 수열의 합, Time: 54ms, Memory: 17MB, Status: Passed - Codetree

--- 250822/부분 수열의 합/the-sum-of-the-subsequences.py
+++ 250822/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -2,13 +2,12 @@ n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
 # Please write your code here.
-INT_MAX = 10 ** 18
-dp = [INT_MAX] * (m + 1)
-dp[0] = 0
+dp = [False] * (m + 1)
+dp[0] = True
 
 for num in A:
-    for j in range(m, num - 1, -1):
-        if dp[j - num] != INT_MAX:
-            dp[j] = min(dp[j], dp[j - num] + 1)
+    for s in range(m, num - 1, -1):
+        if dp[s - num]:
+            dp[s] = True
 
 print("Yes" if dp[m] else "No")
