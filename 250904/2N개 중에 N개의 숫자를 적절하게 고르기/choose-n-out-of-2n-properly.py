0459772fb0fbe15494eee9230ce831f5e3ba6826
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-04 09:21:39 +0900

    Title: 2N개 중에 N개의 숫자를 적절하게 고르기, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 250904/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -2,22 +2,9 @@ n = int(input())
 num = list(map(int, input().split()))
 
 # Please write your code here.
-import sys
 from itertools import combinations
 
-half = len(num) // 2
-min_sum_diff = sys.maxsize
-combs_lst = list(combinations(num, half))
+total = sum(num)
 
-half_combs = []
-seen = set()
-for comb in combs_lst:
-    left = list(comb)
-    right = [x for x in num if x not in left]
-    key = tuple(sorted(left))
-    if key not in seen:
-        half_combs.append([left, right])
-        seen.add(key)
-
-half_combs.sort(key=lambda x: abs(sum(x[0]) - sum(x[1])))
-print(int(abs(sum(half_combs[0][1]) - sum(half_combs[0][0]))))
+min_sum_diff = min(abs(total - 2 * sum(comb)) for comb in combinations(num, n))
+print(min_sum_diff)
