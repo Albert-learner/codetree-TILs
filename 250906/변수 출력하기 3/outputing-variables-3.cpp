91dfc91395acd3a0f35537b479ce3fe827cea2fa
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-06 21:41:56 +0900

    Title: 변수 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250906/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b;
+    char c;
+
+    a = 1;
+    b = 2;
+    c = 'C';
+
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
