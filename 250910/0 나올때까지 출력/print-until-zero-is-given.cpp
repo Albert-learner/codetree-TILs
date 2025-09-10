a193a051b433bc5b4c5a255d9b751f557905c8c8
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-10 16:52:07 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250910/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int num;
+    cin >> num;
+
+    while(num != 0)
+    {
+        cout << num << endl;
+
+        cin >> num;
+    }
+    return 0;
+}
