0cfe8407143892ef04c49791cbc01812b05e8c57
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-20 22:36:47 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    char chr;
+    cin >> chr;
+
+    for(int i = 0; i < 8; i++)
+        cout << chr;
+    return 0;
+}
