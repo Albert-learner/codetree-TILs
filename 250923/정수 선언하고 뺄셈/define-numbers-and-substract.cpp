383e6add6273f7928274eaebf6ecd10a285a6b0c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-23 08:11:26 +0900

    Title: 정수 선언하고 뺄셈, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250923/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int A = 97, B = 13;
+
+    cout << A << " - " << B << " = " << A - B;
+    return 0;
+}
