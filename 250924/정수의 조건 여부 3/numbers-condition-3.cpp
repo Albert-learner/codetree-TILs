b00b4c1abb114eabc18c77d57782fd5ff6bd5b7b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-24 15:24:58 +0900

    Title: 정수의 조건 여부 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250924/정수의 조건 여부 3/numbers-condition-3.cpp
+++ 250924/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -8,8 +8,8 @@ int main()
     cin >> a;
 
     if(a % 13 == 0 || a % 19 == 0)
-        cout << True;
+        cout << true;
     else
-        cout << False;
+        cout << false;
     return 0;
 }
