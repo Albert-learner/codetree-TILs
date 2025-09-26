05b86e7c81976efde39f9c61f581c8fd9ee03c50
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-26 14:12:25 +0900

    Title: 알바로 부자 되기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250926/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -0,0 +1,20 @@
+n = int(input())
+jobs = [tuple(map(int, input().split())) for _ in range(n)]
+s = [job[0] for job in jobs]
+e = [job[1] for job in jobs]
+p = [job[2] for job in jobs]
+
+# Please write your code here.
+from bisect import bisect_left
+
+dp = [0] * (n + 1)
+ends = e[:]
+
+for i in range(1, n + 1):
+    si, pi = s[i - 1], p[i - 1]
+    j = bisect_left(ends, si) - 1
+    take = pi + (dp[j + 1] if j >= 0 else 0)
+    skip = dp[i - 1]
+    dp[i] = max(take, skip)
+
+print(dp[n])
