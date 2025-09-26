67b118b3005939b3dcd433b7d78285a958a4c4f2
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-09-26 13:22:55 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(6);
+
+    cout << 13 << " * " << 0.165000 << " = " << 13 * 0.165000;
+    return 0;
+}
