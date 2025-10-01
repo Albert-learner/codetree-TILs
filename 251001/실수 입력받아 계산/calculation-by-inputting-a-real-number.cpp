6ece57f04170ed53bfecf2e1a62b15285fa4a232
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-01 10:43:26 +0900

    Title: 실수 입력받아 계산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251001/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    float a, b;
+    cin >> a;
+    cin >> b;
+
+    cout << fixed << setprecision(2) << a + b;
+    return 0;
+}
