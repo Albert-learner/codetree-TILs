e5d0d21c94d962e6e15a8c831c403f213543c7ed
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-03 18:26:49 +0900

    Title: 1시간 뒤 시간 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251003/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int h, m;
+    char colon;
+
+    cin >> h >> colon >> m;
+    cout << h + 1 << colon << m;
+    return 0;
+}
