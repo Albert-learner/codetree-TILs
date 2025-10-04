7b455e9d5e503537e89345a0e492120ff2822e87
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-04 22:48:10 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251004/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int d, m, y;
+    char hypun;
+
+    cin >> m >> hypun >> d >> hypun >> y;
+    cout << y << '.' << m << '.' << d;
+    return 0;
+}
