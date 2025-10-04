0c94f42b984e8db6f20d360fd318d5c13021b626
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-04 22:49:34 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251004/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int front, rear;
+    char hypun;
+
+    cin >> front >> hypun >> rear;
+    cout << front << rear;
+    return 0;
+}
