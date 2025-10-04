7e7b69dd62f12704a28b39955a24cb621f339868
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-04 23:07:59 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251004/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int format, front, rear;
+    char hypun;
+
+    cin >> format >> hypun >> front >> hypun >> rear;
+    cout << '0' << format << hypun << rear << hypun << front;
+    return 0;
+}
