3e9fd961678595472996f797b1b736bcd3c4e22e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-08 12:52:24 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251008/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int a;
+    char result;
+    cin >> a;
+
+    result = (a == 1) ? 't' : 'f';
+    cout << result;
+    return 0;
+}
