4c9372a3de8ebabc8dab092b264404c86a808d0f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-08 12:42:51 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251008/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int score;
+    string result;
+    cin >> score;
+
+    result = (score == 100) ? "pass" : "Failure";
+    cout << result;
+    return 0;
+}
