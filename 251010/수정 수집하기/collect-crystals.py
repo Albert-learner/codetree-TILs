aa4b3de7e17b883140c1748f7c8854819350bb91
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-10 15:05:37 +0900

    Title: 수정 수집하기, Time: 91ms, Memory: 21MB, Status: Passed - Codetree

--- 251010/수정 수집하기/collect-crystals.py
+++ 251010/수정 수집하기/collect-crystals.py
@@ -2,19 +2,23 @@ n, k = map(int, input().split())
 inp_str = input()
 
 # Please write your code here.
-dp = [[[0] * 2 for _ in range(k + 1)] for _ in range(n)]
-dp[0][0][0] = 1 if inp_str[0] == 'L' else 0
-dp[0][0][1] = 1 if inp_str[0] == 'R' else 0
+NEG = -10 ** 9
+dp = [[[NEG] * 2 for _ in range(k + 1)] for _ in range(n)]
 
+dp[0][0][0] = 1 if inp_str[0] == 'L' else 0 
+dp[0][0][1] = NEG                              
+if k >= 1:
+    dp[0][1][1] = 1 if inp_str[0] == 'R' else 0     
+    
 for i in range(1, n):
-    for j in range(k + 1):
+    for j in range(k+1):
         stay_left = dp[i - 1][j][0]
-        move_right_to_left = dp[i - 1][j - 1][1] if j > 0 else 0
-        dp[i][j][0] = max(stay_left, move_right_to_left) + (1 if inp_str[i] == 'L' else 0)
+        move_to_left = dp[i - 1][j - 1][1] if j > 0 else NEG
+        dp[i][j][0] = max(stay_left, move_to_left) + (1 if inp_str[i] == 'L' else 0)
 
         stay_right = dp[i - 1][j][1]
-        move_left_to_right = dp[i - 1][j - 1][0] if j > 0 else 0
-        dp[i][j][1] = max(stay_right, move_left_to_right) + (1 if inp_str[i] == 'R' else 0)
+        move_to_right = dp[i - 1][j - 1][0] if j > 0 else NEG
+        dp[i][j][1] = max(stay_right, move_to_right) + (1 if inp_str[i] == 'R' else 0)
 
-answer = max(dp[n - 1][j][p] for j in range(k + 1) for p in range(2))
-print(answer)
+ans = max(dp[n-1][j][p] for j in range(k+1) for p in range(2))
+print(ans)
