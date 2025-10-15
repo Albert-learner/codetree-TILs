387c753b28c62a922f97141c1cfc2d955d6796c5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-15 10:33:40 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251015/두 사람/two-person.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int f_a, s_a;
+    char f_g, s_g;
+    cin >> f_a >> f_g;
+    cin >> s_a >> s_g;
+
+    if((f_a >= 19 || s_a >= 19) || (f_g == 'M' || s_g == 'M'))
+        cout << 1;
+    else
+        cout << 0;
+        
+    return 0;
+}
