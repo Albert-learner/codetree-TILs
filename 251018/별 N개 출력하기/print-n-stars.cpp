bdf54f5b45fc9bbd50dc26165b46140550ef0d4b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-18 13:08:01 +0900

    Title: 별 N개 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251018/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    while(n > 0)
+    {
+        cout << '*' << endl;
+        n -= 1;
+    }
+    
+    return 0;
+}
