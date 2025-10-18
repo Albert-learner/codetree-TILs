b8b7d50380f6124ca6ddbb499b0b2d69a395e73c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-18 12:54:09 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251018/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int n, i = 1;
+    cin >> n;
+
+    while(i <= n)
+    {
+        cout << i << ' ';
+        i += 1;
+    }
+    
+    return 0;
+}
