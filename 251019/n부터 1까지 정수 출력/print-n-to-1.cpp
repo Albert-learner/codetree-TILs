4e87c9d444c474ea9c6b81ad28234b738a9a4a84
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-19 17:34:11 +0900

    Title: n부터 1까지 정수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251019/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    while(n >= 1)
+    {
+        cout << n << ' ';
+        n -= 1;
+    }
+    
+    return 0;
+}
