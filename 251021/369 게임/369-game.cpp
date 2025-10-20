f2e8c633cbefed75b71e62c14f6d91a1e12b1769
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-21 08:47:16 +0900

    Title: 369 게임, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251021/369 게임/369-game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        string s = to_string(i);
+        if(i % 3 == 0 || s.find('3') != string::npos || s.find('6') != string::npos || s.find('9') != string::npos)
+            cout << 0 << ' ';
+        else
+            cout << i << ' ';
+    }
+
+    return 0;
+}
