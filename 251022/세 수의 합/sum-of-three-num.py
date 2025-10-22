24fc56befef3f9e360ee027a6d45987d01eb0f2d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-22 09:24:41 +0900

    Title: 세 수의 합, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 251022/세 수의 합/sum-of-three-num.py
+++ 251022/세 수의 합/sum-of-three-num.py
@@ -9,7 +9,23 @@ for i in range(n - 2):
     while l < r:
         s = arr[i] + arr[l] + arr[r]
         if s == k:
-            ans += 1
+            if arr[l] != arr[r]:
+                vL, vR = arr[l], arr[r]
+                cntL = 1
+                while l + cntL < r and arr[l + cntL] == vL:
+                    cntL += 1
+                cntR = 1
+                while r - cntR > l and arr[r - cntR] == vR:
+                    cntR += 1
+                ans += cntL *cntR
+                l += cntL
+                r -= cntR
+            else:
+                m = r - l + 1
+                ans += m * (m - 1) // 2
+                break
+
+        elif s < k:
             l += 1
         else:
             r -= 1
