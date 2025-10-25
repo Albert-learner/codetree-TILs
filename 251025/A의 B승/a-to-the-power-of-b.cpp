fd5880e1cfa6ada196cce14c2c67dc2159a3bc38
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-25 14:58:35 +0900

    Title: A의 B승, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251025/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    cout << int(pow(a, b));
+    return 0;
+}
