e30f56a743c4409c6c98d722a358329bc533eec0
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-25 14:54:09 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 244ms, Memory: 21MB, Status: Passed - Codetree

--- 251025/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 251025/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -4,7 +4,8 @@ words = [input() for _ in range(n)]
 # Please write your code here.
 from collections import Counter
 
-words_sort_set = [tuple(sorted(set(word))) for word in words]
-words_sort_set_cntr = Counter(words_sort_set)
+keys = ("".join(sorted(w)) for w in words)
 
-print(max(words_sort_set_cntr.values()))
+cnt = Counter(keys)
+
+print(max(cnt.values()) if cnt else 0)
