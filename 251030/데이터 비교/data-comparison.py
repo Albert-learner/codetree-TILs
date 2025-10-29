34c23d97654f5efb8dd35929f5971667a1ac14c4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-10-30 08:34:22 +0900

    Title: 데이터 비교, Time: 105ms, Memory: 30MB, Status: Passed - Codetree

--- 251030/데이터 비교/data-comparison.py
+++ 251030/데이터 비교/data-comparison.py
@@ -5,10 +5,7 @@ m = int(input())
 arr2 = list(map(int, input().split()))
 
 # Please write your code here.
-arr1_lst = list(set(arr1))
+arr1_set = set(arr1)
 
 for num in arr2:
-    if num in arr1_lst:
-        print(1, end = ' ')
-    else:
-        print(0, end = ' ')
+    print(1 if num in arr1_set else 0, end = ' ')
