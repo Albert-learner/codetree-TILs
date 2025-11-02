c564b82af5be73abdeefeb6b8612f9ca72b5fa1e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-02 14:30:23 +0900

    Title: 별표 출력하기 11, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251102/별표 출력하기 11/print-star-11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i = 1; i < 2 * n + 2; i++)
+    {
+        for(int j = 1; j < 2 * n + 2; j++)
+        {
+            if(i % 2 == 1 || j % 2 == 1)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
