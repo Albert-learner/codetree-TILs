716015f7a845dff8a74ed6abdedfc78f15a8cd75
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-05 09:10:40 +0900

    Title: 행에 따라 다르게 출력, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251105/행에 따라 다르게 출력/output-defferently-each-row.cpp
+++ 251105/행에 따라 다르게 출력/output-defferently-each-row.cpp
@@ -2,31 +2,44 @@
 #include <vector>
 using namespace std;
 
-int main() 
-{
-    // Please write your code here.
-    int n, cnts = 1;
-    cin >> n;
+int main() {
+    int N;
+    cin >> N;
 
-    vector<vector<int>> grid(n);
-    for(int i = 0; i < n; i++)
+    vector<vector<int>> grid(N);
+    int cnts = 1;
+
+    for (int i = 0; i < N; i++) 
     {
-        if(i % 2 == 0)
+        if (i % 2 == 0) 
         {
-            for(int j = 0; j < n; j++)
+
+            for (int j = 0; j < N; j++) 
             {
                 grid[i].push_back(cnts + j);
             }
             cnts = grid[i].back() + 2;
-        }
-        else
+        } 
+        else 
         {
-            for(int j = 0; j < n * 2; j += 2)
+            for (int j = 0; j < N * 2; j += 2) 
             {
                 grid[i].push_back(cnts + j);
             }
             cnts = grid[i].back() + 1;
         }
     }
+
+    // 결과 출력
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < N; j++) 
+        {
+            cout << grid[i][j];
+            if (j != N - 1) cout << " ";
+        }
+        cout << "\n";
+    }
+
     return 0;
 }
