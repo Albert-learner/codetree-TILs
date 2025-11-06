a8d7e7efa142db26459659dd6718898dea4ab79c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-06 09:31:33 +0900

    Title: 가까운 숫자, Time: 863ms, Memory: 32MB, Status: Passed - Codetree

--- 251106/가까운 숫자/nearest-number.py
+++ 251106/가까운 숫자/nearest-number.py
@@ -1,25 +1,30 @@
+from sortedcontainers import SortedSet
+import sys
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력:
 n = int(input())
 queries = list(map(int, input().split()))
 
-# Please write your code here.
-from sortedcontainers import SortedList
+s = SortedSet()
+ans = INT_MAX
 
-points = SortedList([0])      
-gaps = SortedList()           
+# x = 0 위치에 점을 놓고 시작합니다.
+s.add(0)
+
 for x in queries:
-    i = points.bisect_left(x)
+    # 가장 근처에 있는 오른쪽 점을 찾습니다.
+    idx = s.bisect_right(x)
+    # 존재한다면, 거리 중 최솟값을 갱신합니다.
+    if idx != len(s):
+        ans = min(ans, s[idx] - x)
+    
+    # 가장 근처에 있는 왼쪽 점을 찾습니다.
+    idx -= 1
+    # 거리 중 최솟값을 갱신합니다.
+    ans = min(ans, x - s[idx])
 
-    if i > 0:
-        left = points[i-1]
-        gaps.add(x - left)
-    if i < len(points):
-        right = points[i]
-        gaps.add(right - x)
-
-
-    if 0 < i < len(points):
-        gaps.remove(right - left) 
-
-    points.add(x)
-
-    print(gaps[0])
+    # 해당 점을 treeset에 추가합니다.
+    s.add(x)
+    print(ans)
