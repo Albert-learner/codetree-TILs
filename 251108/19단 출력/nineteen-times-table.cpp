e5f60f77a118da599a47b1837326c20388146e01
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-08 23:00:21 +0900

    Title: 19단 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251108/19단 출력/nineteen-times-table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    for(int i = 1; i < 20; i++)
+    {
+        for(int j = 1; j < 20; j++)
+        {
+            if(j % 2 == 0 or j == 19)
+                cout << "(" << i << " * " << j << " = " << i * j << endl;
+            else
+                cout << "(" << i << " * " << j << " = " << i * j << " / ";
+        }
+    }
+    return 0;
+}
