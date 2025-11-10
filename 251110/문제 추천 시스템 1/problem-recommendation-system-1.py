572717e85e76e75f899d1a32da209fa71c273ae7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-10 09:00:23 +0900

    Title: 문제 추천 시스템 1, Time: 766ms, Memory: 25MB, Status: Passed - Codetree

--- 251110/문제 추천 시스템 1/problem-recommendation-system-1.py
+++ 251110/문제 추천 시스템 1/problem-recommendation-system-1.py
@@ -1,29 +1,17 @@
-n = int(input())
-P, L = [], []
-for _ in range(n):
-    p, l = map(int, input().split())
-    P.append(p)
-    L.append(l)
-
-m = int(input())
-commands = []
-for _ in range(m):
-    cmd = input().split()
-    if cmd[0] == "rc":
-        commands.append((cmd[0], int(cmd[1])))
-    else:
-        commands.append((cmd[0], int(cmd[1]), int(cmd[2])))
-
-# Please write your code here.
 import bisect
 
-matters = [(l, p) for p, l in zip(P, L)]
-bisect.insort(matters, (l, p))
-
-for line in commands:
-    if line[0] == "ad":
-        bisect.insort(matters, (line[2], line[1]))
-    elif line[0] == "sv":
-        matters.remove((line[2], line[1]))
-    else:
-        print(matters[0][1] if line[1] == -1 else matters[-1][1])
+n = int(input())
+matters = []
+for i in range(n):
+    p, l = map(int, input().split())
+    bisect.insort(matters, (l, p))
+m = int(input())
+for i in range(m):
+    function = list(map(str, input().split()))
+    if function[0] == "ad":  
+        bisect.insort(matters, (int(function[2]), int(function[1])))
+    elif function[0] == "sv": 
+        matters.remove((int(function[2]), int(function[1])))
+    else:  
+        x = int(function[1])
+        print(matters[0][1]) if x == -1 else print(matters[-1][1])
