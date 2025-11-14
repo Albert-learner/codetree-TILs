d92b66cc05af3fb033e008d09dc3f78939e5b3c4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-14 15:49:08 +0900

    Title: 짝수인 것만 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251114/짝수인 것만 출력하기/print-even-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(nums[i] % 2 == 0)
+            cout << nums[i] << ' ';
+    }
+    return 0;
+}
