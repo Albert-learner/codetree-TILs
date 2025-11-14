afa935e5797fdc681a6b952c748b4bdcadcccd6e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-14 14:06:10 +0900

    Title: 큰 숫자만 계속 고르기, Time: 1082ms, Memory: 30MB, Status: Time Limit Exceed - Codetree

--- 251114/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
+++ 251114/큰 숫자만 계속 고르기/keep-picking-the-big-number.py
@@ -2,10 +2,14 @@ n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
+from collections import deque
+
+arr = sorted(deque(arr))
 while m > 0:
-    max_cst, max_cst_idx = max(arr), arr.index(max(arr))
+    max_cst, max_cst_idx = arr[-1], -1
     arr[max_cst_idx] -= 1
 
+    arr = sorted(arr)
     m -= 1
 
 print(max(arr))
