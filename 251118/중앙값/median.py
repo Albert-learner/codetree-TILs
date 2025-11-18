a76e9184fb5cf521b177a671d273ed3d0468467f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-18 19:52:42 +0900

    Title: 중앙값, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251118/중앙값/median.py
@@ -0,0 +1,15 @@
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    arr = list(map(int, input().split()))
+
+    # Please write your code here.
+    queue = []
+    for idx, elem in enumerate(arr, 1):
+        if idx % 2 == 1:
+            queue.append(elem)
+            print(queue[len(queue) // 2], end=' ')
+        else:
+            queue.append(elem)
+
+    print()
