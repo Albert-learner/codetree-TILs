6c831c57ac75532a20f1cd6d2baca010c95213cf
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-18 20:24:52 +0900

    Title: 중앙값, Time: 292ms, Memory: 30MB, Status: Passed - Codetree

--- 251118/중앙값/median.py
+++ 251118/중앙값/median.py
@@ -1,16 +1,30 @@
+import heapq
+
 t = int(input())
 for _ in range(t):
     m = int(input())
     arr = list(map(int, input().split()))
 
     # Please write your code here.
-    queue = []
-    for idx, elem in enumerate(arr, 1):
-        if idx % 2 == 1:
-            queue.append(elem)
-            print(queue[len(queue) // 2], end=' ')
-        else:
-            queue.append(elem)
+    left = []   
+    right = []  
 
-    queue.sort()
-    print()
+    medians = []
+
+    for i, x in enumerate(arr, 1):
+        if not left or x <= -left[0]:
+            heapq.heappush(left, -x)
+        else:
+            heapq.heappush(right, x)
+
+        if len(left) > len(right) + 1:
+            val = -heapq.heappop(left)
+            heapq.heappush(right, val)
+        elif len(right) > len(left):
+            val = heapq.heappop(right)
+            heapq.heappush(left, -val)
+
+        if i % 2 == 1:
+            medians.append(-left[0])
+
+    print(*medians)
