b240adf9fa74796f6207c29ef0afaee5eba5092f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-18 21:27:11 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251118/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
+++ 251118/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -4,25 +4,24 @@ using namespace std;
 
 int main() 
 {
-    // Please write your code here.
-    int N, t_cnts = 0, t_idx = 0;
+    int N;
     cin >> N;
-    vector<int> nums(N);
 
-    for(int i = 0; i < N; i++)
+    vector<int> n_lst(N);
+    for (int i = 0; i < N; i++) {
+        cin >> n_lst[i];
+    }
+
+    vector<int> twos;
+    for (int i = 0; i < N; i++) 
     {
-        cin >> nums[i];
-        if(nums[i] == 2)
+        if (n_lst[i] == 2) 
         {
-            if(t_cnts != 3)
-                t_cnts += 1;
-            else
-            {
-                t_idx = i + 1;
-                break;
-            }
+            twos.push_back(i + 1); 
         }
     }
-    cout << t_idx;
+
+    cout << twos[2] << '\n'; 
+
     return 0;
 }
