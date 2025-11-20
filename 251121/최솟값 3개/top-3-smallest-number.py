972ebf34fa2c8a39308c41302b37074b92405c89
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-21 08:17:28 +0900

    Title: 최솟값 3개, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251121/최솟값 3개/top-3-smallest-number.py
+++ 251121/최솟값 3개/top-3-smallest-number.py
@@ -5,14 +5,14 @@ arr = list(map(int, input().split()))
 import heapq
 
 pq = []
+mul = 1
 for elem in arr:
     heapq.heappush(pq, elem)
 
     if len(pq) < 3:
         print(-1)
     else:
-        mul = 1
         for sub_elem in pq[:3]:
             mul *= sub_elem
 
-        print(mul)
+    print(mul)
