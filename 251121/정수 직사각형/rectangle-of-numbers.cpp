bca4aff55e9fea3ff38fc8cdc16854ec8de91d54
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-21 14:46:38 +0900

    Title: 정수 직사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251121/정수 직사각형/rectangle-of-numbers.cpp
+++ 251121/정수 직사각형/rectangle-of-numbers.cpp
@@ -4,26 +4,30 @@ using namespace std;
 
 int main() 
 {
-    // Please write your code here.
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> board(M, vector<int>(N));
-    for(int i = 0; i < N; i++)
+    vector<vector<int>> board(N, vector<int>(M));
+
+    // 값 채우기
+    for (int i = 0; i < N; i++) 
     {
-        for(int j = 1; j < M + 1; j++)
+        for (int j = 1; j <= M; j++) 
         {
             board[i][j - 1] = i * M + j;
         }
     }
 
-    for(int i = 0; i < N; i++)
+
+    for (int i = 0; i < N; i++) 
     {
-        for(int j = 0; j < M; j++)
+        for (int j = 0; j < M; j++) 
         {
-            cout << board[i][j] << ' ';
+            cout << board[i][j];
+            if (j + 1 < M) cout << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
+
     return 0;
 }
