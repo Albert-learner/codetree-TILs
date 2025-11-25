c472ee94bb4170669473b38cd8441c88e5121eef
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-25 09:35:40 +0900

    Title: 격자로 사각형 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251125/격자로 사각형 만들기/print-grid-in-rectangle.cpp
+++ 251125/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -4,35 +4,39 @@ using namespace std;
 
 int main() 
 {
-    // Please write your code here.
     int N;
     cin >> N;
-    vector<vector<int>> board(N, vector<int>(N, 0));
 
-    for(int i = 0; i < N; i++)
+    vector<vector<long long>> board(N, vector<long long>(N, 0));
+
+    for (int i = 0; i < N; i++) 
     {
-        for(int j = 0; j < N; j++)
+        for (int j = 0; j < N; j++) 
         {
-            if(i == 0 || j == 0)
+            if (i == 0 || j == 0) 
+            {
                 board[i][j] = 1;
+            }
         }
     }
 
-    for(int i = 1; i < N; i++)
+    for (int i = 1; i < N; i++) 
     {
-        for(int j = 1; j < N; j++)
+        for (int j = 1; j < N; j++) 
         {
-            board[i][j] = board[i - 1][j - 1] + board[i][j - 1] + board[i - 1][j]
+            board[i][j] = board[i - 1][j - 1] + board[i][j - 1] + board[i - 1][j];
         }
     }
 
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++) 
     {
-        for(int j = 0; j < N; j++)
+        for (int j = 0; j < N; j++) 
         {
-            cout << board[i][j] << ' ';
+            cout << board[i][j];
+            if (j + 1 < N) cout << ' ';
         }
-        cout << endl;
+        cout << '\n';
     }
+
     return 0;
 }
