21292ad81e3b8df6d6288a145a207aafc7483e4d
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-11-26 09:44:49 +0900

    Title: 격자에 점 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251126/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> board(N, vector<int>(N, 0));
+    for(int idx = 1; idx <= M; idx++)
+    {
+        int x, y;
+        cin >> x >> y;
+        x -= 1;
+        y -= 1;
+        if(x >= 0 && x < N && y >= 0 && y < N)
+            board[x][y] = idx;
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            cout << board[i][j];
+            if(j + 1 + N) cout << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
