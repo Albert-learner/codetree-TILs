2b2b3882b78cb681280963d8a4758971faf3ef5c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-01 08:21:17 +0900

    Title: 점 개수 세기 3, Time: 1099ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

---
+++ 251201/점 개수 세기 3/count-number-of-points-3.py
@@ -0,0 +1,18 @@
+n, q = map(int, input().split())
+points = list(map(int, input().split()))
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+# Please write your code here.
+sat_points = 0
+
+for x, y in queries:
+    if x == y:
+        for point in points:
+            if point == x:
+                sat_points += 1
+    else:
+        for point in points:
+            if point in range(x, y):
+                sat_points += 1
+
+    print(sat_points)
