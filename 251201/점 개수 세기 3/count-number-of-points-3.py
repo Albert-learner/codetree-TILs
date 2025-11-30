e5c31c4fa4391ccd0e737b94d606da59245d8ce4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-01 08:36:59 +0900

    Title: 점 개수 세기 3, Time: 247ms, Memory: 29MB, Status: Passed - Codetree

--- 251201/점 개수 세기 3/count-number-of-points-3.py
+++ 251201/점 개수 세기 3/count-number-of-points-3.py
@@ -1,15 +1,29 @@
-n, q = map(int, input().split())
-points = list(map(int, input().split()))
-queries = [tuple(map(int, input().split())) for _ in range(q)]
+from sortedcontainers import SortedSet
 
-# Please write your code here.
-import bisect
+# 변수 선언 및 입력:
+n, q = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+queries = [
+    tuple(map(int, input().split()))
+    for _ in range(q)
+]
 
-points.sort()
+# 주어진 x좌표값들을 전부 treeset에 넣어줍니다.
+nums = SortedSet(arr)
 
-for x, y in queries:
-    left = bisect.bisect_left(points, x)
-    right = bisect.bisect_right(points, y)
+# treeset에서 정점을 작은 번호부터 뽑으면서
+# 각 정점별로 1번부터 순서대로 매칭하여
+# 그 결과를 hashmap에 넣어줍니다.
+mapper = dict()
+cnt = 1
+for num in nums:
+    mapper[num] = cnt
+    cnt += 1
 
-    sat_points = right - left
-    print(sat_points)
+# 질의에 대해
+# 각 [a, b]에 해당하는 번호를
+# mapper를 통해 구해
+# 두 번호 사이의 점의 수를 출력합니다.
+for a, b in queries:
+    new_a, new_b = mapper[a], mapper[b]
+    print(new_b - new_a + 1)
