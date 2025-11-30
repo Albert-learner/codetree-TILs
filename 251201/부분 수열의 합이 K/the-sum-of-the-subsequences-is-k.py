91142cdeb3d7446fa9546966fb2a3de37fbdeeb3
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-01 08:14:39 +0900

    Title: 부분 수열의 합이 K, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251201/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+# Please write your code here.
+import sys
+
+prefix_sum = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+ans = 0
+nums = set(prefix_sum)
+for num in nums:
+    if (num - k) in nums:
+        ans += 1
+
+print(ans)
