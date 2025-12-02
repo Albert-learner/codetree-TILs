36c52355f7f8639be3c5c988b3336cd48ced668a
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-02 09:03:36 +0900

    Title: 문자열 추가하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251202/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    string input_str;
+    getline(cin, input_str);
+
+    cout << input_str << "Hello";
+    return 0;
+}
