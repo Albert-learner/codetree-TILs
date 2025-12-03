2d9e1db7586f4d9a84aa637bf886447ba5eecc73
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-04 08:38:09 +0900

    Title: 최대 직사각형 합, Time: 314ms, Memory: 21MB, Status: Passed - Codetree

--- 251204/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 251204/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -1,22 +1,65 @@
+import sys
+
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력:
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-INF = 10 ** 18
-best = -INF
+arr = [[0] * (n + 1)]
+for _ in range(n):
+    arr.append([0] + list(map(int, input().split())))
 
-for top in range(n):
-    col_sum = [0] * n
+prefix_sum = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
 
-    for bottom in range(top, n):
-        for c in range(n):
-            col_sum[c] += arr[bottom][c]
 
-        cur_max = col_sum[0]
-        best = max(best, cur_max)
-        for c in range(1, n):
-            cur_max = max(col_sum[c], cur_max + col_sum[c])
-            if cur_max > best:
-                best = cur_max
+# (x1, y1), (x2, y2) 직사각형 구간 내의 원소의 합을 반환합니다.
+def get_sum(x1, y1, x2, y2):
+    return prefix_sum[x2][y2]     - prefix_sum[x1 - 1][y2] - \
+           prefix_sum[x2][y1 - 1] + prefix_sum[x1 - 1][y1 - 1]
 
-print(best)
+
+# 시작행이 x1, 끝 행이 x2인 직사각형 중
+# 가능한 최대 합을 반환합니다.
+def get_max_area(x1, x2):
+    # 시작행과 끝 행이 x1, x2로 정해지면,
+    # 각 열마다 x1부터 x2까지 행에 적혀있는 숫자들을 누적했을 때
+    # 마치 1차원에서 최대 연속 부분 수열의 합을 구하는 문제와 같아집니다.
+    # 이는 동적계획법으로 간단히 해결이 가능합니다.
+    dp = [0] * (n + 1)
+
+    for y in range(1, n + 1):
+        # y열에 있는 숫자들의 합을 구해줍니다.
+        value = get_sum(x1, y, x2, y)
+        dp[y] = max(value, dp[y - 1] + value)
+
+    # dp 값 중 최댓값이 원하는 값이 됩니다.
+    max_area = INT_MIN
+    for y in range(1, n + 1):
+        max_area = max(max_area, dp[y])
+    
+    return max_area
+
+
+# 누적합 배열을 만들어줍니다.
+prefix_sum[0][0] = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix_sum[i][j] = prefix_sum[i - 1][j] + \
+                           prefix_sum[i][j - 1] - \
+                           prefix_sum[i - 1][j - 1] + \
+                           arr[i][j]
+
+# 직사각형의
+# 시작 행과 끝 행을 결정합니다.
+# 각 쌍에 대해 가능한 직사각형 중
+# 최대 합을 계산해
+# 답과 비교하여 최댓값을 갱신해줍니다.
+ans = INT_MIN
+for i in range(1, n + 1):
+    for j in range(i, n + 1):
+        ans = max(ans, get_max_area(i, j))
+
+print(ans)
