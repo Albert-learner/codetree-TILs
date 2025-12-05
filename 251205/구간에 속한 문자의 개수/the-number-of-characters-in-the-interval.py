b532a615dfc24c489bb2169def9170879f0f45f7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-05 12:47:45 +0900

    Title: 구간에 속한 문자의 개수, Time: 386ms, Memory: 46MB, Status: Passed - Codetree

--- 251205/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
+++ 251205/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.py
@@ -7,16 +7,35 @@ grid = [input() for _ in range(n)]
 queries = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
-for r1, c1, r2, c2 in queries:
-    r1, c1 = r1 - 1, c1 - 1
-    alp_cnts = {'a': 0, 'b': 0, 'c': 0}
-    for r in range(r1, r2):
-        for c in range(c1, c2):
-            if grid[r][c] == 'a':
-                alp_cnts['a'] += 1
-            elif grid[r][c] == 'b':
-                alp_cnts['b'] += 1
-            elif grid[r][c] == 'c':
-                alp_cnts['c'] += 1
+pa = [[0] * (m + 1) for _ in range(n + 1)]
+pb = [[0] * (m + 1) for _ in range(n + 1)]
+pc = [[0] * (m + 1) for _ in range(n + 1)]
 
-    print(*alp_cnts.values())
+for i in range(1, n + 1):
+    row = grid[i - 1]
+    for j in range(1, m + 1):
+        ch = row[j - 1]
+        pa[i][j] = pa[i - 1][j] + pa[i][j - 1] - pa[i - 1][j - 1]
+        pb[i][j] = pb[i - 1][j] + pb[i][j - 1] - pb[i - 1][j - 1]
+        pc[i][j] = pc[i - 1][j] + pc[i][j - 1] - pc[i - 1][j - 1]
+
+        if ch == 'a':
+            pa[i][j] += 1
+        elif ch == 'b':
+            pb[i][j] += 1
+        elif ch == 'c':
+            pc[i][j] += 1
+
+def get_sum(pref, r1, c1, r2, c2):
+    return (
+        pref[r2][c2]
+        - pref[r1 - 1][c2]
+        - pref[r2][c1 - 1]
+        + pref[r1 - 1][c1 - 1]
+    )
+
+for r1, c1, r2, c2 in queries:
+    a_cnt = get_sum(pa, r1, c1, r2, c2)
+    b_cnt = get_sum(pb, r1, c1, r2, c2)
+    c_cnt = get_sum(pc, r1, c1, r2, c2)
+    print(a_cnt, b_cnt, c_cnt)
