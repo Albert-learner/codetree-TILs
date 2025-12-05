99d641accbb7cd151c7e532c034fe66c6777a20b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-05 14:18:44 +0900

    Title: 두 번째를 첫 번째로, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251205/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input_str;
+    getline(cin, input_str);
+
+    char from = input_str[1];
+    char to = input_str[0];
+
+    for(char &c : input_str) {
+        if(c == from) c = to;
+    }
+
+    cout << input_str;
+    return 0;
+}
