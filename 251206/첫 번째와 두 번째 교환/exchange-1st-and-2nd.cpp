0f9f1b1e8ec81d6998bb2b967f26492c99357414
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-06 22:43:15 +0900

    Title: 첫 번째와 두 번째 교환, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251206/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string input_str;
+    cin >> input_str;
+
+    if(input_str.length() >= 2)
+    {
+        char temp = input_str[0];
+        input_str[0] = input_str[1];
+        input_str[1] = temp;
+    }
+
+    for(int idx = 2; idx < input_str.length(); idx++)
+    {
+        if(input_str[idx] == input_str[0]) 
+        {
+            input_str[idx] = input_str[1];
+        }
+        else if(input_str[idx] == input_str[1]) 
+        {
+            input_str[idx] = input_str[0];
+        }
+    }
+
+    cout << input_str;
+    return 0;
+}
