89c073da90b330d3167d4fe3a718cb53606e5990
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-08 08:47:25 +0900

    Title: 점 개수 세기 2, Time: 5095ms, Memory: 66MB, Status: Time Limit Exceed - Codetree

--- 251208/점 개수 세기 2/count-number-of-points-2.py
+++ 251208/점 개수 세기 2/count-number-of-points-2.py
@@ -5,12 +5,9 @@ queries = [tuple(map(int, input().split())) for _ in range(q)]
 
 # Please write your code here.
 for x1, y1, x2, y2 in queries:
-    x_set = set(range(x1, x2 + 1))
-    y_set = set(range(y1, y2 + 1))
-
     sat_points = 0
     for x, y in points:
-        if x in x_set and y in y_set:
+        if x1 <= x <= x2 and y1 <= y <= y2:
             sat_points += 1
 
     print(sat_points)
