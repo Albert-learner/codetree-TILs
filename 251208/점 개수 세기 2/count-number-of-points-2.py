7742785b173fa5bb8d6a2fbcb051958adecafc69
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-08 08:28:37 +0900

    Title: 점 개수 세기 2, Time: 192ms, Memory: 27MB, Status: Runtime Error - Codetree

---
+++ 251208/점 개수 세기 2/count-number-of-points-2.py
@@ -0,0 +1,16 @@
+n, q = map(int, input().split())
+
+points = [tuple(map(int, input().split())) for _ in range(n)]
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+
+# Please write your code here.
+for x1, y1, x2, y2 in queries:
+    x_lst = list(range(x1, x2 + 1))
+    y_lst = list(range(y1, y2 + 1))
+
+    sat_points = 0
+    for x, y in points:
+        if x in x_lst and y in y_lst:
+            sat_points += 1
+
+    print(sat_points)
