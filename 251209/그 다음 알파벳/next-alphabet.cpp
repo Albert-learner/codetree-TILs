36f4b1f31300e905301d59c43082d234828230be
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-09 08:39:36 +0900

    Title: 그 다음 알파벳, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251209/그 다음 알파벳/next-alphabet.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    char inp_chr;
+    cin >> inp_chr;
+
+    cout << char((int)inp_chr + 1);
+    return 0;
+}
