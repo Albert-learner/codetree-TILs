2c90808d7b2aa0dd5e40ee50061f58e19fa1fa4a
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-09 08:38:31 +0900

    Title: 아스키코드 변환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int b;
+    char eng;
+    cin >> eng >> b;
+
+    cout << (int)eng << ' ' << (char)b;
+    return 0;
+}
