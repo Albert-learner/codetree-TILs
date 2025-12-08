e18e1d07d2cb2cb348313d51fa6948f8b3cf7644
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-09 08:37:20 +0900

    Title: 아스키코드표 맞추기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int first, second, third, fourth, fifth;
+    cin >> first >> second >> third >> fourth >> fifth;
+
+    cout << (char)first << ' ' << (char)second << ' ' << (char)third << ' ' << (char)fourth << ' ' << (char)fifth;
+    return 0;
+}
