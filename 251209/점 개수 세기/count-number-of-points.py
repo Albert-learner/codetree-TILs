b67ddaeab9a8c3a64eb7c2a866f9ad5540c84638
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-09 08:33:54 +0900

    Title: 점 개수 세기, Time: 240ms, Memory: 34MB, Status: Passed - Codetree

--- 251209/점 개수 세기/count-number-of-points.py
+++ 251209/점 개수 세기/count-number-of-points.py
@@ -1,35 +1,19 @@
-from sortedcontainers import SortedSet
+import sys
+from bisect import bisect_left, bisect_right
+
+input = sys.stdin.readline
 
 # 변수 선언 및 입력:
-n, q = tuple(map(int, input().split()))
+n, q = map(int, input().split())
 arr = list(map(int, input().split()))
-queries = [
-    tuple(map(int, input().split()))
-    for _ in range(q)
-]
+queries = [tuple(map(int, input().split())) for _ in range(q)]
 
-mapper = {}
+# 점들을 정렬합니다.
+# (모든 점이 서로 다른 위치라고 했으니 set은 굳이 안 써도 됨)
+nums = sorted(arr)
 
-# 주어진 x좌표값들을 전부 treeset에 넣어줍니다.
-nums = SortedSet(arr)
-
-# 예외를 처리하기 위해 최댓값보다 큰 값을 treeset에 넣어줍니다.
-nums.add((10 ** 9) + 1)
-
-# treeset에서 정점을 작은 번호부터 뽑으면서
-# 각 정점별로 1번부터 순서대로 매칭하여
-# 그 결과를 hashmap에 넣어줍니다.
-cnt = 1
-for num in nums:
-    mapper[num] = cnt
-    cnt += 1
-
-# 질의에 대해
-# 각 [a, b]에 해당하는 번호를
-# mapper를 통해 구해
-# 두 번호 사이의 점의 수를 출력합니다.
+# 각 질의 [a, b]마다 이분 탐색으로 개수를 구합니다.
 for a, b in queries:
-    new_a = mapper[nums[nums.bisect_left(a)]]
-    new_b = mapper[nums[nums.bisect_right(b)]]
-
-    print(new_b - new_a)
+    left = bisect_left(nums, a)   # x >= a 인 첫 인덱스
+    right = bisect_right(nums, b) # x > b 인 첫 인덱스
+    print(right - left)
