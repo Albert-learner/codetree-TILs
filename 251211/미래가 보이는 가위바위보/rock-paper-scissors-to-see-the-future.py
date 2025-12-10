1a659000b181e41a5ae406b4b47bf63c373d051c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-11 08:51:41 +0900

    Title: 미래가 보이는 가위바위보, Time: 154ms, Memory: 25MB, Status: Passed - Codetree

--- 251211/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.py
+++ 251211/미래가 보이는 가위바위보/rock-paper-scissors-to-see-the-future.py
@@ -1,30 +1,36 @@
-N = int(input())
-B = [input() for _ in range(N)]
+# 변수 선언 및 입력:
+n = int(input())
+matches = [input() for _ in range(n)]
 
-# Please write your code here.
-gests = ["H", "S", "P"]
-
-def win(gest, opp):
-    return (gest == 'H' and opp == 'S') or \
-           (gest == 'S' and opp == 'P') or \
-           (gest == 'P' and opp == 'H')
-
-L = [[0] * 3 for _ in range(N + 1)]
-for i in range(1, N + 1):
-    opp = B[i - 1]
-    for g in range(3):
-        L[i][g] = L[i - 1][g] + (1 if win(gests[g], opp) else 0)
-
-R = [[0] * 3 for _ in range(N + 1)]
-for i in range(N - 1, -1, -1):
-    opp = B[i]
-    for g in range(3):
-        R[i][g] = R[i + 1][g] + (1 if win(gests[g], opp) else 0)
-
+L, R = [0] * n, [0] * n
 ans = 0
-for k in range(N + 1):
-    bestL = max(L[k])
-    bestR = max(R[k])
-    ans = max(ans, bestL + bestR)
+
+# L 배열을 채워줍니다.
+# L[i] = 0번부터 i번까지 동일한 모양만 냈을 때
+#        최대로 이길 수 있는 횟수
+for shape in "PHS":
+    same_cnt = 0
+    for i in range(n):    
+        if matches[i] == shape:
+            same_cnt += 1
+        
+        L[i] = max(L[i], same_cnt)
+
+# R 배열을 채워줍니다.
+# R[i] = i번부터 n - 1번까지 동일한 모양만 냈을 때
+#        최대로 이길 수 있는 횟수
+
+for shape in "PHS":
+    same_cnt = 0
+    for i in range(n - 1, -1, -1):
+        if matches[i] == shape:
+            same_cnt += 1
+        
+        R[i] = max(R[i], same_cnt)
+
+# 해당 순간에 선택을 변경했다 했을 때
+# 최대로 이기는 횟수를 갱신해줍니다.
+for i in range(0, n - 1):
+    ans = max(ans, L[i] + R[i + 1])
 
 print(ans)
