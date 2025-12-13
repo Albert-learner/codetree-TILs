df919f0f83e7922c45b2316e4c1d220fe2ac3902
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-13 12:16:25 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251213/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int N;
+void print_str();
+int main() 
+{
+    cin >> N;
+
+    // Please write your code here.
+    print_str();
+    return 0;
+}
+
+void print_str()
+{
+    for(int i = 0; i < N; i++)
+    {
+        cout << "12345^&*()_" << endl;
+    }
+}
