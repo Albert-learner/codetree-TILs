66fa9e416155fc7d89085522ad8ff3f0c7138902
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-16 08:45:03 +0900

    Title: 두 수의 거듭제곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251216/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int a, b;
+
+int main() 
+{
+    cin >> a >> b;
+
+    // Please write your code here.
+    cout << (int)pow(a, b);
+    return 0;
+}
