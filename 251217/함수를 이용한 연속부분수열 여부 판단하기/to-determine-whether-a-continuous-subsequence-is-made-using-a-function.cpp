26f34fe6a602e13995e8e7e1cfff92bc9f483c41
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-17 08:47:00 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251217/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 251217/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 using namespace std;
 
+vector<int> build_lps(const vector<int>& pat) {
+    int m = (int)pat.size();
+    vector<int> lps(m, 0);
+    for (int i = 1, len = 0; i < m; ) {
+        if (pat[i] == pat[len]) {
+            lps[i++] = ++len;
+        } else if (len > 0) {
+            len = lps[len - 1];
+        } else {
+            lps[i++] = 0;
+        }
+    }
+    return lps;
+}
 
-int main() 
-{
+bool kmp_contains(const vector<int>& text, const vector<int>& pat) {
+    int n = (int)text.size();
+    int m = (int)pat.size();
+    if (m == 0) return true;
+
+    vector<int> lps = build_lps(pat);
+
+    for (int i = 0, j = 0; i < n; ) {
+        if (text[i] == pat[j]) {
+            i++; j++;
+            if (j == m) return true; // 매칭 완료
+        } else if (j > 0) {
+            j = lps[j - 1];
+        } else {
+            i++;
+        }
+    }
+    return false;
+}
+
+int main() {
     int n1, n2;
     cin >> n1 >> n2;
 
-    vector<int> a(n1), b(n2);
-    for (int i = 0; i < n1; i++) cin >> a[i];
+    vector<int> a_seq(n1), b_seq(n2);
+    for (int i = 0; i < n1; i++) cin >> a_seq[i];
+    for (int i = 0; i < n2; i++) cin >> b_seq[i];
 
-    for (int i = 0; i < n2; i++) cin >> b[i];
-
-    // Please write your code here.
-    unordered_set<int> s(a.begin(), a.end());
-
-    bool ok = true;
-    for(int x: a)
-    {
-        if(s.find(x) == s.end())
-        {
-            ok = false;
-            break;
-        }
-    }
-
-    if(ok)
-        cout << "Yes";
-    else
-        cout << "No";
+    cout << (kmp_contains(a_seq, b_seq) ? "Yes" : "No");
     return 0;
 }
