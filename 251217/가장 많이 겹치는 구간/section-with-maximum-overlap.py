76cffe4094964d86606ca5bd18e0323f78903fac
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-17 09:05:44 +0900

    Title: 가장 많이 겹치는 구간, Time: 102ms, Memory: 21MB, Status: Passed - Codetree

--- 251217/가장 많이 겹치는 구간/section-with-maximum-overlap.py
+++ 251217/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -1,18 +1,28 @@
+MAX_R = 200000
+
+# 변수 선언 및 입력:
 n = int(input())
-intervals = [tuple(map(int, input().split())) for _ in range(n)]
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+checked = [0] * (MAX_R + 1)
+ans = 0
 
-# Please write your code here.
-events = []
-for x1, x2 in intervals:
-    events.append((x1, 1))
-    events.append((x2 + 1, -1))
+# 주어진 좌표의 범위가 작을 때에는
+# 배열을 이용하여 직접 각 칸에
+# +1 -1을 진행해도 무방합니다.
+for x1, x2 in segments:
+    checked[x1] += 1
+    checked[x2] -= 1
 
-events.sort()
-
-cur, ans = 0, 0
-for _, delta in events:
-    cur += delta
-    if cur > ans:
-        ans = cur
+# 모든 곳을 조사해보며
+# 그 중 가장 많이 겹치는 횟수를 구합니다.
+# 겹치는 횟수는
+# 각 위치에 적혀있는 숫자들의 합을 누적해주면 구해집니다.
+overlapped_cnt = 0
+for x in range(1, MAX_R + 1):
+    overlapped_cnt += checked[x]
+    ans = max(ans, overlapped_cnt)
 
 print(ans)
