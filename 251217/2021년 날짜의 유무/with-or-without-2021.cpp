e8fc6a023c5de3f75d2d06f00d3b5e9f5ac9d8a4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-17 09:47:07 +0900

    Title: 2021년 날짜의 유무, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251217/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, D;
+    cin >> M >> D;
+
+    if (M >= 1 && M <= 12) {
+        if (M != 2) {
+            if (M == 1 || M == 3 || M == 5 || M == 7 ||
+                M == 8 || M == 10 || M == 12) {
+                if (D >= 1 && D <= 31)
+                    cout << "Yes";
+                else
+                    cout << "No";
+            } else {
+                if (D >= 1 && D <= 30)
+                    cout << "Yes";
+                else
+                    cout << "No";
+            }
+        } else {
+            if (D >= 1 && D <= 28)
+                cout << "Yes";
+            else
+                cout << "No";
+        }
+    } else {
+        cout << "No";
+    }
+
+    return 0;
+}
