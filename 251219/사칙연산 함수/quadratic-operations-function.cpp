5d4f3836db028634a8cf8b39f6412417d7de6c4b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-19 10:53:46 +0900

    Title: 사칙연산 함수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251219/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int a;
+int c;
+string o;
+
+int add(int a, int b);
+int diff(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+int main() 
+{
+    cin >> a >> o >> c;
+
+    // Please write your code here.
+    if (o == "+" || o == "-" || o == "*" || o == "/") 
+    {
+        if (o == "+") 
+        {
+            cout << a << " " << o << " " << c << " = " << add(a, c);
+        } 
+        else if (o == "-") 
+        {
+            cout << a << " " << o << " " << c << " = " << diff(a, c);
+        } 
+        else if (o == "*") 
+        {
+            cout << a << " " << o << " " << c << " = " << multiply(a, c);
+        } 
+        else 
+        { // "/"
+            cout << a << " " << o << " " << c << " = " << divide(a, c);
+        }
+    } 
+    else 
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int diff(int a, int b)
+{
+    return a - b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    return a / b;
+}
