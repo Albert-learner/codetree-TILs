594aa44143ba541ac618d44db62eb43b5e7979c9
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-20 17:44:05 +0900

    Title: 두 정수 값 교환하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251220/두 정수 값 교환하기/to-exchange-two-integer-values.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+
+int main() 
+{
+    cin >> n >> m;
+
+    // Please write your code here.
+    swap(n, m);
+    cout << n << ' ' << m;
+    return 0;
+}
