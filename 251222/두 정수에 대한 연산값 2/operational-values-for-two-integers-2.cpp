cff5f4702d27c6ab2ef2fd218940fec470a8fe11
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-22 08:46:34 +0900

    Title: 두 정수에 대한 연산값 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251222/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+pair<int, int> operate(int a, int b);
+
+int main() 
+{
+    cin >> a >> b;
+
+    // Please write your code here.
+    auto result = operate(a, b);
+    cout << result.first << ' ' << result.second;
+    return 0;
+}
+
+pair<int, int> operate(int a, int b)
+{
+    if(a < b)
+    {
+        a += 10;
+        b *= 2;
+    }
+    else
+    {
+        b += 10;
+        a *= 2
+    }
+
+    return {a, b};
+}
