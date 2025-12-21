c78e9c25a969219b97e4d1d3387ca0fb16eb5345
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-22 08:21:04 +0900

    Title: 짝수만 2로 나누기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251222/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> modify(vector<int> lst) 
+{
+    for (int i = 0; i < lst.size(); i++) 
+    {
+        if (lst[i] % 2 == 0) 
+        {
+            lst[i] /= 2;
+        }
+    }
+    return lst;
+}
+
+int main() 
+{
+    int N;
+    cin >> N;
+
+    vector<int> n_lst(N);
+    for (int i = 0; i < N; i++) 
+    {
+        cin >> n_lst[i];
+    }
+
+    vector<int> result = modify(n_lst);
+
+    for (int x : result) 
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
