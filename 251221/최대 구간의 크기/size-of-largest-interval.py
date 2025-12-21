c18a98eb7f4d66040e3f7f79b8fb023c7d74dde0
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-21 11:40:37 +0900

    Title: 최대 구간의 크기, Time: 1010ms, Memory: 53MB, Status: Time Limit Exceed - Codetree

--- 251221/최대 구간의 크기/size-of-largest-interval.py
+++ 251221/최대 구간의 크기/size-of-largest-interval.py
@@ -1,21 +1,53 @@
+# 변수 선언 및 입력:
 n = int(input())
-intervals = [tuple(map(int, input().split())) for _ in range(n)]
-a, b = zip(*intervals)
-a_lst, b_lst = list(a), list(b)
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-# Please write your code here.
-intervals.sort()
+# 각 선분을 두 지점으로 나눠 담은 뒤,
+# 정렬해줍니다.
+# 이때 (x좌표, +1-1값, 선분 번호)
+# 형태로 넣어줍니다.
+# +1은 시작점
+# -1은 끝점을 뜻합니다.
+points = []
+for i, (x1, x2) in enumerate(segments):
+    points.append((x1, +1, i)) # 시작점
+    points.append((x2, -1, i)) # 끝점
 
-total_left, total_right = 0, 0
-cur_l, cur_r = intervals[0]
+# 정렬을 진행합니다.
+points.sort()
 
-for l, r in intervals[1:]:
-    if l > cur_r:
-        total_left += cur_r - cur_l
-        cur_l, cur_r = l, r
+# 각 점을 순서대로 순회합니다.
+# 등장하고 아직 사라지지 않은
+# 선분을 hashset으로 관리합니다.
+segs = set()
+
+ans = 0       # 가장 큰 구간의 크기를 출력합니다.
+start_x = -1  # 현재 합쳐진 구간의 시작 x값을 기록해줍니다.
+for x, v, index in points:
+    # 시작점인 경우입니다.
+    if v == +1:
+        # 남아있는 선분이 없다면
+        # 합쳐진 구간의 시작이므로
+        # start_x값을 갱신해줍니다.
+        if not segs:
+            start_x = x    
+        
+        # 해당 선분 번호를 hashset에 넣어줍니다.
+        segs.add(index)
+
+    # 끝점인 경우입니다.
     else:
-        if r > cur_r:
-            cur_r = r
+        # 해당 선분을 제거합니다.
+        segs.remove(index)
 
-total_right += cur_r - cur_l
-print(max(total_left, total_right))
+        # 남아있는 선분이 없다면
+        # 합쳐진 구간의 끝이므로
+        # 답을 갱신해줍니다.
+        if not segs:
+            end_x = x
+            ans = max(ans, end_x - start_x)
+
+print(ans)
