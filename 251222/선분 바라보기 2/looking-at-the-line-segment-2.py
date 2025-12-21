68a629dc7800f785db4d4cb2378729586611d811
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-22 08:36:18 +0900

    Title: 선분 바라보기 2, Time: 943ms, Memory: 50MB, Status: Passed - Codetree

--- 251222/선분 바라보기 2/looking-at-the-line-segment-2.py
+++ 251222/선분 바라보기 2/looking-at-the-line-segment-2.py
@@ -1,40 +1,61 @@
+from sortedcontainers import SortedSet
+
+# 변수 선언 및 입력:
 n = int(input())
-y, x1, x2 = [], [], []
-for _ in range(n):
-    yi, x1i, x2i = map(int, input().split())
-    y.append(yi)
-    x1.append(x1i)
-    x2.append(x2i)
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-# Please write your code here.
-import heapq
+# 보이는 색깔의 경우 True로 표기합니다.
+visible = [False] * n
 
-events = []
+# 각 선분을 두 지점으로 나눠 담은 뒤,
+# 정렬해줍니다.
+# 이때 (x좌표, +1-1값, 선분 번호, y값)
+# 형태로 넣어줍니다.
+# +1은 시작점
+# -1은 끝점을 뜻합니다.
+points = []
 for i in range(n):
-    events.append((x1[i], 1, i))
-    events.append((x2[i], 0, i))
+    y, x1, x2 = segments[i]
+    points.append((x1, +1, i, y)) # 시작점
+    points.append((x2, -1, i, y)) # 끝점
 
-events.sort(key=lambda t: (t[0], t[1]))
+# 정렬을 진행합니다.
+points.sort()
 
-active = [False] * n
-heap = []  
-visible = set()
+# 각 점을 순서대로 순회합니다.
+# 등장하고 아직 사라지지 않은
+# 선분을 중 y값이 가장 작은 선분의 번호를 빠르게 구하기 위해
+# treeset에
+# (y값, 선분 번호) 형태로 넣어줍니다.
+segs = SortedSet()
+for _, v, index, y in points:
+    # 시작점인 경우입니다.
+    if v == +1:
+        # 해당 선분 정보를 treeset에 넣어줍니다.
+        segs.add((y, index))
 
-m = len(events)
-for idx in range(m):
-    x, typ, sid = events[idx]
+    # 끝점인 경우입니다.
+    else:
+        # 해당 선분을 제거합니다.
+        segs.remove((y, index))
 
-    if typ == 0:
-        active[sid] = False
-    else:    
-        active[sid] = True
-        heapq.heappush(heap, (y[sid], sid))
+    # 남아 있는 선분이 없다면 패스합니다. 
+    if not segs:
+        continue
 
-    next_x = events[idx + 1][0] if idx + 1 < m else None
-    if next_x is not None and next_x > x:
-        while heap and not active[heap[0][1]]:
-            heapq.heappop(heap)
-        if heap:
-            visible.add(heap[0][1])
+    # 현재 눈에 보이는 선분을 찾아
+    # 해당 index에 True값을 적어줍니다.
+    _, target_index = segs[0]
+    visible[target_index] = True
 
-print(len(visible))
+# 한 번이라도 보였던
+# 선분의 개수를 세줍니다.
+ans = 0
+for i in range(n):
+    if visible[i]:
+        ans += 1
+
+print(ans)
