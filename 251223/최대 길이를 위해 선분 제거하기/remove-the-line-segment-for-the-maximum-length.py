c880f61ff2c855c69ec0ac0eb4ec920db2fdfb49
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-23 08:58:26 +0900

    Title: 최대 길이를 위해 선분 제거하기, Time: 986ms, Memory: 54MB, Status: Passed - Codetree

--- 251223/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
+++ 251223/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
@@ -1,51 +1,67 @@
-n = int(input())
-segments = [tuple(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
 import sys
 
-if n == 1:
-    print(0)
-    sys.exit(0)
+INT_MAX = sys.maxsize
 
-events = []
-for i, (l, r) in enumerate(segments):
-    events.append((l, 1, i))
-    events.append((r, 0, i))
+# 변수 선언 및 입력:
+n = int(input())
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-events.sort(key=lambda x: (x[0], x[1]))
+weights = [0] * n # 각 선분을 제외했을 때 빠지게 되는 길이를 저장합니다.
 
-active = set()
-unique = [0] * n
-total_union = 0
+# 각 선분을 두 지점으로 나눠 담은 뒤,
+# 정렬해줍니다.
+# 이때 (x좌표, +1-1값, 선분 번호)
+# 형태로 넣어줍니다.
+# +1은 시작점
+# -1은 끝점을 뜻합니다.
+points = []
+for i in range(n):
+    x1, x2 = segments[i]
+    points.append((x1, +1, i)) # 시작점
+    points.append((x2, -1, i)) # 끝점
 
-prev_x = events[0][0]
-idx = 0
-m = len(events)
+# 정렬을 진행합니다.
+points.sort()
 
-while idx < m:
-    x = events[idx][0]
+# 각 점을 순서대로 순회합니다.
+# 각 선분을 제외했을 때 빠지게 되는 길이를
+# 각 weights[index]에 표시해줍니다.
+tot_length = 0       # 전체 길이의 합을 계산합니다.
+segs = set()  # 현재 걸쳐져 있는 선분 번호를 관리합니다.
+prev_x = -1          # 바로 직전 x값을 관리합니다.
+for x, v, index in points:
+    # 걸쳐져 있는 선분의 수는 segs의 크기와 같습니다.
+    seg_cnt = len(segs)
 
-    length = x - prev_x
-    if length > 0:
-        if active:
-            total_union += length
-            if len(active) == 1:
-                only_id = next(iter(active))
-                unique[only_id] += length
+    # 걸쳐져 있는 선분의 수가 1개 이상이라면
+    # 전체 길이의 합을 갱신합니다.
+    if seg_cnt > 0:
+        tot_length += x - prev_x
+    
+    # 걸쳐져 있는 선분의 수가 정확히 1개라면
+    # 해당 걸쳐져 있는 선분의 weights를 갱신합니다.
+    if seg_cnt == 1:
+        target_index = list(segs)[0]
+        weights[target_index] += x - prev_x
 
-    while idx < m and events[idx][0] == x:
-        _, typ, sid = events[idx]
-        if typ == 0:          # end
-            active.discard(sid)
-        else:                 # start
-            active.add(sid)
-        idx += 1
+    # 시작점인 경우입니다.
+    if v == +1:
+        # 걸쳐져 있는 선분 번호를 갱신합니다.
+        segs.add(index)
 
+    # 끝점인 경우입니다.
+    else:
+        # 걸쳐져 있는 선분 번호를 제거합니다.
+        segs.remove(index)
+
+    # 이전 x 값을 갱신해줍니다.
     prev_x = x
 
-ans = 0
+min_length = INT_MAX # 특정 선분을 제외했을 때 빠지게 되는 길이 중 최솟값을 계산합니다.
 for i in range(n):
-    ans = max(ans, total_union - unique[i])
+    min_length = min(min_length, weights[i])
 
-print(ans)
+print(tot_length - min_length)
