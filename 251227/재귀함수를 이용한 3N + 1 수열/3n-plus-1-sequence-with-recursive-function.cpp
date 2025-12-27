5901718fe681c49323909342baa397c4f0de6083
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-27 15:53:24 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251227/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int recur_seq(int num);
+
+int main() 
+{
+    cin >> n;
+
+    // Please write your code here.
+    cout << recur_seq(n);
+    return 0;
+}
+
+int recur_seq(int num)
+{
+    if(num == 1)
+        return 0;
+
+    if(num % 2 == 0)
+        return recur_seq((int)(num / 2)) + 1;
+    else
+        return recur_seq((int)(num / 3) + 1) + 1;
+}
