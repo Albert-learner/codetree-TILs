077f8f7e6d38d8808688073de52ce8b95acb7f53
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-27 15:23:57 +0900

    Title: 균일하게 나누기 2, Time: 88ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 251227/균일하게 나누기 2/divide-evenly-2.py
+++ 251227/균일하게 나누기 2/divide-evenly-2.py
@@ -1,49 +1,11 @@
-n = int(input())
-points = [tuple(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
-MAXY = 1001
-
-left = [0] * (MAXY + 1)
-right = [0] * (MAXY + 1)
-
-for x, y in points:
-    right[y] += 1
-
-points.sort()
-
-def evaluate():
-    total_left = sum(left)
-    total_right = sum(right)
-
-    pl = [0] * (MAXY + 1)
-    pr = [0] * (MAXY + 1)
-    runl = runr = 0
-    for y in range(MAXY + 1):
-        runl += left[y]
-        runr += right[y]
-        pl[y] = runl
-        pr[y] = runr
-
-    best = 10 ** 18
-    for b in range(2, 1001, 2):
-        below_y = b - 1
-
-        lb = pl[below_y]
-        la = total_left - lb
-        rb = pr[below_y]
-        ra = total_right - rb
-
-        best = min(best, max(lb, la, rb, ra))
-
-    return best
-
-ans = evaluate()
+ans = evaluate()  # 세로선을 가장 왼쪽(예: x=0) 쪽에 둔 경우도 고려
 
 i = 0
 while i < n:
     cur_x = points[i][0]
 
+    # 세로선을 x = cur_x + 1 로 옮긴다고 생각하면,
+    # x == cur_x 인 점들은 이제 왼쪽으로 넘어감
     while i < n and points[i][0] == cur_x:
         _, y = points[i]
         right[y] -= 1
