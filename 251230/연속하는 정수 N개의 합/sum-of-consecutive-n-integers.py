c9eb94066eb23db28864118cbaba885cd11d9d6c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-30 21:06:01 +0900

    Title: 연속하는 정수 N개의 합, Time: 65ms, Memory: 22MB, Status: Passed - Codetree

--- 251230/연속하는 정수 N개의 합/sum-of-consecutive-n-integers.py
+++ 251230/연속하는 정수 N개의 합/sum-of-consecutive-n-integers.py
@@ -1,19 +1,25 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-# Please write your code here.
-cnt, cur_sum, l = 0, 0, 0
+ans = 0
 
-for r in range(n):
-    cur_sum += arr[r]
+# 구간을 잡아봅니다.
+sum_val = 0
+j = 0
+for i in range(1, n + 1):
+    # 구간 내 합이 m보다 작으면 계속 진행합니다.
+    while j + 1 <= n and sum_val < m:
+        sum_val += arr[j + 1]
+        j += 1
 
-    while cur_sum > m and l <= r:
-        cur_sum -= arr[l]
-        l += 1
+    # 구간 내 합이 m이 되면 정답을 1 증가시킵니다.
+    if sum_val == m:
+        ans += 1
 
-    if cur_sum == m:
-        cnt += 1
-        cur_sum -= arr[l]
-        l += 1
+    # 다음 구간으로 넘어가기 전에
+    # arr[i]에 해당하는 값은 구간에서 제외시킵니다.
+    sum_val -= arr[i]
 
-print(cnt)
+# 정답을 출력합니다.
+print(ans)
