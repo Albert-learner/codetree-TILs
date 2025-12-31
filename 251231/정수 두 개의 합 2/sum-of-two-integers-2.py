d4db3210fcffab9bc670602ffb194596e26a2046
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-31 21:55:10 +0900

    Title: 정수 두 개의 합 2, Time: 109ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 251231/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+arr.sort()
+
+cnts = 0
+l, r = 0, n - 1
+
+while l < r:
+    if arr[l] + arr[r] <= k:
+        cnts += (r - l)
+        l += 1
+    else:
+        r -= 1
+
+print(cnts)
