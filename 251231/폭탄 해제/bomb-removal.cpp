000e76bae887908aaa24b4296406fab3a77e0fad
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2025-12-31 22:22:19 +0900

    Title: 폭탄 해제, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251231/폭탄 해제/bomb-removal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CODE {
+public:
+    string code;
+    string color;
+    int second;
+
+    CODE(string c, string col, int s)
+        : code(c), color(col), second(s) {}
+
+    void print_code() {
+        cout << "code : " << code << endl;
+        cout << "color : " << color << endl;
+        cout << "second : " << second << endl;
+    }
+};
+
+int main() {
+    string code, color;
+    int second;
+
+    cin >> code >> color >> second;
+
+    CODE code_inst(code, color, second);
+    code_inst.print_code();
+
+    return 0;
+}
