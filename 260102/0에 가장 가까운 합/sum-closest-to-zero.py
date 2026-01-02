ac2a8c2cb55959a3eb244c441245f58df9818148
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-02 12:43:13 +0900

    Title: 0에 가장 가까운 합, Time: 64ms, Memory: 20MB, Status: Passed - Codetree

--- 260102/0에 가장 가까운 합/sum-closest-to-zero.py
+++ 260102/0에 가장 가까운 합/sum-closest-to-zero.py
@@ -5,20 +5,15 @@ a = list(map(int, input().split()))
 a.sort()
 
 l, r = 0, n - 1
-best_sum = float("inf")
-best_pair = (a[l], a[r])
+best = float("inf")
 
 while l < r:
     s = a[l] + a[r]
-    if abs(s) < abs(best_sum):
-        best_sum = s
-        best_pair = (a[l], a[r])
-        if best_sum == 0:
-            break
+    best = min(best, abs(s))
 
     if s < 0:
         l += 1
     else:
         r -= 1
 
-print(best_sum)
+print(best)
