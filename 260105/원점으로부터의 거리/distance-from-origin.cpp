a605554dd3b96ae2d2f776299c512164b52c6412
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-05 09:30:59 +0900

    Title: 원점으로부터의 거리, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260105/원점으로부터의 거리/distance-from-origin.cpp
+++ 260105/원점으로부터의 거리/distance-from-origin.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
-struct Points
+struct Point 
 {
     int x;
     int y;
-    int num;
+    int num; 
 };
 
 int main() 
 {
     int N;
     cin >> N;
-    
-    vector<Points> points;
+
+    vector<Point> points;
     points.reserve(N);
 
-    // Please write your code here.
-    for(int i = 1; i <= N; i++)
+    for (int i = 1; i <= N; i++) 
     {
         int x, y;
         cin >> x >> y;
         points.push_back({x, y, i});
     }
 
-    sort(points.begin(), points.end(), 
-        [](const Points& a, const Points& b)
-            {
-                return abs(a.x) + abs(a.y) < abs(b.x) + abs(b.y);
-            });
+    sort(points.begin(), points.end(),
+         [](const Point& a, const Point& b) 
+         {
+             return abs(a.x) + abs(a.y) < abs(b.x) + abs(b.y);
+         });
 
-    for(const auto& p: points)
+    for (const auto& p : points) 
     {
-        cout << p.num << endl;
+        cout << p.num << '\n';
     }
+
     return 0;
 }
