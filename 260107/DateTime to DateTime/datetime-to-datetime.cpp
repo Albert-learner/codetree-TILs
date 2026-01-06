1c2c79f5805bd10cc2ba68a93c9ded30322d3098
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-07 08:40:06 +0900

    Title: DateTime to DateTime, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() 
+{
+    int a, b, c, start_time = 0, end_time = 0;
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    start_time = 10 * 24 * 60 + 11 * 60 + 11;
+    end_time = (a - 1) * 24 * 60 + b * 60 + c;
+
+    if(end_time - start_time < 0)
+        cout << -1;
+    else
+        cout << end_time - start_time;
+    return 0;
+}
