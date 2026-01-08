8a0405468efb299243e0aff1cd9dfb8f641ce5ed
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-08 09:27:16 +0900

    Title: 그 요일은, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260108/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int m1, m2, d1, d2;
+    string find_day;
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> find_day;
+
+    // Please write your code here.
+    vector<string> days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    vector<int> months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    auto get_num = [&](const string& f_day) -> int{
+        for(int i = 0; i < (int)days.size(); i++)
+            if(days[i] == f_day) 
+                return i;
+
+        return 0;
+    };
+
+    int day_num = get_num(find_day);
+    d1 += day_num;
+
+    auto total_days = [&](int m, int d) -> long long{
+        long long t_days = 0;
+        for (int i = 1; i < m; i++) t_days += months[i];
+        t_days += d;
+        return t_days;
+    };
+
+    long long diff_days = total_days(m2, d2) - total_days(m1, d1);
+    cout << (diff_days / 7 + 1) << "\n";
+    return 0;
+}
