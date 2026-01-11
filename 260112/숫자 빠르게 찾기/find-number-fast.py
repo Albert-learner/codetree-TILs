e258613cdc86f541948e0c89c85528c9f7e19952
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-12 08:29:48 +0900

    Title: 숫자 빠르게 찾기, Time: 367ms, Memory: 24MB, Status: Passed - Codetree

--- 260112/숫자 빠르게 찾기/find-number-fast.py
+++ 260112/숫자 빠르게 찾기/find-number-fast.py
@@ -1,21 +1,26 @@
-n, m = map(int, input().split())
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
-queries = [int(input()) for _ in range(m)]
 
-# Please write your code here.
-for qry in queries:
-    idx = -1
+
+def find(target):
     left, right = 0, n - 1
 
     while left <= right:
         mid = (left + right) // 2
-        if arr[mid] == qry:
-            idx = mid + 1
-            break
+        if arr[mid] == target:
+            return mid
 
-        if arr[mid] > qry:
+        if arr[mid] > target:
             right = mid - 1
         else:
             left = mid + 1
+    
+    return -1
 
-    print(idx)
+
+for _ in range(m):
+    x = int(input())
+    index = find(x) # 이진탐색을 진행합니다.
+
+    print(-1 if index < 0 else index + 1)
