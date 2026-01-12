dbe772e8ca29a634ce3caeb4a746447fb7a39736
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-13 08:22:23 +0900

    Title: 가장 먼저 나오는 숫자, Time: 120ms, Memory: 31MB, Status: Passed - Codetree

--- 260113/가장 먼저 나오는 숫자/first-appear-number.py
+++ 260113/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,25 +1,29 @@
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-query = list(map(int, input().split()))
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+querries = list(map(int, input().split()))
 
-# Please write your code here.
-def binary_search(target):
-    left, right = 0, n - 1
-    idx = n
-
+# target보다 같거나 큰 최초의 위치를 반환합니다.
+def lower_bound(target):
+    left, right = 1, n
+    min_idx = n + 1
     while left <= right:
         mid = (left + right) // 2
-
-        if arr[mid] == target:
-            idx = min(idx, mid)
-
         if arr[mid] >= target:
             right = mid - 1
+            min_idx = min(min_idx, mid)
         else:
             left = mid + 1
 
-    return idx
+    return min_idx
 
-for qry in query:
-    idx = binary_search(qry)
-    print(idx + 1 if idx != n else -1)
+for querry in querries:
+    # 이진탐색을 진행합니다.
+    # querry보다 크거나 같은 최소의 값의 위치를 구하고
+    # 그 값이 querry와 같으면 위치를,
+    # 다르면 -1을 출력합니다.
+    lo = lower_bound(querry)
+    if lo <= n and arr[lo] == querry:
+        print(lo)
+    else:
+        print(-1)
