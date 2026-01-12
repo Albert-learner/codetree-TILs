65ed6466871f7d22a6884ab64415c2d68a0ac881
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-13 08:45:35 +0900

    Title: 선분 위의 점, Time: 334ms, Memory: 38MB, Status: Passed - Codetree

--- 260113/선분 위의 점/point-on-the-line-segment.py
+++ 260113/선분 위의 점/point-on-the-line-segment.py
@@ -3,23 +3,17 @@ points = list(map(int, input().split()))
 segments = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
-def binary_search(start, end, target):
-    if target > end or target < start:
-        return 0
-    else:
-        while start <= end:
-            mid = (start + end) // 2
-            if mid == target:
-                return 1
-            elif mid > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-        
-        return 0
+from bisect import bisect_left, bisect_right
 
+points.sort()
+
+out = []
 for x, y in segments:
-    cnts = 0
-    for point in points:
-        cnts += binary_search(x, y, point)
-    print(cnts)
+    if x > y:
+        x, y = y, x
+
+    left = bisect_left(points, x)
+    right = bisect_right(points, y)
+    out.append(str(right - left))
+
+print("\n".join(out))
