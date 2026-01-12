460ebe4f5918f9206aea49099d28359ac6c005f4
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-13 08:46:08 +0900

    Title: 선분 위의 점, Time: 523ms, Memory: 27MB, Status: Passed - Codetree

--- 260113/선분 위의 점/point-on-the-line-segment.py
+++ 260113/선분 위의 점/point-on-the-line-segment.py
@@ -1,19 +1,49 @@
-n, m = map(int, input().split())
-points = list(map(int, input().split()))
-segments = [tuple(map(int, input().split())) for _ in range(m)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-# Please write your code here.
-from bisect import bisect_left, bisect_right
 
-points.sort()
+# target보다 큰 최초의 위치를 반환합니다.
+def upper_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            right = mid - 1
+            min_idx = min(min_idx, mid)
+        else:
+            left = mid + 1
 
-out = []
-for x, y in segments:
-    if x > y:
-        x, y = y, x
+    return min_idx
 
-    left = bisect_left(points, x)
-    right = bisect_right(points, y)
-    out.append(str(right - left))
 
-print("\n".join(out))
+# target보다 같거나 큰 최초의 위치를 반환합니다.
+def lower_bound(target):
+    left, right = 0, n - 1
+    min_idx = n
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            right = mid - 1
+            min_idx = min(min_idx, mid)
+        else:
+            left = mid + 1
+
+    return min_idx
+
+
+# 이진탐색을 진행하기 전에
+# 정렬을 진행해줍니다.
+arr.sort()
+
+# m개의 질의에 대한 답을 계산합니다.
+for _ in range(m):
+    a, b = tuple(map(int, input().split()))
+
+    # 이진탐색을 진행합니다.
+    # b보다 큰 최초의 숫자 위치에서
+    # a보다 같거나 큰 최초의 숫자 위치를 빼면
+    # 문제에서 원하는 답이 됩니다.
+    count = upper_bound(b) - lower_bound(a)
+    print(count)
