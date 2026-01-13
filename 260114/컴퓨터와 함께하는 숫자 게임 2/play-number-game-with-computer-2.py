e33728a0801f41b56a4c73dc3660fe74dfe3416b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-14 08:44:00 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260114/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
+++ 260114/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -1,30 +1,41 @@
+import sys
+
+INT_MAX = sys.maxsize
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력:
 m = int(input())
-a, b = map(int, input().split())
+a, b = tuple(map(int, input().split()))
 
-# Please write your code here.
-def steps_to_find(x, m):
-    l, r = 1, m
-    cnts = 0
+min_ans, max_ans = INT_MAX, INT_MIN
 
-    while True:
-        mid = (l + r) // 2
-        cnts += 1
-        if mid == x:
-            return cnts
-        elif mid > x:
-            r = mid - 1
+
+def get_try_num(target):
+    left, right = 1, m
+    cnt = 0
+    while left <= right:
+        mid = (left + right) // 2
+        cnt += 1
+
+        if mid == target:
+            return cnt
+        
+        if mid > target:
+            right = mid - 1
         else:
-            l = mid + 1
+            left = mid + 1
 
-min_steps = 10 ** 18
-max_steps = 0
+    return -1
 
-for x in range(a, b + 1):
-    s = steps_to_find(x, m)
-    if s < min_steps:
-        min_steps = s
-    
-    if s > max_steps:
-        max_steps = s
+   
+# 컴퓨터가 선택한 수를
+# a부터 b까지 각각 가정해보며
+# 그 중 최소 횟수와 최대 횟수를 구합니다.
+for i in range(a, b + 1):
+    # 이진탐색을 진행하여 진행 횟수를 구합니다.
+    num = get_try_num(i)
+    # 답을 갱신합니다.
+    min_ans = min(min_ans, num)
+    max_ans = max(max_ans, num)
 
-print(min_steps, max_steps)
+print(min_ans, max_ans)
