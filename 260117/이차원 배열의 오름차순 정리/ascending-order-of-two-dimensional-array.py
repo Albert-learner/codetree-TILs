5bda700973ac9c1a10cdafbefbdba913fb3a9081
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-17 22:18:58 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 260117/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.py
+++ 260117/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.py
@@ -1,23 +1,23 @@
+# 변수 선언 및 입력:
 n = int(input())
 k = int(input())
 
-# Please write your code here.
-def count_leq(x: int) -> int:
-    cnts = 0
+lo = 1                        # 답이 될 수 있는 가장 작은 숫자 값을 설정합니다.
+hi = n * n                    # 답이 될 수 있는 가장 큰 숫자 값을 설정합니다.
+ans = n * n                   # 답을 저장합니다.
+
+while lo <= hi:               # [lo, hi]가 유효한 구간이면 계속 수행합니다.
+    mid = (lo + hi) // 2      # 가운데 위치를 선택합니다.
+    
+    val = 0
     for i in range(1, n + 1):
-        cnts += min(n, x // i)
-
-    return cnts
-
-left, right = 1, n * n
-ans = right
-
-while left <= right:
-    mid = (left + right) // 2
-    if count_leq(mid) >= k:
-        ans = mid
-        right = mid - 1
+        val += min(n, mid // i)
+    
+    if val >= k:             # 결정문제에 대한 답이 Yes라면
+        hi = mid - 1         # 왼쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 hi를 바꿔줍니다.
+        ans = min(ans, mid)  # 답의 후보들 중 최솟값을 계속 갱신해줍니다.
     else:
-        left = mid + 1
+        lo = mid + 1         # 결정문제에 대한 답이 No라면 lo를 바꿔줍니다.
 
+# 정답을 출력합니다.
 print(ans)
