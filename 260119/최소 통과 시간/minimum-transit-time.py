3e386f0c88521379bff000c560a3bdedf259ce19
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-19 09:31:17 +0900

    Title: 최소 통과 시간, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260119/최소 통과 시간/minimum-transit-time.py
+++ 260119/최소 통과 시간/minimum-transit-time.py
@@ -1,15 +1,11 @@
-n, m = map(int, input().split())
-arr = [int(input()) for _ in range(m)]
-
-# Please write your code here.
 def can_pass(mid, times, n):
-    total = sum(mid / time for time in times)
+    total = sum(mid // time for time in times)
     return total >= n
 
 def min_pass_time(n, m, times):
     left, right = 0, max(times) * n
     answer = right
-
+    
     while left <= right:
         mid = (left + right) // 2
         if can_pass(mid, times, n):
@@ -17,7 +13,11 @@ def min_pass_time(n, m, times):
             right = mid - 1
         else:
             left = mid + 1
-
     return answer
 
-print(min_pass_time(n, m, arr))
+# 입력 받기
+n, m = map(int, input().split())
+times = [int(input()) for _ in range(m)]
+
+# 결과 출력
+print(min_pass_time(n, m, times))
