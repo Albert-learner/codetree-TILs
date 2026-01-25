263c9063ad25eea786bd9323a6b4dc48349e772f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-26 08:37:34 +0900

    Title: 선두를 지켜라, Time: 17ms, Memory: 8MB, Status: Passed - Codetree

--- 260126/선두를 지켜라/keep-the-lead.cpp
+++ 260126/선두를 지켜라/keep-the-lead.cpp
@@ -8,21 +8,22 @@ int main()
 
     vector<long long> a_dist(1, 0), b_dist(1, 0);
 
-    auto make_dist = [&](int p_num, vector<long long>& which_dist)
+    auto make_dist = [&](int p_num, vector<long long>& which_dist) 
     {
-        for(int i = 0; i < p_num; i++)
+        for (int i = 0; i < p_num; i++) 
         {
             long long velocity;
             int times;
             cin >> velocity >> times;
-            for(int j = 0; j < times; j++)
+            for (int j = 0; j < times; j++) 
+            {
                 which_dist.push_back(which_dist.back() + velocity);
+            }
         }
     };
 
-    // Please write your code here.
     make_dist(N, a_dist);
-    make_dist(N, b_dist);
+    make_dist(M, b_dist);
 
     vector<long long> compare;
     compare.reserve(min(a_dist.size(), b_dist.size()));
@@ -45,6 +46,5 @@ int main()
     }
 
     cout << answer << "\n";
-
     return 0;
 }
