20ad4998c62549c15cb260b26fa89fea9d5dbab3
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-26 08:27:32 +0900

    Title: 버스를 기다리며, Time: 83ms, Memory: 25MB, Status: Passed - Codetree

--- 260126/버스를 기다리며/waiting-for-the-bus.py
+++ 260126/버스를 기다리며/waiting-for-the-bus.py
@@ -1,31 +1,37 @@
-N, M, C = map(int, input().split())
+# 변수 선언 및 입력:
+n, m, c = tuple(map(int, input().split()))
 t = list(map(int, input().split()))
 
-# Please write your code here.
+
+# wait 이하로만 기다릴 수 있을 때,
+# 모든 사람을 태우는게 가능한지 여부를 판단합니다.
+def isPossible(wait):
+    bus = 1
+    firstArrival = t[0]
+    firstIndex = 0
+    for i in range(n):
+        # 시작 사람부터 i번째 사람까지 한번에 타지 못하게 되면,
+        # i-1번째 사람까지를 한번에 태우고 i번째 사람을 시작 사람으로 갱신합니다.
+        if t[i] - firstArrival > wait or i + 1 - firstIndex > c:
+            bus += 1
+            firstArrival = t[i]
+            firstIndex = i
+    return bus <= m
+
+
 t.sort()
 
-def can(max_wait: int) -> bool:
-    i, buses = 0, 0
-    while i < N:
-        buses += 1
-        if buses > M:
-            return False
+lo = 0                     # 답이 될 수 있는 가장 작은 숫자 값을 설정합니다.
+hi = 10**9                 # 답이 될 수 있는 가장 큰 숫자 값을 설정합니다.
+ans = 10**9                # 답을 저장합니다.
 
-        first_time = t[i]
-        cnts = 0
-
-        while i < N and cnts < C and t[i] - first_time <= max_wait:
-            i += 1
-            cnts += 1
-
-    return True
-
-lo, hi = 0, 10 ** 9
-while lo < hi:
-    mid = (lo + hi) // 2
-    if can(mid):
-        hi = mid
+while lo <= hi:            # [lo, hi]가 유효한 구간이면 계속 수행합니다.
+    mid = (lo + hi) // 2   # 가운데 위치를 선택합니다.
+    if isPossible(mid):    # 결정문제에 대한 답이 Yes라면
+        hi = mid - 1       # 왼쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 right를 바꿔줍니다.
+        ans = min(ans, mid)# 답의 후보들 중 최솟값을 계속 갱신해줍니다.
     else:
-        lo = mid + 1
+        lo = mid + 1       # 결정문제에 대한 답이 No라면 right를 바꿔줍니다.
 
-print(lo)
+# 정답을 출력합니다.
+print(ans)
