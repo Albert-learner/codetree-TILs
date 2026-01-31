e277b2f3dd1021eb82795bdbb2fd8f28725c25a2
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-01-31 22:23:03 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260131/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int N, M;
+    cin >> N >> M;
+
+    // Please write your code here.
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+
+    vector<vector<int>> board(N, vector<int>(M, 0));
+
+    int dir = 0;
+    int x = 0, y = 0;
+    int num = 1;
+
+    for(int step = 0; step < N * M; step++)
+    {
+        board[x][y] = num;
+
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] != 0) 
+            dir = (dir + 1) % 4;
+
+        x += dx[dir];
+        y += dy[dir];
+        num++;
+    }
+
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < M; j++) 
+        {
+            cout << board[i][j] << (j + 1 == M ? '\n' : ' ');
+        }
+    }
+    return 0;
+}
