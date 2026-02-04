560f7ab9f4f7e2f395ef5d67a770c4b84cc37dbc
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-04 09:08:00 +0900

    Title: 수 채우기, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/수 채우기/fill-in-number.py
+++ 260204/수 채우기/fill-in-number.py
@@ -1,12 +1,19 @@
+MAX_NUM = 100000
+
+# 변수 선언 및 입력:
 n = int(input())
+ans = MAX_NUM
 
-# Please write your code here.
-ans = float("inf")
+# 사용할 5원 동전의 수를 전부 가정해보며
+# 그 중 가장 좋은 선택을 합니다.
+for i in range(0, MAX_NUM + 1):
+    remainder = n - 5 * i
+    if remainder >= 0 and remainder % 2 == 0:
+        ans = min(ans, i + (remainder // 2))
 
-for five in range(n // 5, -1, -1):
-    rest = n - 5 * five
-    if rest % 2 == 0:
-        ans = min(ans, five + rest // 2)
-        break
+# 만약 그러한 동전을 만드는 것이 불가능하다면
+# -1을 넣어줍니다.
+if ans == MAX_NUM:
+    ans = -1
 
-print(ans if ans != float("inf") else -1)
+print(ans)
