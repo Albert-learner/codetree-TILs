e86ba208bcdc88cf1113a91b4bbd6a56b94c8f2f
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-06 10:35:02 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 54ms, Memory: 19MB, Status: Passed - Codetree

--- 260206/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 260206/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -1,14 +1,21 @@
+# 변수 선언 및 입력:
 n = int(input())
 price = list(map(int, input().split()))
 
-# Please write your code here.
-min_price = 10 ** 30
-best = 0
+# 배열을 앞에서부터 순회하며 최소값을 갱신해줍니다.
+# 각 원소에 대하여 해당 시점의 최소값과의 차이가
+# 최대가 될 때를 갱신해줍니다.
+max_profit = 0
+min_price = price[0]
+for i in range(n):
+    profit = price[i] - min_price
 
-for p in price:
-    if p < min_price:
-        min_price = p
-    else:
-        best = max(best, p - min_price)
+    # 답을 갱신해줍니다.
+    if profit > max_profit:
+        max_profit = profit
 
-print(best)
+    # 지금까지의 최솟값을 갱신해줍니다.
+    if min_price > price[i]:
+        min_price = price[i]
+    
+print(max_profit)
