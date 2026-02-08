4ed823dbbfeec66164f797df1a6169a7a7964bd8
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-09 08:58:58 +0900

    Title: 자연수 M/2개의 쌍, Time: 482ms, Memory: 25MB, Status: Passed - Codetree

--- 260209/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 260209/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -1,30 +1,45 @@
-N = int(input())
-pairs = [tuple(map(int, input().split())) for _ in range(N)]
-
-# Please write your code here.
-pairs.sort(key = lambda x: x[1])
-
-cnts = [pair[0] for pair in pairs]
-vals = [pair[1] for pair in pairs]
-
-i, j = 0, N - 1
+# 변수 선언 및 입력:
+n = int(input())
 ans = 0
 
-while i <= j:
-    if i == j:
-        if cnts[i] > 0:
-            ans = max(ans, vals[i] * 2)
-        break
+nums = []
+for _ in range(n):
+    x, y = tuple(map(int, input().split()))
+    nums.append((y, x))
 
-    take = min(cnts[i], cnts[j])
-    ans = max(ans, vals[i] + vals[j])
+# y 기준으로 오름차순 정렬을 진행합니다.
+nums.sort()
 
-    cnts[i] -= take
-    cnts[j] -= take
+# 최선의 전략을 오름차순 정렬 뒤
+# 가장 작은 수와 큰 수를 하나씩 매칭해주는 것입니다.
+li, ri = 0, n - 1
+while li <= ri:
+    ly, lx = nums[li]
+    ry, rx = nums[ri]
 
-    if cnts[i] == 0:
-        i += 1
-    if cnts[j] == 0:
-        j -= 1
+    # 답을 갱신합니다.
+    ans = max(ans, ly + ry)
+
+    # 왼쪽 개수가 더 적다면
+    # 왼쪽을 전부 매칭시켜줍니다.
+    if lx < rx:
+        # 오른쪽은 lx만큼 개수를 줄여줍니다.
+        nums[ri] = (ry, rx - lx)
+        # 왼쪽 위치를 한칸 뒤로 옮겨줍니다.
+        li += 1
+
+    # 오른쪽 개수가 더 적다면
+    # 오른쪽을 전부 매칭시켜줍니다.
+    elif lx > rx:
+        # 왼쪽은 rx만큼 개수를 줄여줍니다.
+        nums[li] = (ly, lx - rx)
+        # 오른쪽 위치를 한칸 앞으로 옮겨줍니다.
+        ri -= 1
+
+    # 개수가 동일하다면
+    # li, ri 위치 모두 옮겨줍니다.
+    else:
+        li += 1
+        ri -= 1
 
 print(ans)
