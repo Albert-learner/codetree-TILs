0842595df14c800acd55d43736f553e6dc5734db
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-12 08:29:17 +0900

    Title: 회의실 겹치지 않게 하기, Time: 281ms, Memory: 27MB, Status: Passed - Codetree

--- 260212/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
+++ 260212/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -1,14 +1,27 @@
+# 변수 선언 및 입력:
 n = int(input())
-meetings = [tuple(map(int, input().split())) for _ in range(n)]
+meetings = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-# Please write your code here.
-meetings.sort(lambda x: (x[1], x[0]))
+# 끝나는 시간을 기준으로 정렬합니다.
+meetings.sort(key=lambda x: x[1])
 
-max_cnts, last_end = 0, -1
+# 끝나는 시간이 증가하는 순으로 보며
+# 겹치지 않는 경우에 해당하는 회의를
+# 전부 선택합니다.
 
-for start, end in meetings:
-    if start >= last_end:
-        max_cnts += 1
-        last_end = end
+# 가장 최근에 잡힌 회의가 끝난 시간을 기록하며 진행합니다.
+last_e, max_cnt = -1, 0
+for s, e in meetings:
+    # 이전 회의와 겹치지 않는다면
+    # 해당 회의를 선택해줍니다. 
+    if last_e <= s:
+        last_e = e
+        max_cnt += 1
 
-print(len(meetings) - max_cnts)
+# 전체 회의 수에서
+# 최대로 진행할 수 있는 회의 수를 제외하면
+# 답이 됩니다.
+print(n - max_cnt)
