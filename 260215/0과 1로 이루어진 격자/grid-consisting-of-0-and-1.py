ad53e48131e0df27bc328252ce8a0ed236df5a59
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-15 14:06:38 +0900

    Title: 0과 1로 이루어진 격자, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 260215/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.py
+++ 260215/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.py
@@ -1,20 +1,23 @@
 n = int(input())
-grid = [list(input()) for _ in range(n)]
+arr = [
+    list(map(int, input()))
+    for _ in range(n)
+]
 
-# Please write your code here.
-a = [[1 if grid[i][j] == '1' else 0 for j in range(n)] for i in range(n)]
 
-def flip(x, y):
-    for i in range(x + 1):
-        for j in range(y + 1):
-            a[i][j] ^= 1
-
-ans = 0
-
+# 뒤에서부터 선택을 진행합니다.
+# 현재 위치에서 꼭 눌러야만
+# 문제 조건을 만족시킬 수 있다면
+# 눌러주고 넘어갑니다.
+cnt = 0
 for i in range(n - 1, -1, -1):
     for j in range(n - 1, -1, -1):
-        if a[i][j] == 1:
-            flip(i, j)
-            ans += 1
+        # arr[i][j]가 1이면 누릅니다.
+        if arr[i][j] == 1:
+            cnt += 1
+            for k in range(0, i + 1):
+                for l in range(0, j + 1):
+                    arr[k][l] = 1 - arr[k][l]
 
-print(ans)
+
+print(cnt)
