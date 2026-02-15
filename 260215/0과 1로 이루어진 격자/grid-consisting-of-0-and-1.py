8faffefe512cf7525bcc438f4b79bee2f06add74
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-15 13:53:56 +0900

    Title: 0과 1로 이루어진 격자, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260215/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.py
@@ -0,0 +1,20 @@
+n = int(input())
+grid = [list(input()) for _ in range(n)]
+
+# Please write your code here.
+a = [[1 if grid[i][j] == '1' else 0 for j in range(n)] for i in range(n)]
+
+def flip(x, y):
+    for i in range(x + 1):
+        for j in range(x + 1):
+            a[i][j] ^= 1
+
+ans = 0
+
+for i in range(n - 1, -1, -1):
+    for j in range(n - 1, -1, -1):
+        if a[i][j] == 1:
+            flip(i, j)
+            ans += 1
+
+print(ans)
