e8aab2a9cb1778d3a775126f7c19f96157817ef7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-21 14:30:37 +0900

    Title: 천 개의 정거장, Time: 835ms, Memory: 45MB, Status: Passed - Codetree

--- 260221/천 개의 정거장/thousand-stops.py
+++ 260221/천 개의 정거장/thousand-stops.py
@@ -1,68 +1,59 @@
-A, B, N = map(int, input().split())
+INF = (10**17) + 1
 
-bus_fare = []
-stop_count = []
-bus_stops = []
+# 변수 선언 및 입력:
+m = 1000 # 서로 다른 정점의 수
+a, b, n = tuple(map(int, input().split()))
+graph = [
+    [(INF, 0)] * (m + 1)         # (비용, 시간)을 기록
+    for _ in range(m + 1)
+]
+dist = [(INF, 0)] * (m + 1)      # (비용, 시간)을 기록
+visited = [False] * (m + 1)
 
-for _ in range(N):
-    fare, count = map(int, input().split())
-    bus_fare.append(fare)
-    stop_count.append(count)
-    bus_stops.append(list(map(int, input().split())))
+for i in range(1, m + 1):
+    # 자기 자신은 비용과 시간이 전혀 소요되지 않음
+    graph[i][i] = (0, 0)
 
-# Please write your code here.
-import heapq
+# 그래프를 인접행렬로 표현
+for _ in range(n):
+    cost, stop_num = tuple(map(int, input().split()))
+    
+    stops = list(map(int, input().split()))
 
-POINT_MAX = 1000
-node_id = POINT_MAX + 1  
+    for i in range(stop_num):
+        for j in range(i + 1, stop_num):
+            graph[stops[i]][stops[j]] = min(graph[stops[i]][stops[j]],
+                                            (cost, j - i))
 
-ride_id = [] 
-for i in range(N):
-    cnt = stop_count[i]
-    ids = list(range(node_id, node_id + cnt))
-    node_id += cnt
-    ride_id.append(ids)
+# 시작위치에는 dist값을 0으로 설정
+dist[a] = (0, 0)
 
-V = node_id  
-adj = [[] for _ in range(V)]
+# O(|V|^2) 다익스트라 코드
+for _ in range(m):
+    # V개의 정점 중 
+    # 아직 방문하지 않은 정점 중
+    # dist값이 가장 작은 정점을 찾아줍니다.
+    min_index = -1
+    for i in range(1, m + 1):
+        if visited[i]:
+            continue
+        
+        if min_index == -1 or dist[min_index] > dist[i]:
+            min_index = i
 
-# add edges
-for i in range(N):
-    fare = bus_fare[i]
-    stops = bus_stops[i]
-    ids = ride_id[i]
-    cnt = len(stops)
+    # 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
+    visited[min_index] = True
+    min_cost, min_time = dist[min_index]
 
-    for p in range(cnt):
-        cur_ride = ids[p]
-        s = stops[p]
+    # 최솟값에 해당하는 정점에 연결된 간선들을 보며
+    # 시작점으로부터의 최단거리 값을 갱신해줍니다.
+    for i in range(1, m + 1):
+        cost, time = graph[min_index][i]
+        dist[i] = min(dist[i], (min_cost + cost, min_time + time))
 
-        adj[s].append((cur_ride, fare, 0))
+# 만약 도달이 불가능하다면 -1 -1이 답이 됩니다.
+if dist[b] == (INF, 0):
+    dist[b] = (-1, -1)
 
-        adj[cur_ride].append((s, 0, 0))
-
-        if p + 1 < cnt:
-            adj[cur_ride].append((ids[p + 1], 0, 1))
-
-INF = (10 ** 30, 10 ** 30)
-dist = [INF] * V
-dist[A] = (0, 0)
-
-pq = [(0, 0, A)] 
-while pq:
-    c, t, u = heapq.heappop(pq)
-    if (c, t) != dist[u]:
-        continue
-    if u == B: 
-        break
-    for v, dc, dt in adj[u]:
-        nc, nt = c + dc, t + dt
-        if (nc, nt) < dist[v]:
-            dist[v] = (nc, nt)
-            heapq.heappush(pq, (nc, nt, v))
-
-ans = dist[B]
-if ans == INF:
-    print("-1 -1")
-else:
-    print(ans[0], ans[1])
+min_cost, min_time = dist[b]
+print(min_cost, min_time)
