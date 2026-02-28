b51f56110d8a16874fb40a889ca92eebc1175be3
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-02-28 16:35:25 +0900

    Title: 간선 없애기, Time: 97ms, Memory: 18MB, Status: Passed - Codetree

--- 260228/간선 없애기/eliminate-edges.py
+++ 260228/간선 없애기/eliminate-edges.py
@@ -1,68 +1,112 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-
-# Please write your code here.
 import heapq
+import sys
 
-INF = 10 ** 30
+INT_MAX = sys.maxsize
 
-adj = [[] for _ in range(n + 1)]
-for idx, (u, v, w) in enumerate(edges):
-    adj[u].append((v, w))
-    adj[v].append((u, w))
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 
-def dijkstra(start: int):
-    dist = [INF] * (n + 1)
-    dist[start] = 0
-    pq = [(0, start)]
+graph = [[] for _ in range(n + 1)]
+pq = []
+
+dist = [0] * (n + 1)
+path = [0] * (n + 1)
+
+ans = 0
+
+# 그래프를 인접리스트로 표현합니다.
+# 양방향 그래프이므로
+# 양쪽에 추가해줘야함에 유의합니다.
+for _ in range(m):
+    x, y, z = tuple(map(int, input().split()))
+    graph[x].append((y, z))
+    graph[y].append((x, z))
+
+
+# 지워진 간선을 제외하고
+# Dijkstra 알고리즘을 진행합니다.
+def dijkstra(removed_edge):
+    # 그래프에 있는 모든 노드들에 대해
+    # 초기값을 전부 아주 큰 값으로 설정
+    for i in range(1, n + 1):
+        dist[i] = INT_MAX
+
+    # 시작위치에는 dist값을 0으로 설정
+    dist[1] = 0
+
+    # 우선순위 큐에 시작점을 넣어줍니다.
+    # 거리가 가까운 곳이 먼저 나와야 하며
+    # 해당 지점이 어디인지에 대한 정보도 필요하므로
+    # (거리, 정점 번호) 형태로 넣어줘야 합니다.
+    heapq.heappush(pq, (0, 1))
+
+    # O(|E|log|V|) 다익스트라 코드
+    # 우선순위 큐에
+    # 원소가 남아있다면 계속 진행해줍니다.
     while pq:
-        d, u = heapq.heappop(pq)
-        if d != dist[u]:
+        # 가장 거리가 가까운 정보를 받아온 뒤, 원소를 제거해줍니다.
+        min_dist, min_index = heapq.heappop(pq)
+
+        # 우선순위 큐를 이용하면
+        # 같은 정점의 원소가 
+        # 여러 번 들어가는 문제가 발생할 수 있어
+        # min_dist가 최신 dist[min_index]값과 다르다면
+        # 계산할 필요 없이 패스해줍니다.
+        if min_dist != dist[min_index]:
             continue
-        for v, w in adj[u]:
-            nd = d + w
-            if nd < dist[v]:
-                dist[v] = nd
-                heapq.heappush(pq, (nd, v))
-    return dist
 
-dist1 = dijkstra(1)
-distN = dijkstra(n)
-D = dist1[n]
+        # 최솟값에 해당하는 정점에 연결된 간선들을 보며
+        # 시작점으로부터의 최단거리 값을 갱신해줍니다.
+        for target_index, target_dist in graph[min_index]:
+            # 만약 지워진 간선이라면
+            # 사용하지 못합니다.
+            if removed_edge == (min_index, target_index) or \
+               removed_edge == (target_index, min_index):
+               continue
+            
+            # 현재 위치에서 연결된 간선으로 가는 것이 더 작다면
+            new_dist = dist[min_index] + target_dist
+            if dist[target_index] > new_dist:
+                # 값을 갱신해주고, 우선순위 큐에 해당 정보를 넣어줍니다.
+                dist[target_index] = new_dist
+                heapq.heappush(pq, (new_dist, target_index))
 
-sp_out = [[] for _ in range(n + 1)]  
-dag_edges = [] 
+                # path값을 갱신해줍니다.
+                path[target_index] = min_index
 
-for idx, (u, v, w) in enumerate(edges):
-    if dist1[u] + w == dist1[v] and dist1[u] + w + distN[v] == D:
-        sp_out[u].append((v, idx))
-        dag_edges.append((u, v, idx))
-    elif dist1[v] + w == dist1[u] and dist1[v] + w + distN[u] == D:
-        sp_out[v].append((u, idx))
-        dag_edges.append((v, u, idx))
 
-nodes = list(range(1, n + 1))
-nodes.sort(key=lambda x: dist1[x])
+# 간선이 지워지지 않은 상태에서
+# 최단거리를 구해줍니다.
+dijkstra((-1, -1))
 
-cnt1 = [0] * (n + 1)
-cnt1[1] = 1
-for u in nodes:
-    if cnt1[u] == 0:
-        continue
-    for v, _ in sp_out[u]:
-        cnt1[v] += cnt1[u]
+# 도착지에서 시작하여
+# 시작점가 나오기 전까지
+# path를 따라 움직여줍니다.
+x = n
+vertices = []
+vertices.append(x)
+while x != 1:
+    x = path[x]
+    vertices.append(x)
 
-total = cnt1[n] 
+# 초기 최단거리를 기록합니다.
+orig_dist = dist[n]
 
-cnt2 = [0] * (n + 1)
-cnt2[n] = 1
-for u in reversed(nodes):
-    for v, _ in sp_out[u]:
-        cnt2[u] += cnt2[v]
+# 최단거리에 해당하는
+# 간선을 하나 잡아 삭제한 뒤
+# 최단거리를 구해봅니다.
+# 그 중 최대로 변한 정도를 갱신해줍니다.
+length = len(vertices)
+for i in range(length - 1, 0, -1):
+    x = vertices[i]
+    y = vertices[i - 1]
+    # 최단거리를 구해줍니다.
+    # 이때 사용 불가능한 간선을 같이 적어줍니다.
+    dijkstra((x, y))
 
-answer = 0
-for u, v, _ in dag_edges:
-    if cnt1[u] * cnt2[v] == total:
-        answer += 1
+    # 최단거리가 바뀌었다면
+    # 답을 갱신해줍니다.
+    if dist[n] != orig_dist:
+        ans += 1
 
-print(answer)
+print(ans)
