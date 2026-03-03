6c47bb5bff88ed8a656ad54a87d07d159094e007
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-03-04 08:53:18 +0900

    Title: 행렬로 주어진 간선, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260304/행렬로 주어진 간선/edge-given-by-matrix.py
+++ 260304/행렬로 주어진 간선/edge-given-by-matrix.py
@@ -1,20 +1,26 @@
+# 변수 선언 및 입력:
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
+graph = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
+for i in range(1, n + 1):
+    graph[i] = [0] + list(map(int, input().split()))
+    
+# i -> i로는 갈 수 있다고 전부 표시를 해줘야 합니다.
+for i in range(1, n + 1):
+    graph[i][i] = 1
 
-# Please write your code here.
-reach = [row[:] for row in graph]
+for k in range(1, n + 1): # 확실하게 거쳐갈 정점을 1번부터 N번까지 순서대로 정의합니다.
+    for i in range(1, n + 1): # 고정된 k에 대해 모든 쌍 (i, j)를 살펴봅니다.
+        for j in range(1, n + 1):
+            # i -> k, k -> j로 가능 길이 있다면
+            # i -> j도 가능하다는 뜻입니다.
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
 
-for i in range(n):
-    reach[i][i] = 1
-
-for k in range(n):
-    for i in range(n):
-        if reach[i][k] == 0:
-            continue
-
-        for j in range(n):
-            if reach[k][j]:
-                reach[i][j] = 1
-
-for i in range(n):
-    print(*reach[i])
+# 모든 쌍에 대한 이동 가능 결과를 출력합니다.
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(graph[i][j], end=" ")
+    print()
