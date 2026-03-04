9739283a1539890175b051714b05542d34ce9e0b
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-03-05 08:58:08 +0900

    Title: 이동 가능한 점들의 쌍, Time: 352ms, Memory: 20MB, Status: Passed - Codetree

--- 260305/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
+++ 260305/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -1,49 +1,50 @@
-N, M, P, Q = map(int, input().split())
+import sys
 
-# Please write your code here.
-INF = 10 ** 18
+INT_MAX = sys.maxsize
 
-dist = [[INF] * (N + 1) for _ in range(N + 1)]
-for i in range(1, N + 1):
+# 변수 선언 및 입력:
+n, m, p, q = tuple(map(int, input().split()))
+
+# dist 초기값을 전부 아주 큰 값으로 설정
+dist = [
+    [INT_MAX] * (n + 1)
+    for _ in range(n + 1)
+]
+
+# 자기 자신으로 가는 값은 0으로 설정해줘야 함에 유의합니다.
+for i in range(1, n + 1):
     dist[i][i] = 0
 
-for _ in range(M):
-    u, v, w = map(int, input().split())
-    if w < dist[u][v]:
-        dist[u][v] = w
+# 그래프를 인접행렬로 표현
+for _ in range(m):
+    x, y, z = tuple(map(int, input().split()))
+    dist[x][y] = z
 
-queries = [tuple(map(int, input().split())) for _ in range(Q)]
+for k in range(1, n + 1): # 확실하게 거쳐갈 정점을 1번부터 N번까지 순서대로 정의합니다.
+    for i in range(1, n + 1): # 고정된 k에 대해 모든 쌍 (i, j)를 살펴봅니다.
+        for j in range(1, n + 1):
+            # i에서 j로 가는 거리가 k를 경유해 가는 것이 더 좋다면
+            # dist[i][j]값을 갱신해줍니다.
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-for k in range(1, N + 1):
-    dk = dist[k]
-    for i in range(1, N + 1):
-        dik = dist[i][k]
-        if dik == INF:
-            continue
-        di = dist[i]
-        base = dik
-        # j 루프
-        for j in range(1, N + 1):
-            nd = base + dk[j]
-            if nd < di[j]:
-                di[j] = nd
+ans = 0
+cnt = 0
 
-count = 0
-total_cost = 0
+# q개의 질문에 대해 최단 거리를 답변합니다.
+for _ in range(q):
+    x, y = tuple(map(int, input().split()))
 
-for a, b in queries:
-    best = INF
-    da = dist[a]
-    for r in range(1, P + 1):
-        if da[r] == INF or dist[r][b] == INF:
-            continue
-        cand = da[r] + dist[r][b]
-        if cand < best:
-            best = cand
+    distance = INT_MAX
+    # 모든 p개의 빨간 점에 대해, 이동 가능한지 확인합니다.
+    for i in range(1, p + 1):
+        distance = min(distance, dist[x][i] + dist[i][y])
 
-    if best != INF:
-        count += 1
-        total_cost += best
+    # 만약 이동 불가능하다면, 넘어갑니다.
+    if distance >= INT_MAX:
+        continue
+        
+    ans += distance
+    cnt += 1
 
-print(count)
-print(total_cost)
+print(cnt)
+print(ans)
