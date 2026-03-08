d601ab1c4a7f01295ba679fe8e276810e44b5f87
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-03-09 08:55:53 +0900

    Title: 트리의 지름, Time: 433ms, Memory: 45MB, Status: Passed - Codetree

--- 260309/트리의 지름/diameter-of-tree.py
+++ 260309/트리의 지름/diameter-of-tree.py
@@ -3,7 +3,7 @@ edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
 
 # Please write your code here.
 import sys
-sys.recursionlimit(200000)
+sys.setrecursionlimit(200000)
 
 graph = [[] for _ in range(n + 1)]
 
