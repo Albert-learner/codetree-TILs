2a842388d4816e5e21fb6beb8eecc30044fd538e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-03-16 08:59:28 +0900

    Title: 트리 최적의 노드, Time: 964ms, Memory: 141MB, Status: Passed - Codetree

--- 260316/트리 최적의 노드/tree-optimal-node.py
+++ 260316/트리 최적의 노드/tree-optimal-node.py
@@ -1,36 +1,52 @@
+import sys
+sys.setrecursionlimit(100000)
+
+# 변수 선언 및 입력:
 n = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dist = [0] * (n + 1)
+max_dist = 0
+last_node = 0
 
-# Please write your code here.
-from collections import deque
+# n - 1개의 간선 정보를 입력받습니다.
+for _ in range(n - 1):
+    x, y = tuple(map(int, input().split()))
+    edges[x].append(y)
+    edges[y].append(x)
 
-graph = [[] for _ in range(n + 1)]
-for a, b in edges:
-    graph[a].append(b)
-    graph[b].append(a)
 
-def BFS(start):
-    dist = [-1] * (n + 1)
-    dist[start] = 0
-    q = deque([start])
+# 모든 노드의 정점을 탐색하는 DFS를 진행합니다.
+def dfs(x):
+    global max_dist, last_node
+    
+    for y in edges[x]:
+        # 이미 방문한 정점이면 스킵합니다.
+        if visited[y]: 
+            continue
 
-    while q:
-        cur = q.popleft()
-        for nxt in graph[cur]:
-            if dist[nxt] == -1:
-                dist[nxt] = dist[cur] + 1
-                q.append(nxt)
+        visited[y] = True
+        dist[y] = dist[x] + 1
 
-    far_node = start
-    far_dist = 0
-    for i in range(1, n + 1):
-        if dist[i] > far_dist:
-            far_dist = dist[i]
-            far_node = i
+        # 현재 정점을 기준으로 가장 먼 노드를 찾습니다.
+        if dist[y] > max_dist:
+            max_dist = dist[y]
+            last_node = y
 
-    return far_node, far_dist
+        dfs(y)
 
-u, _ = BFS(1)
-v, diameter = BFS(u)
 
-print((diameter + 1) // 2)
+# DFS를 통해 가장 먼 노드를 찾습니다.
+visited[1] = True
+dfs(1)
+
+# 가장 먼 노드에서 시작해 다시 한번 DFS를 돌려 트리의 가장 긴 거리를 찾습니다.
+for i in range(1, n + 1):
+    visited[i] = False
+    dist[i] = 0
+
+visited[last_node] = True
+dfs(last_node)
+
+# 거리의 중간값을 출력합니다.
+print((max_dist + 1) // 2)
