ed4c741b895c62bf2d377ac6085fdba3ef8c81e9
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-03-21 15:38:27 +0900

    Title: 트리 경유, Time: 646ms, Memory: 29MB, Status: Passed - Codetree

--- 260321/트리 경유/a-way-through-the-tree.py
+++ 260321/트리 경유/a-way-through-the-tree.py
@@ -1,25 +1,31 @@
-n, q = map(int, input().split())
-dest = [int(input()) for _ in range(q)]
+# 변수 선언 및 입력:
+n, q = tuple(map(int, input().split()))
+colored = [0] * (n + 1)
 
-# Please write your code here.
-occupied = set()
 
-for x in dest:
-    path = []
-    cur = x
+def last_colored_node(dest):
+    # 가는 길에 색칠된 노드가 없다면 0을 반환합니다.
+    ret = 0
 
-    while cur >= 1:
-        path.append(cur)
-        cur //= 2
+    while dest:
+        # 해당 위치의 노드가 색칠되었다면 반환할 값을 갱신합니다.
+        if colored[dest]: 
+            ret = dest
 
-    path.reverse()
-    blocked = 0
-    for node in path:
-        if node in occupied:
-            blocked = node
-            break
+        # dest에서 2를 나눠줄 때마다 자신의 부모 노드로 가게 됩니다.
+        dest //= 2
 
-    print(blocked)
+    return ret
 
-    if blocked == 0:
-        occupied.add(x)
+
+# q개의 줄에 걸쳐 노드를 입력받습니다.
+for _ in range(q):
+    dest = int(input())
+
+    # 입력받은 노드를 따라 갈 때, 색칠된 노드가 있는지 확인합니다.
+    ans = last_colored_node(dest)
+    print(ans)
+
+    # 만약 색칠된 노드가 없었다면 dest 노드에 색칠해줍니다.
+    if ans == 0: 
+        colored[dest] = 1
