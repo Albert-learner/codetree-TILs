957226319f78bb6975c4dd74b65d800668dbfa79
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-03-30 09:57:10 +0900

    Title: 정점 순회, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/정점 순회/node-traversal.py
+++ 260330/정점 순회/node-traversal.py
@@ -1,49 +1,49 @@
-n, s, d = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
-
-# Please write your code here.
 import sys
-sys.setrecursionlimit(200000)
+sys.setrecursionlimit(100000)
 
-if n == 1:
-    print(0)
-    sys.exit()
+# 변수 선언 및 입력:
+n, s, d = tuple(map(int, input().split()))
+edges = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dist = [0] * (n + 1)
 
-graph = [[] for _ in range(n + 1)]
-for a, b in edges:
-    graph[a].append(b)
-    graph[b].append(a)
+ans = 0
 
-parent = [0] * (n + 1)
-order = []
-stack = [s]
-parent[s] = -1
+# n - 1개의 간선 정보를 입력받습니다.
+for _ in range(n - 1):
+    x, y = tuple(map(int, input().split()))
 
-while stack:
-    cur = stack.pop()
-    order.append(cur)
-    for nxt in graph[cur]:
-        if nxt == parent[cur]:
+    # 간선 정보를 인접리스트에 넣어줍니다.
+    edges[x].append(y)
+    edges[y].append(x)
+
+
+# DFS를 통해 연결된 모든 정점을 순회합니다.
+# 동시에 dp값을 계산해줍니다.
+def dfs(x):
+    # 노드 x에 연결된 간선을 살펴보며 전부 방문해줍니다.
+    for y in edges[x]:
+        # 만약 y번 정점을 이미 방문했다면 스킵합니다.
+        if visited[y]: 
             continue
 
-        parent[nxt] = cur
-        stack.append(nxt)
+        visited[y] = True
+        dfs(y)
 
-max_depth = [0] * (n + 1)
+        # 가장 멀리 있는 리프노드까지의 거리를 저장합니다.
+        dist[x] = max(dist[x], 1 + dist[y])
 
-for cur in reversed(order):
-    for nxt in graph[cur]:
-        if nxt == parent[cur]:
-            continue
 
-        max_depth[cur] = max(max_depth[cur], max_depth[nxt] + 1)
+# 루트를 시작으로 DFS를 진행하며 값을 갱신합니다.
+visited[s] = True
+dfs(s)
 
-need_edges = 0
-for node in range(1, n + 1):
-    if node == s:
+# s번 정점을 제외한 리프까지의 거리가 d 이상인 정점의 개수를 구합니다.
+for i in range(1, n + 1):
+    if i == s: 
         continue
+    if dist[i] >= d: 
+        ans += 1
 
-    if max_depth[node] >= d:
-        need_edges += 1
-
-print(need_edges * 2)
+# s번 정점을 제외한 정답은 거리가 d이상인 정점의 개수 * 2가 됩니다.
+print(ans * 2)
