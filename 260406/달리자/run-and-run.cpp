41fc3283096e8d4a80f2a06271805c084c18eaf2
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-06 09:16:28 +0900

    Title: 달리자, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/달리자/run-and-run.cpp
+++ 260406/달리자/run-and-run.cpp
@@ -1,30 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#define MAX_N 100
+
 using namespace std;
 
-int main() 
-{
-    int N;
-    cin >> N;
-    vector<int> A_people(N);
-    vector<int> B_people(N);
-    for (int i = 0; i < N; i++) 
-    {
-        cin >> A_people[i] >> B_people[i];
-    }
+int n;
 
-    // Please write your code here.
-    int min_dists = 0, dists;
-    for(int i = 0; i < N; i++)
-    {
-        if(A_people[i] > B_people[i])
-        {
-            dists = A_people[i] - B_people[i];
-            A_people[i] -= dists;
-            A_people[i + 1] += dists;
-            min_dists += dists;
+int a[MAX_N], b[MAX_N];
+int ans;
+
+int main() {
+    // 입력
+    cin >> n;
+    for(int i = 0 ; i < n; i++)
+        cin >> a[i];
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+  
+    // 입력으로 주어진 사람 수를 보고
+    // 최소 어느 만큼의 거리를 달려야 하는지 확인합니다.
+    for(int i = 0; i < n; i++) {
+        if(a[i] > b[i]) {
+            // 최소 a[i] - b[i]명의 사람들은 오른쪽으로 달려야 합니다.
+            int num = a[i] - b[i];
+            a[i] -= num;
+            a[i + 1] += num;
+            ans += num;
         }
     }
 
-    cout << min_dists;
+    cout << ans;
+
     return 0;
 }
