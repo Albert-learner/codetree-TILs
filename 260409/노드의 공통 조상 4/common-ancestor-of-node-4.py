cdde7770a5402d132afed01207537092154be36e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-09 08:59:02 +0900

    Title: 노드의 공통 조상 4, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/노드의 공통 조상 4/common-ancestor-of-node-4.py
+++ 260409/노드의 공통 조상 4/common-ancestor-of-node-4.py
@@ -1,64 +1,65 @@
-n = int(input())
-
-edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
-
-q = int(input())
-
-queries = [tuple(map(int, input().split())) for _ in range(q)]
-
-# Please write your code here.
 import sys
-sys.setrecursionlimit(200000)
+sys.setrecursionlimit(100000)
 
-graph = [[] for _ in range(n + 1)]
-for a, b in edges:
-    graph[a].append(b)
-    graph[b].append(a)
-
-LOG = (n).bit_length()
-depth = [0] * (n + 1)
-parent = [[0] * (n + 1) for _ in range(LOG)]
+# 변수 선언 및 입력:
+n = int(input())
+edges = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
+parent = [0] * (n + 1)
+depth = [0] * (n + 1)
 
-stack = [1]
+# n - 1개의 에지 정보를 입력받습니다.
+for _ in range(n - 1):
+    x, y = tuple(map(int, input().split()))
+
+    edges[x].append(y)
+    edges[y].append(x)
+
+
+# 트리 순회를 진행합니다.
+# 동시에 depth를 기록해줍니다.
+def dfs(x):
+    # 노드 x의 자식들을 살펴봅니다.
+    for y in edges[x]:
+        # 이미 방문한 노드라면 패스합니다.
+        if visited[y]:
+            continue
+
+        # depth값을 갱신해주며
+        # 재귀적으로 탐색합니다.
+        visited[y] = True
+        depth[y] = depth[x] + 1
+        parent[y] = x
+        dfs(y)
+
+
+# dfs 탐색을 진행하며
+# 각 노드의 depth를 계산합니다.
 visited[1] = True
-parent[0][1] = 0
-depth[1] = 0
+depth[1] = 1
+dfs(1)
 
-while stack:
-    u = stack.pop()
-    for v in graph[u]:
-        if not visited[v]:
-            visited[v] = True
-            depth[v] = depth[u] + 1
-            parent[0][v] = u
-            stack.append(v)
+# q개의 쿼리가 주어집니다.
+q = int(input())
+for _ in range(q):
+    # 가장 가까운 공통 조상을 구할 두 노드의 번호가 주어집니다.
+    a, b = tuple(map(int, input().split()))
+   
+    # Step 1.
+    # 두 노드 a, b의 depth를 비교하며
+    # depth가 더 큰 쪽을 위로 올리는 것을 반복하며 두 노드의 depth를 맞춰줍니다.
+    while depth[a] != depth[b]:
+        if depth[a] > depth[b]:
+            a = parent[a]
+        else:
+            b = parent[b]
 
-for j in range(1, LOG):
-    for i in range(1, n + 1):
-        parent[j][i] = parent[j - 1][parent[j - 1][i]]
+    # Step 2.
+    # 두 노드 a, b가 일치해질떄까지
+    # 한 칸씩 위로 올라갑니다.
+    while a != b:
+        a = parent[a]
+        b = parent[b]
 
-def lca(a, b):
-    if depth[a] < depth[b]:
-        a, b = b, a
-
-    diff = depth[a] - depth[b]
-    bit = 0
-    while diff:
-        if diff & 1:
-            a = parent[bit][a]
-        diff >>= 1
-        bit += 1
-
-    if a == b:
-        return a
-
-    for j in range(LOG - 1, -1, -1):
-        if parent[j][a] != parent[j][b]:
-            a = parent[j][a]
-            b = parent[j][b]
-
-    return parent[0][a]
-
-for a, b in queries:
-    print(lca(a, b))
+    # LCA 값을 출력합니다.
+    print(a)
