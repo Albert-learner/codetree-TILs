ddde320e017d34cda783cc2090915d08c5392ac1
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-13 08:58:55 +0900

    Title: 트리 위에 색칠된 정점 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/트리 위에 색칠된 정점 수/count-colored-node.py
+++ 260413/트리 위에 색칠된 정점 수/count-colored-node.py
@@ -1,73 +1,146 @@
+import sys
+sys.setrecursionlimit(100000)
+
+MAX_H = 17
+
+# 변수 선언 및 입력:
 n = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(n - 1)]
+edges = [[] for _ in range(n + 1)]
+is_colored = [False] * (n + 1)
+visited = [False] * (n + 1)
+depth = [0] * (n + 1)
+
+# 루트 노드에서 i번 노드까지의 경로 상에서 색칠된 노드의 개수를 의미합니다.
+colored_num_from_root = [0] * (n + 1)
+
+# parent[h][i] : i번 노드에서 2^h번 부모를 따라 위로 올라갔을 때의 노드 번호를 관리합니다.
+parent = [
+    [0] * (n + 1)
+    for _ in range(MAX_H + 1)
+]
+
+# n - 1개의 에지 정보를 입력받습니다.
+for _ in range(n - 1):
+    x, y = tuple(map(int, input().split()))
+
+    edges[x].append(y)
+    edges[y].append(x)
 
 k = int(input())
-colored = [int(input()) for _ in range(k)]
+# k개의 색칠된 노드 번호를 입력받습니다.
+for _ in range(k):
+    x = int(input())
+    is_colored[x] = True
 
-q = int(input())
-queries = [tuple(map(int, input().split())) for _ in range(q)]
+# 트리 순회를 진행합니다.
+# 동시에 depth, colored_num_from_root를 기록해줍니다.
+def dfs(x):
+    # 노드 x의 자식들을 살펴봅니다.
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
 
-# Please write your code here.
-import sys
-sys.setrecursionlimit(200000)
+            # depth값을 갱신해주며
+            # 재귀적으로 탐색합니다.
+            depth[y] = depth[x] + 1
 
-graph = [[] for _ in range(n + 1)]
-for a, b in edges:
-    graph[a].append(b)
-    graph[b].append(a)
+            if is_colored[y]: 
+                colored_num_from_root[y] = colored_num_from_root[x] + 1
+            else:
+                colored_num_from_root[y] = colored_num_from_root[x]
 
-is_colored = [0] * (n + 1)
-for x in colored:
-    is_colored[x] = 1
+            # 이때 y번 노드에서 1번(=2^0)번 부모를 따라 위로 올라갔을 때의 
+            # 노드 번호는 x가 됩니다.
+            parent[0][y] = x
 
-LOG = (n).bit_length()
-parent = [[0] * (n + 1) for _ in range(LOG)]
-depth = [0] * (n + 1)
-prefix = [0] * (n + 1)
-visited = [False] * (n + 1)
+            dfs(y)
 
-stack = [1]
-visited[1] = True
-depth[1] = 0
-parent[0][1] = 0
-prefix[1] = is_colored[1]
 
-while stack:
-    u = stack.pop()
-    for v in graph[u]:
-        if visited[v]:
-            continue
-
-        visited[v] = True
-        parent[0][v] = u
-        depth[v] = depth[u] + 1
-        prefix[v] = prefix[u] + is_colored[v]
-        stack.append(v)
-
-for j in range(1, LOG):
-    for i in range(1, n + 1):
-        parent[j][i] = parent[j - 1][parent[j - 1][i]]
-
-def lca(a, b):
+# a와 b의 LCA를 구한 뒤, LCA와 a, b의
+# colored_num_from_root를 바탕으로 a와 b사이의 색칠된 노드의 개수를 구합니다.
+def get_num(a, b):
+    # Step 0.
+    # 노드 a의 높이가 더 같거나 커지도록 만들어줍니다.
     if depth[a] < depth[b]:
         a, b = b, a
 
-    diff = depth[a] - depth[b]
-    for j in range(LOG):
-        if diff & (1 << j):
-            a = parent[j][a]
+    a_save, b_save = a, b
 
+    # Step 1.
+    # 노드 a의 높이를 노드 b의 높이까지 끌어올려줍니다.
+    # 이는 십진수를 이진수로 빠르게 바꾸는 방법 중 하나인
+    # 2의 거듭제곱 중 가장 큰 값을 계속 빼주는 원리를 이용합니다.
+    for h in range(MAX_H, -1, -1):
+        # a의 높이를 b에 맞추기 위해
+        # 아직 2^h 만큼 더 끌어 올려줘도 된다면
+        # 그만큼 올라갔을 때의 조상 값으로 변경해줍니다.
+        if depth[a] - depth[b] >= (1 << h):
+            a = parent[h][a]
+
+    # 이미 a와 b가 일치한다면
+    # 그 값이 lca입니다.
+    # a, b, lca를 이용해 a노드와 b노드 사이의
+    # 색칠된 노드의 개수를 찾습니다.
     if a == b:
-        return a
+        # (a, b) 사이에 색칠된 노드의 개수는
+        # 루트로부터 a, b까지 각각 색칠된 노드의 합을 구한 뒤
+        # 겹치는 값인 루트->lca까지 색칠된 노드의 수를 뺀 값이 됩니다.
+        # 이때 lca가 색칠되었는지 여부에 따라 값을 잘 조정해줘야 함에 유의합니다.
+        lca = a
+        if is_colored[lca]:
+            return colored_num_from_root[a_save] + colored_num_from_root[b_save] - 2 * colored_num_from_root[lca] + 1
+        else:
+            return colored_num_from_root[a_save] + colored_num_from_root[b_save] - 2 * colored_num_from_root[lca]
 
-    for j in range(LOG - 1, -1, -1):
-        if parent[j][a] != parent[j][b]:
-            a = parent[j][a]
-            b = parent[j][b]
+    # Step 2.
+    # 두 노드 a, b가 일치해지기 직전까지
+    # 위로 올라갑니다.
+    # 이때 역시 해당 높이에 도달하기 위해 점프해야 하는 값을 x라 한다면
+    # x라는 십진수를 이진수로 빠르게 바꾸는 방법 중 하나인
+    # 2의 거듭제곱 중 가장 큰 값을 계속 빼주는 원리를 이용합니다.
+    # 단, 그 시점을 찾기 위해
+    # 두 노드가 일치해지기 바로 직전까지 최대한 올라가는 방법으로 진행합니다.
+    for h in range(MAX_H, -1, -1):
+        if parent[h][a] != parent[h][b]:
+            a = parent[h][a]
+            b = parent[h][b]
 
-    return parent[0][a]
+    # 이제 a, b는
+    # 같아지기 바로 직전의 위치까지 올라온 것이므로
+    # 바로 위 부모노드 값이 lca입니다.
+    # a, b, lca를 이용해 a노드와 b노드 사이의
+    # 색칠된 노드의 개수를 찾습니다.
+    # (a, b) 사이에 색칠된 노드의 개수는
+    # 루트로부터 a, b까지 각각 색칠된 노드의 합을 구한 뒤
+    # 겹치는 값인 루트->lca까지 색칠된 노드의 수를 뺀 값이 됩니다.
+    # 이때 lca가 색칠되었는지 여부에 따라 값을 잘 조정해줘야 함에 유의합니다.
+    lca = parent[0][a]
+    if is_colored[lca]:
+        return colored_num_from_root[a_save] + colored_num_from_root[b_save] - 2 * colored_num_from_root[lca] + 1
+    else:
+        return colored_num_from_root[a_save] + colored_num_from_root[b_save] - 2 * colored_num_from_root[lca]
 
-for a, b in queries:
-    c = lca(a, b)
-    ans = prefix[a] + prefix[b] - 2 * prefix[c] + is_colored[c]
-    print(ans)
+
+# dfs 탐색을 진행하며
+# 각 노드의 depth와 parent값을 계산합니다.
+depth[1] = 1
+visited[1] = True
+dfs(1)
+
+# parent값을 갱신해줍니다.
+for h in range(1, MAX_H + 1):
+    # 0 ~ h - 1까지는 이미 계산이 되어 있다는 전제 하에
+    # 정점 i로부터 2^h번 위로 올라갔을 때의 위치는
+    # 정점 i로부터 2^(h-1)번 위로 올라간 뒤, 
+    # 다시 그 노드로부터 2^(h-1)번 위로 다시 올라가면 
+    # O(1)에 바로 구해집니다.
+    for i in range(1, n + 1):
+        parent[h][i] = parent[h - 1][parent[h - 1][i]]
+
+# q개의 질의에 대해 답을 구해줍니다.
+q = int(input())
+for _ in range(q):
+    a, b = tuple(map(int, input().split()))
+
+    # a부터 b까지 색칠된 노드의 수를 구합니다.
+    print(get_num(a, b))
