6ba462313c7630834f847e05e671fbc4a38e9a4a
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-14 08:42:49 +0900

    Title: 집합의 원소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260414/집합의 원소/elements-of-a-set.py
+++ 260414/집합의 원소/elements-of-a-set.py
@@ -1,31 +1,42 @@
-n, m = map(int, input().split())
-queries = [list(map(int, input().split())) for _ in range(m)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+uf = [0] * (n + 1)
 
-# Please write your code here.
-parents = list(range(n + 1))
+# 초기 uf 값을 설정합니다.
+for i in range(1, n + 1):
+    uf[i] = i
 
+
+# x의 대표 번호를 찾아줍니다.
 def find(x):
-    if parents[x] != x:
-        parents[x] = find(parents[x])
-    
-    return parents[x]
+    # x가 루트 노드라면 x값을 반환합니다.
+    if uf[x] == x:
+        return x
+    # x가 루트 노드가 아니라면
+    # x의 부모인 uf[x]에서 탐색을 더 진행한 뒤
+    # 찾아낸 루트 노드를 uf[x]에 넣어줌과 동시에
+    # 해당 노드값을 반환합니다.
+    uf[x] = find(uf[x])
+    return uf[x]
 
-def union(a, b):
-    fa = find(a)
-    fb = find(b)
 
-    if fa == fb:
-        return
+# x, y가 같은 집합이 되도록 합니다.
+def union(x, y):
+    # x, y의 대표 번호를 찾은 뒤
+    # 연결해줍니다.
+    X, Y = find(x), find(y)
+    uf[X] = Y
 
-    if fa < fb:
-        parents[fb] = fa
-    else:
-        parents[fa] = fb
 
-for cmd, a, b in queries:
-    if cmd == 0:
+for _ in range(m):
+    q_type, a, b = tuple(map(int, input().split()))
+
+    # 합치는 명령입니다.
+    if q_type == 0:
         union(a, b)
+    # 같은 집합에 있는지를 판단하는 명령입니다.
     else:
+        # 같은 집합인 경우에는 1을 출력합니다.
         if find(a) == find(b):
             print(1)
         else:
