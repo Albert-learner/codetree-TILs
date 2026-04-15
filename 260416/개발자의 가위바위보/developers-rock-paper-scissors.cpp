674ac965065da116f4f4ad58fcc20c4fb4f42a1c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-16 08:49:01 +0900

    Title: 개발자의 가위바위보, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260416/개발자의 가위바위보/developers-rock-paper-scissors.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int N;
+    cin >> N;
+    vector<pair<int, int>> games(N);
+    for(int i = 0; i < N; i++)
+        cin >> games[i].first >> games[i].second;
+
+    int max_wins = 0, wins = 0;
+    for(auto [a, b]: games)
+    {
+        if(a == 1 && b == 2)
+            wins += 1;
+        else if(a == 2 && b == 3)
+            wins += 1;
+        else if(a == 3 && b == 1)
+            wins += 1;
+    }
+
+    max_wins = max(max_wins, wins);
+    wins = 0;
+    for(auto [a, b]: games)
+    {
+        if(a == 1 && b == 3)
+            wins += 1;
+        else if(a == 3 && b == 2)
+            wins += 1;
+        else if(a == 2 && b == 1)
+            wins += 1;
+    }
+
+    max_wins = max(max_wins, wins);
+    cout << max_wins;
+    return 0;
+}
