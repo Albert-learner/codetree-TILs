181b5b3ce86614371bce76658b1592f1de4a91ef
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-19 11:48:43 +0900

    Title: 최소 간선의 크기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260419/최소 간선의 크기/minimum-edge-size.py
@@ -0,0 +1,38 @@
+n, m = map(int, input().split())
+a, b = map(int, input().split())
+
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+from_v, to_v, satisfaction = zip(*edges)
+
+# Please write your code here.
+parent = list(range(n + 1))
+rank = [0] * (n + 1)
+
+def find(x):
+    while parent[x] != x:
+        parent[x] = parent[parent[x]]
+        x = parent[x]
+
+    return x
+
+def union(a, b):
+    fa, fb = find(a), find(b)
+
+    if fa == fb:
+        return
+
+    if rank[fa] < rank[fb]:
+        parent[fa] = fb
+    elif rank[fa] > rank[fb]:
+        parent[fb] = fa
+    else:
+        parent[fb] = fa
+        rank[ra] += 1
+
+edges.sort(key = lambda x: -x[2])
+
+for u, v, s in edges:
+    union(u, v)
+    if find(a) == find(b):
+        print(s)
+        br
