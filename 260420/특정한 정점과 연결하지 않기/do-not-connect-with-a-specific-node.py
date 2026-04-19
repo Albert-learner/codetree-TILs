4fe842561c93407b974987aca0166d04b6d7bbe7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-20 08:35:08 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260420/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.py
+++ 260420/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.py
@@ -1,47 +1,84 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-A, B, k = map(int, input().split())
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 
-# Please write your code here.
-parent = list(range(n + 1))
-size = [1] * (n + 1)
+# 번호별 그룹을 관리합니다.
+uf = [0] * (n + 1)
 
-def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
+# 그룹별 노드 개수를 관리합니다.
+sz = [0] * (n + 1)
 
-    return x
+# 각 그룹을 방문했는지 여부를 기록합니다.
+visited = [False] * (n + 1)
 
-def union(a, b):
-    fa, fb = find(a), find(b)
+group_list = []
 
-    if fa == fb:
-        return
-
-    parent[fb] = fa
-    size[fa] += size[fb]
-
-for a, b in edges:
-    union(a, b)
-
-rootA = find(A)
-rootB = find(B)
-
-roots = set()
+# 초기 uf, sz 값을 설정합니다.
 for i in range(1, n + 1):
-    roots.add(find(i))
+    uf[i] = i
+    sz[i] = 1
 
-candidates = []
-for r in roots:
-    if r == rootA or r == rootB:
+
+# x의 대표 번호를 찾아줍니다.
+def find(x):
+    # x가 루트 노드라면 x값을 반환합니다.
+    if uf[x] == x:
+        return x
+    # x가 루트 노드가 아니라면
+    # x의 부모인 uf[x]에서 탐색을 더 진행한 뒤
+    # 찾아낸 루트 노드를 uf[x]에 넣어줌과 동시에
+    # 해당 노드값을 반환합니다.
+    uf[x] = find(uf[x])
+    return uf[x]
+
+
+# x, y가 같은 집합이 되도록 합니다.
+def union(x, y):
+    # x, y의 대표 번호를 찾은 뒤
+    # 연결해줍니다.
+    X = find(x)
+    Y = find(y)
+    if X != Y:
+        uf[X] = Y
+        sz[Y] += sz[X]
+
+
+# m개의 간선 정보를 입력받습니다.
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+
+    # 주어진 모든 정점을 연결시킵니다.
+    union(x, y)
+
+a, b, k = tuple(map(int, input().split()))
+   
+A = find(a)
+B = find(b)
+# 모든 노드의 그룹을 정리합니다.
+# B번 노드와도, A번 노드와도 연결되지 않은 그룹들을 ans에 저장합니다.
+for i in range(1, n + 1):
+    I = find(i)
+
+    # A, B와 같은 그룹이라면 패스합니다.
+    if I == A or I == B: 
         continue
-    candidates.append(size[r])
 
-candidates.sort(reverse = True)
+    # 이미 선택된 그룹이라면 패스합니다.
+    if visited[I]: 
+        continue
 
-answer = size[rootA]
-for i in range(min(k, len(candidates))):
-    answer += candidates[i]
+    # 아직 연결되지 않은 그룹들의 크기를 넣어줍니다.
+    visited[I] = True
+    group_list.append(sz[I])
 
-print(answer)
+# a와 연결될 수 있는 최대 노드의 개수를 구합니다.
+sz_a = sz[A]
+
+# 내림차순으로 정렬합니다.
+group_list.sort(reverse=True)
+
+# a와 가능한 많은 그룹을 연결시킵니다. 최대 k개까지의 그룹을 연결시킬 수 있습니다.
+for i in range(min(k, len(group_list))):
+    sz_a += group_list[i]
+
+# 최대 k개까지의 그룹을 연결시킨 후 크기의 총합을 출력합니다.
+print(sz_a)
