90a37ad35e43d99a3c8b2a73ed6c60eeb23196fb
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-21 08:42:11 +0900

    Title: 가장 많은 정점과 연결, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/가장 많은 정점과 연결/connect-with-the-most-vertices.py
+++ 260421/가장 많은 정점과 연결/connect-with-the-most-vertices.py
@@ -1,52 +1,62 @@
-n, m, k = map(int, input().split())
+# 변수 선언 및 입력:
+n, m, k = tuple(map(int, input().split()))
 
-# Note: Adding 0 at start to match 1-based indexing of C++
-vertex = [0] + list(map(int, input().split()))
+uf = [0] * (n + 1)
 
-edges = [tuple(map(int, input().split())) for _ in range(m)]
+visited = [0] * (n + 1)
 
-# Please write your code here.
-parent = list(range(n + 1))
-size = [1] * (n + 1)
+cost_list = []
+
+# min_cost의 초기값이 주어집니다.
+min_cost = [0] + list(map(int, input().split()))
+
+# uf 값을 초기값을 적어줍니다.
+for i in range(1, n + 1):
+    uf[i] = i
+
 
 def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
 
-    return x
 
-def union(a, b):
-    fa, fb = find(a), find(b)
+def union(x, y):
+    X = find(x)
+    Y = find(y)
+    uf[X] = Y
 
-    if fa == fb:
-        return
+    # 각 그룹의 최소비용을 관리합니다.
+    min_cost[Y] = min(min_cost[Y], min_cost[X])
 
-    if size[fa] < size[fb]:
-        fa, fb = fb, fa
 
-    parent[fb] = fa
-    size[fa] += size[fb]
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
 
-for a, b in edges:
-    union(a, b)
+    # 이미 연결되어 있으므로 union을 진행합니다.
+    union(x, y)
 
-comp_min = {}
+# 각 그룹들에 대해 min_cost값을 모아줍니다.
 for i in range(1, n + 1):
-    r = find(i)
-    if r not in comp_min:
-        comp_min[r] = vertex[i]
-    else:
-        comp_min[r] = min(comp_min[r], vertex[i])
+    I = find(i)
 
-mins = list(comp_min.values())
-if len(mins) == 1:
-    answer = 0
-else:
-    min_val = min(mins)
-    answer = sum(mins) + (len(mins) - 2) * min_val
+    # 중복되는 그룹은 패스합니다.
+    if visited[I]:
+        continue
+    
+    visited[I] = True
+    cost_list.append(min_cost[I])
 
-if answer <= k:
-    print(answer)
+# min_cost를 오름차순으로 정렬합니다.
+cost_list.sort()
+
+# 가장 작은 min_cost 쪽과 간선을 연결하는 것이 최선입니다.
+ans = 0
+for i in range(1, len(cost_list)):
+    ans += cost_list[0] + cost_list[i]
+
+if ans <= k:
+    print(ans)
 else:
     print("NO")
