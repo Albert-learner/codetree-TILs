a137155b768bf8758f12716a2d0be67e36d0a814
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-27 09:05:48 +0900

    Title: 격자 위의 연결, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/격자 위의 연결/connections-on-the-grid.py
+++ 260427/격자 위의 연결/connections-on-the-grid.py
@@ -1,62 +1,62 @@
-n, m = map(int, input().split())
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = []
 
-horizontal = [list(map(int, input().split())) for _ in range(n)]
-vertical = [list(map(int, input().split())) for _ in range(n - 1)]
+uf = [0] * (n * m + 1)
 
-# Please write your code here.
-parent = list(range(n * m))
-rank = [0] * (n * m)
+# n개의 줄에 대해 가로 간선을 모두 입력받습니다.
+for i in range(1, n + 1):
+    costs = list(map(int, input().split()))
+    for j in range(1, m):
+        # 각 칸을 1부터 n * m까지 번호를 매겨줍니다.
+        x = (i - 1) * m + j
+        y = (i - 1) * m + j + 1
+
+        edges.append((costs[j - 1], x, y))
+
+# n - 1개의 줄에 대해 세로 간선을 모두 입력받습니다.
+for i in range(1, n):
+    costs = list(map(int, input().split()))
+    for j in range(1, m + 1):
+        # 각 칸을 1부터 n * m까지 번호를 매겨줍니다.
+        x = (i - 1) * m + j
+        y = i * m + j
+
+        edges.append((costs[j - 1], x, y))
+
 
 def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
 
-    return x
 
-def union(a, b):
-    fa, fb = find(a), find(b)
+def union(x, y):
+    X = find(x)
+    Y = find(y)
+    uf[X] = Y
 
-    if fa == fb:
-        return False
 
-    if rank[fa] < rank[fb]:
-        fa, fb = fb, fa
-
-    parent[fb] = fa
-    if rank[fa] == rank[fb]:
-        rank[fa] += 1
-
-    return True
-
-def node_id(r, c):
-    return r * m + c
-
-edges = []
-for r in range(n):
-    for c in range(m - 1):
-        a = node_id(r, c)
-        b = node_id(r, c + 1)
-        w = horizontal[r][c]
-        edges.append((w, a, b))
-
-for r in range(n - 1):
-    for c in range(m):
-        a = node_id(r, c)
-        b = node_id(r + 1, c)
-        w = vertical[r][c]
-        edges.append((w, a, b))
-
+# cost 순으로 오름차순 정렬을 진행합니다.
 edges.sort()
 
-answer = 0
-used = 0
-for w, a, b in edges:
-    if union(a, b):
-        answer += w
-        used += 1
+# uf 값을 초기값을 적어줍니다.
+for i in range(1, n * m + 1):
+    uf[i] = i
 
-        if used == n * m - 1:
-            break
+# cost가 낮은 간선부터 순서대로 보며
+# 아직 두 노드가 연결이 되어있지 않을 경우에만
+# 해당 간선을 선택하고 두 노드를 합쳐주면서
+# mst를 만들어줍니다.
+ans = 0
+for cost, x, y in edges:
+    # x, y가 연결되어 있지 않다면
+    if find(x) != find(y):
+        # 해당 간선은 MST에 속하는 간선이므로
+        # 답을 갱신해주고
+        # 두 노드를 연결해줍니다.
+        ans += cost
+        union(x, y)
 
-print(answer)
+print(ans)
