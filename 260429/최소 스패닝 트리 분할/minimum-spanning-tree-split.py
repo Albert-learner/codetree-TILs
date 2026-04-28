f12f707e2281ab9150dfce5b2704902670ce3546
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-04-29 08:17:41 +0900

    Title: 최소 스패닝 트리 분할, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260429/최소 스패닝 트리 분할/minimum-spanning-tree-split.py
+++ 260429/최소 스패닝 트리 분할/minimum-spanning-tree-split.py
@@ -1,38 +1,47 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
 
-# Please write your code here.
-parent = list(range(n + 1))
+uf = [0] * (n + 1)
+
 
 def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
 
-    return x
 
-def union(a, b):
-    fa, fb = find(a), find(b)
+def union(x, y):
+    X, Y = find(x), find(y)
+    uf[X] = Y
 
-    if fa == fb:
-        return False
 
-    parent[fb] = fa
-    return True
+# cost 순으로 오름차순 정렬을 진행합니다.
+edges.sort(key=lambda x: x[2])
 
-edges.sort(key = lambda x: x[2])
+# uf 값을 초기값을 적어줍니다.
+for i in range(1, n + 1):
+    uf[i] = i
 
-total = 0
-max_edge = 0
-used = 0
+# cost가 낮은 간선부터 순서대로 보며
+# 아직 두 노드가 연결이 되어있지 않을 경우에만
+# 해당 간선을 선택하고 두 노드를 합쳐주면서
+# mst를 만들어줍니다.
+ans = 0
+last_cost = 0 # 마지막 간선의 가중치를 저장합니다.
+for x, y, cost in edges:
+    # x, y가 연결되어 있지 않다면
+    if find(x) != find(y):
+        # 해당 간선은 MST에 속하는 간선이므로
+        # 답을 갱신해주고
+        # 두 노드를 연결해줍니다.
+        ans += cost
+        last_cost = cost
+        union(x, y)
 
-for a, b, w in edges:
-    if union(a, b):
-        total += w
-        max_edge = max(max_edge, w)
-        used += 1
-
-        if used == n - 1:
-            break
-
-print(total - max_edge)
+# MST에서 가중치가 가장 큰 마지막 간선을 제외한 것이 답이 됩니다.
+print(ans - last_cost)
