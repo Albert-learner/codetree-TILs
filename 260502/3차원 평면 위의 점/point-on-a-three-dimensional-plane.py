489326e62936624bce6576592b25ff37ea1c11da
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-02 11:58:58 +0900

    [Passed] 3차원 평면 위의 점 (1141ms, 46MB)

--- 260502/3차원 평면 위의 점/point-on-a-three-dimensional-plane.py
+++ 260502/3차원 평면 위의 점/point-on-a-three-dimensional-plane.py
@@ -1,47 +1,73 @@
+# 변수 선언 및 입력:
 n = int(input())
-points = [tuple(map(int, input().split())) for _ in range(n)]
-px = sorted([(points[i][0], i) for i in range(n)])
-py = sorted([(points[i][1], i) for i in range(n)])
-pz = sorted([(points[i][2], i) for i in range(n)])
 
-# Please write your code here.
+# n개의 점의 정보를 전부 입력받습니다.
+points = []
+for i in range(1, n + 1):
+    x, y, z = tuple(map(int, input().split()))
+    points.append((x, y, z, i))
+
 edges = []
 
-for arr in (px, py, pz):
-    for i in range(n - 1):
-        cost = abs(arr[i][0] - arr[i + 1][0])
-        u = arr[i][1]
-        v = arr[i + 1][1]
-        edges.append((cost, u, v))
-
-edges.sort()
-
-parent = list(range(n))
+uf = [0] * (n + 1)
 
 def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
+    if uf[x] == x:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
 
-    return x
 
-def union(a, b):
-    fa, fb = find(a), find(b)
+def union(x, y):
+    X = find(x)
+    Y = find(y)
+    uf[X] = Y
 
-    if fa == fb:
-        return False
 
-    parent[fb] = fa
-    return True
+def push(p1, p2):
+    x1, y1, z1, idx1 = p1
+    x2, y2, z2, idx2 = p2
+    cost = min(min(abs(x1 - x2), abs(y1 - y2)), abs(z1 - z2))
+    edges.append((cost, idx1, idx2))
 
-answer = 0
-cnt = 0
 
-for cost, u, v in edges:
-    if union(u, v):
-        answer += cost
-        cnt += 1
-        if cnt == n - 1:
-            break
+# 모든 점을 x 순으로 정렬합니다.
+points.sort(key=lambda x : x[0])
+# 각 정렬된 점에서 연속한 순서대로 간선을 이어줍니다.
+for i in range(n - 1):
+    push(points[i], points[i + 1])
 
-print(answer)
+# 모든 점을 y 순으로 정렬합니다.
+points.sort(key=lambda x : x[1])
+# 각 정렬된 점에서 연속한 순서대로 간선을 이어줍니다.
+for i in range(n - 1):
+    push(points[i], points[i + 1])
+
+# 모든 점을 z 순으로 정렬합니다.
+points.sort(key=lambda x : x[2])
+# 각 정렬된 점에서 연속한 순서대로 간선을 이어줍니다.
+for i in range(n - 1):
+    push(points[i], points[i + 1])
+
+# cost 순으로 오름차순 정렬을 진행합니다.
+edges.sort()
+
+# uf 값을 초기값을 적어줍니다.
+for i in range(1, n + 1):
+    uf[i] = i
+
+# cost가 낮은 간선부터 순서대로 보며
+# 아직 두 노드가 연결이 되어있지 않을 경우에만
+# 해당 간선을 선택하고 두 노드를 합쳐주면서
+# mst를 만들어줍니다.
+ans = 0
+for cost, x, y in edges:
+    # x, y가 연결되어 있지 않다면
+    if find(x) != find(y):
+        # 해당 간선은 MST에 속하는 간선이므로
+        # 답을 갱신해주고
+        # 두 노드를 연결해줍니다.
+        ans += cost
+        union(x, y)
+
+print(ans)
