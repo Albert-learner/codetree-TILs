8be3f37f092931e8babed5cb7ff8449ff4d8c2d3
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-05 22:57:51 +0900

    [Passed] 두 점으로 만드는 정사각형 (8ms, 0MB)

---
+++ trail2/두 점으로 만드는 정사각형/square-made-of-two-points.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    // Please write your code here.
+    int x1, y1, x2, y2, a1, b1, a2, b2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> a1 >> b1 >> a2 >> b2;
+
+    int min_x = min({x1, x2, a1, a2}), min_y = min({y1, y2, b1, b2});
+    int max_x = max({x1, x2, a1, a2}), max_y = max({y1, y2, b1, b2});
+
+    int square_len = max({int(abs(max_x - min_x)), int(abs(max_y - min_y))});
+    cout << int(pow(square_len, 2));
+    return 0;
+}
