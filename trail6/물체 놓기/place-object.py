5b054783b8fce3a17e8e7160f542428435990552
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-05 22:50:24 +0900

    [Passed] 물체 놓기 (90ms, 18MB)

--- trail6/물체 놓기/place-object.py
+++ trail6/물체 놓기/place-object.py
@@ -1,54 +1,29 @@
-import sys
-
-INT_MAX = sys.maxsize
-
-# 변수 선언 및 입력:
 n = int(input())
-graph = [
-    [0] * (n + 1)
-    for _ in range(n + 1)
-]
-visited = [False] * (n + 1)
+direct = [int(input()) for _ in range(n)]
+cost = [list(map(int, input().split())) for _ in range(n)]
 
-# 그래프에 있는 모든 노드들에 대해
-# 초기값을 전부 아주 큰 값으로 설정
-dist = [INT_MAX] * (n + 1) 
+# Please write your code here.
+visited = [False] * n
 
-# 그래프를 인접행렬로 표현
-# 더미 노드(0번 노드)를 만들어서 1번부터 n번까지 노드를 (직접 물체를 놓는 비용) 으로 연결합니다.
-for i in range(1, n + 1):
-    graph[0][i] = int(input())
+min_cost = direct[:]
 
-for i in range(1, n + 1):
-    graph[i] = [0] + list(map(int, input().split()))
+answer = 0
 
-# 시작위치(더미노드)에는 dist값을 0으로 설정
-dist[0] = 0
+for _ in range(n):
+    u = -1
+    best = 10 ** 18
 
-# O(|V|^2) 프림 코드
-ans = 0
-for i in range(0, n + 1):
-    # V개의 정점 중 
-    # 아직 방문하지 않은 정점 중
-    # dist값이 가장 작은 정점을 찾아줍니다.
-    min_index = -1
-    for j in range(0, n + 1):
-        if visited[j]:
-            continue
-        
-        if min_index == -1 or dist[min_index] > dist[j]:
-            min_index = j
+    for i in range(n):
+        if not visited[i] and min_cost[i] < best:
+            best = min_cost[i]
+            u = i
 
-    # 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
-    visited[min_index] = True
+    visited[u] = True
+    answer += best
 
-    # mst 값을 갱신해줍니다.
-    ans += dist[min_index]
+    for v in range(n):
+        if not visited[v] and cost[u][v] < min_cost[v]:
+            min_cost[v] = cost[u][v]
 
-    # 최솟값에 해당하는 정점에 연결된 간선들을 보며
-    # 시작점으로부터의 최솟값을 갱신해줍니다.
-    for j in range(1, n + 1):
-        dist[j] = min(dist[j], graph[min_index][j])
-
-print(ans)
+print(answer)
 
