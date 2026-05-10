164a3f60230cfd0fd06345883ebe83dd5b451bac
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-10 10:02:26 +0900

    [Passed] 최소 스패닝 트리 7 (375ms, 36MB)

--- trail6/최소 스패닝 트리 7/minimum-spanning-tree-7.py
+++ trail6/최소 스패닝 트리 7/minimum-spanning-tree-7.py
@@ -1,66 +1,145 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
+import sys
+sys.setrecursionlimit(40000)
 
-# Please write your code here.
-from collections import deque
+import sys
+import heapq
 
-parent = list(range(n + 1))
+INT_MAX = sys.maxsize
 
-def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]
-        x = parent[x]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 
-    return x
+# mst를 위한 변수들
+graph = [[] for _ in range(n + 1)]
+pq = []
+mst_edges = []
 
-def union(a, b):
-    fa, fb = find(a), find(b)
+# Prim 진행시 각 노드가 MST에 연결되기 위해
+# 어느 노드에 연결하는 것이 최선인지를 관리합니다.
+dist_from = [0] * (n + 1)
 
-    if fa == fb:
-        return False
+# tree를 위한 변수들
+edge = [[] for _ in range(n + 1)]
+max_dist = 0
+last_node = 0
 
-    parent[fb] = fa
-    return True
+dist = [0] * (n + 1)
+visited = [False] * (n + 1)
 
-edges.sort(key = lambda x: x[2])
-mst_weight = 0
-mst_adj = [[] for _ in range(n + 1)]
-cnt = 0
+# 그래프를 인접리스트로 표현합니다.
+for _ in range(m):
+    x, y, z = tuple(map(int, input().split()))
+    graph[x].append((y, z))
+    graph[y].append((x, z))
 
-for u, v, w in edges:
-    if union(u, v):
-        mst_weight += w
-        mst_adj[u].append((v, w))
-        mst_adj[v].append((u, w))
-        cnt += 1
 
-        if cnt == n - 1:
-            break
+def mst():
+    # 그래프에 있는 모든 노드들에 대해
+    # 초기값을 전부 아주 큰 값으로 설정
+    for i in range(1, n + 1):
+        dist[i] = INT_MAX
 
-def bfs(start):
-    dist = [-1] * (n + 1)
-    dist[start] = 0
-    q = deque([start])
+    # 시작위치에는 dist값을 0으로 설정
+    dist[1] = 0
 
-    far_node = start
-    max_dist = 0
+    # 우선순위 큐에 시작점을 넣어줍니다.
+    # 거리가 가까운 곳이 먼저 나와야 하며
+    # 해당 지점이 어디인지에 대한 정보도 필요하므로
+    # (거리, 정점 번호) 형태로 넣어줘야 합니다.
+    heapq.heappush(pq, (0, 1))
 
-    while q:
-        u = q.popleft()
+    # O(|E|log|V|) 프림 코드
+    # 우선순위 큐에
+    # 원소가 남아있다면 계속 진행해줍니다.
+    total_dist = 0
+    while pq:
+        # 가장 거리가 가까운 정보를 받아온 뒤, 원소를 제거해줍니다.
+        min_dist, min_index = heapq.heappop(pq)
 
-        if dist[u] > max_dist:
-            max_dist = dist[u]
-            far_node = u
+        # 우선순위 큐를 이용하면
+        # 같은 정점의 원소가 
+        # 여러 번 들어가는 문제가 발생할 수 있어
+        # 이미 계산해본 적이 있는 경우라면
+        # 바로 패스해줍니다.
+        if visited[min_index]:
+            continue
 
-        for v, w in mst_adj[u]:
-            if dist[v] == -1:
-                dist[v] = dist[u] + w
-                q.append(v)
+        # visited 값을 true로 바꿔주고
+        # 답을 갱신해줍니다. 
+        visited[min_index] = True
 
-    return far_node, max_dist
+        # mst에 해당하는 간선들을 만들어줍니다.
+        if min_index != 1:
+            mst_edges.append((
+                min(min_index, dist_from[min_index]), 
+                max(min_index, dist_from[min_index]), 
+                dist[min_index]
+            ))
 
-x, _ = bfs(1)
-y, diameter = bfs(x)
+        total_dist += min_dist
 
-print(mst_weight)
-print(diameter)
+        # 최솟값에 해당하는 정점에 연결된 간선들을 보며
+        # 최솟값을 갱신해줍니다.
+        for target_index, target_dist in graph[min_index]:
+            # 현재 위치에서 연결된 간선으로 가는 것이 더 작다면
+            new_dist = target_dist
+            if dist[target_index] > new_dist:
+                # 값을 갱신해주고, 우선순위 큐에 해당 정보를 넣어줍니다.
+                dist[target_index] = new_dist
+                heapq.heappush(pq, (new_dist, target_index))
+
+                # target_index번 정점이 새로 MST에 들어오기 위해서는 
+                # min_index 정점과 연결되는 것이 최선임을 표시해줍니다.
+                dist_from[target_index] = min_index
+
+    return total_dist
+
+
+# 모든 노드의 정점을 탐색하는 DFS를 진행합니다.
+def dfs(x):
+    global max_dist, last_node
+
+    for y, d in edge[x]:
+        # 이미 방문한 정점이면 스킵합니다.
+        if visited[y]: 
+            continue
+
+        visited[y] = True
+        dist[y] = dist[x] + d
+
+        # 현재 정점을 기준으로 가장 먼 노드를 찾습니다.
+        if dist[y] > max_dist:
+            max_dist = dist[y]
+            last_node = y
+
+        dfs(y)
+
+
+def tree_diameter():
+    # n - 1개의 간선 정보를 인접리스트에 넣어줍니다.
+    for x, y, d in mst_edges:
+        edge[x].append((y, d))
+        edge[y].append((x, d))
+    
+    # DFS를 통해 가장 먼 노드를 찾습니다.
+    for i in range(1, n + 1):
+        visited[i] = False
+        dist[i] = 0
+
+    visited[1] = True
+    dfs(1)
+
+    # 가장 먼 노드에서 시작해 다시 한번 DFS를 돌려 트리의 가장 긴 거리를 찾습니다.
+    for i in range(1, n + 1):
+        visited[i] = False
+        dist[i] = 0
+
+    visited[last_node] = True
+    dfs(last_node)
+
+    # 트리의 가장 긴 거리를 출력합니다.
+    return max_dist
+
+
+print(mst())
+print(tree_diameter())
