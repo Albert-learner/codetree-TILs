efcd9bfe069657edf05ebdd4dfcdfde023734160
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-12 08:32:06 +0900

    [Passed] 친구의 키 2 (178ms, 30MB)

---
+++ trail6/친구의 키 2/height-of-friends-2.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+# Please write your code here.
+from collections import deque
+
+graph = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+
+for a, b in edges:
+    graph[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+
+for i in range(1, n + 1):
+    if indegree[i] == 0:
+        q.append(i)
+
+visited_cnt = 0
+while q:
+    cur = q.popleft()
+    visited_cnt += 1
+
+    for nxt in graph[cur]:
+        indegree[nxt] -= 1
+
+        if indegree[nxt] == 0:
+            q.append(nxt)
+
+if visited_cnt == n:
+    print("Consistent")
+else:
+    print("Inconsistent")
