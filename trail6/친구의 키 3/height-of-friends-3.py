c3dd254ead8346ee42b04432911e8896e7cae048
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-21 09:14:11 +0900

    [Passed] 친구의 키 3 (271ms, 26MB)

--- trail6/친구의 키 3/height-of-friends-3.py
+++ trail6/친구의 키 3/height-of-friends-3.py
@@ -1,35 +1,61 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-from_list, to_list = zip(*edges) if m > 0 else ([], [])
-
-# Please write your code here.
 import heapq
-from collections import deque
 
-graph = [[] for _ in range(n + 1)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = [[] for _ in range(n + 1)]
+
+# 진입차수를 관리합니다.
 indegree = [0] * (n + 1)
 
-for a, b in edges:
-    graph[a].append(b)
-    indegree[b] += 1
+# 모든 정점의 방문 여부를 관리합니다.
+visited = [False] * (n + 1)
 
+# 위상정렬을 위한 우선순위 큐를 관리합니다.
 pq = []
+
+ans = []
+
+# 인접리스트로 관리합니다.
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+
+    edges[x].append(y) 
+    indegree[y] += 1 # 진입차수를 갱신합니다.
+
+# 처음 indegree 값이 0인 곳이 시작점이 됩니다.
+# 이 노드들을 priority queue에 넣어줍니다.
 for i in range(1, n + 1):
-    if indegree[i] == 0:
+    if not indegree[i]:
         heapq.heappush(pq, i)
 
-answer = []
+# 위상정렬을 진행합니다.
+# priority queue에 원소가 남아있다면 계속 진행합니다.
 while pq:
-    cur = heapq.heappop(pq)
-    answer.append(cur)
+    # 가장 앞에 있는 원소를 뽑아줍니다.
+    x = heapq.heappop(pq)
+    
+    ans.append(x)
+    visited[x] = True
 
-    for nxt in graph[cur]:
-        indegree[nxt] -= 1
+    # x에서 갈 수 있는 모든 곳을 탐색합니다.
+    for y in edges[x]:
+        # 해당 노드의 indegree를 1만큼 감소시켜줍니다.
+        indegree[y] -= 1
 
-        if indegree[nxt] == 0:
-            heapq.heappush(pq, nxt)
+        # 비로소 indegree가 0이 되었다면
+        # priority queue에 새로 넣어줍니다.
+        if not indegree[y]:
+            heapq.heappush(pq, y)
 
-if len(answer) != n:
+# 모든 노드를 방문했다면 그래프 내에 사이클이 존재하지 않는다는 뜻입니다.
+is_cycle = False
+for i in range(1, n + 1):
+    if not visited[i]: 
+        is_cycle = True
+
+# 사이클이 존재한다면 가능한 답이 없다는 뜻이므로 -1을 출력합니다.
+if is_cycle:
     print(-1)
 else:
-    print(*answer)
+    for num in ans:
+        print(num, end=" ")
