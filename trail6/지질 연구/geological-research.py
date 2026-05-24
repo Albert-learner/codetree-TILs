603a35ff15c7ec81edffa9b29f028aedeb5eeb61
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-24 12:09:31 +0900

    [Passed] 지질 연구 (206ms, 23MB)

--- trail6/지질 연구/geological-research.py
+++ trail6/지질 연구/geological-research.py
@@ -1,47 +1,72 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-
-# Please write your code here.
 from collections import deque
 
-graph = [[] for _ in range(n + 1)]
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+edges = [[] for _ in range(n + 1)]
+
+# 진입차수를 관리합니다.
 indegree = [0] * (n + 1)
 
-for a, b in edges:
-    graph[a].append(b)
-    indegree[b] += 1
+# pressure[i] : i번 지층의 압력도를 관리합니다.
+pressure = [0] * (n + 1)
 
-pressure = [1] * (n + 1)
-
+# max_pressure[i] : i번 지층이 받는 압력 중 최대 압력입니다.
+# max_pressure_cnt[i] : i번 지층이 최대 압력으로 받는 지층 개수입니다.
 max_pressure = [0] * (n + 1)
-max_cnt = [0] * (n + 1)
+max_pressure_cnt = [0] * (n + 1)
 
+# 위상정렬을 위한 큐를 관리합니다.
 q = deque()
 
+# 인접리스트로 관리합니다.
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+
+    edges[x].append(y) 
+    indegree[y] += 1 # 진입차수를 갱신합니다.
+
+# 처음 indegree 값이 0인 곳이 시작점이 됩니다.
+# 이 노드들을 queue에 넣어줍니다.
 for i in range(1, n + 1):
-    if indegree[i] == 0:
+    if not indegree[i]:
         q.append(i)
 
+        # 아무 압력도 받지 않는 지층입니다.
+        # 해당 지층은 압력도가 1입니다.
+        pressure[i] = 1
+
+# 위상정렬을 진행합니다.
+# queue에 원소가 남아있다면 계속 진행합니다.
 while q:
-    cur = q.popleft()
+    # 가장 앞에 있는 원소를 뽑아줍니다.
+    x = q.popleft()
 
-    for nxt in graph[cur]:
-        p = pressure[cur]
+    # x에서 갈 수 있는 모든 곳을 탐색합니다.
+    for y in edges[x]:
+        # y번 압력의 정보를 갱신합니다.
+        if max_pressure[y] < pressure[x]:
+            max_pressure[y] = pressure[x]
+            max_pressure_cnt[y] = 1
+        elif max_pressure[y] == pressure[x]:
+            max_pressure_cnt[y] += 1
 
-        if p > max_pressure[nxt]:
-            max_pressure[nxt] = p
-            max_cnt[nxt] = 1
-        elif p == max_pressure[nxt]:
-            max_cnt[nxt] += 1
+        # 해당 노드의 indegree를 1만큼 감소시켜줍니다.
+        indegree[y] -= 1
 
-        indegree[nxt] -= 1
-
-        if indegree[nxt] == 0:
-            if max_cnt[nxt] >= 2:
-                pressure[nxt] = max_pressure[nxt] + 1
+        # 비로소 indegree가 0이 되었다면
+        # queue에 새로 넣어줍니다.
+        if not indegree[y]:
+            if max_pressure_cnt[y] == 1:
+                pressure[y] = max_pressure[y]
             else:
-                pressure[nxt] = max_pressure[nxt]
+                pressure[y] = max_pressure[y] + 1
 
-            q.append(nxt)
+            q.append(y)
+            
+# 모든 지층의 압력도 중에서
+# 가장 큰 값을 출력합니다.
+ans = 0
+for i in range(1, n + 1):
+    ans = max(ans, pressure[i])
 
-print(max(pressure))
+print(ans)
