778d81959845dbdd3d7ff419b071d404c0f719e5
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-05-28 08:19:01 +0900

    [Passed] 우선순위 건설 (88ms, 18MB)

--- trail6/우선순위 건설/priority-construction.py
+++ trail6/우선순위 건설/priority-construction.py
@@ -1,45 +1,64 @@
-n = int(input())
-t = []
-prev_build = [[] for _ in range(500)]
-prev_count = [0] * 500
-
-for i in range(n):
-    nums = list(map(int, input().split()))
-    t.append(nums[0])
-    prev_build[i] = [x - 1 for x in nums[1:-1]]
-    prev_count[i] = len(prev_build[i])
-
-# Please write your code here.
 from collections import deque
 
-graph = [[] for _ in range(n)]
-indegree = [0] * n
+# 변수 선언 및 입력:
+n = int(input())
+edges = [[] for _ in range(n + 1)]
 
-for i in range(n):
-    indegree[i] = prev_count[i]
+# 진입차수를 관리합니다.
+indegree = [0] * (n + 1)
 
-    for prev in prev_build[i]:
-        graph[prev].append(i)
+# total_times[i] : i번 건물이 완성되기까지 걸리는 최소 시간
+total_times = [0] * (n + 1)
 
-finish_time = [0] * n
+# setup_time[i] : i번 건물만 짓는데 걸리는 시간
+setup_time = [0] * (n + 1)
 
+# dp[i] : i번 일을 완료하기 위해 필요한 최소 시간을 관리합니다.
+dp = [0] * (n + 1)
+
+# 위상정렬을 위한 큐를 관리합니다.
 q = deque()
 
-for i in range(n):
-    if indegree[i] == 0:
-        finish_time[i] = t[i]
+# 인접리스트로 관리합니다.
+for i in range(1, n + 1):
+    setup_time[i], *nums, _ = list(map(int, input().split()))
+
+    for x in nums:
+        y = i
+        edges[x].append(y)
+        indegree[y] += 1 # 진입차수를 갱신합니다.
+
+# 처음 indegree 값이 0인 곳이 시작점이 됩니다.
+# 이 노드들을 queue에 넣어줍니다.
+for i in range(1, n + 1):
+    if not indegree[i]:
         q.append(i)
+        
+        # 바로 만들 수 있는 건물들은
+        # 완성 시간이 곧 짓는 시간입니다.
+        total_times[i] = setup_time[i]
 
+# 위상정렬을 진행합니다.
+# queue에 원소가 남아있다면 계속 진행합니다.
 while q:
-    cur = q.popleft()
+    # 가장 앞에 있는 원소를 뽑아줍니다.
+    x = q.popleft()
+    
+    # x에서 갈 수 있는 모든 곳을 탐색합니다.
+    for y in edges[x]:
+        # total_times 정보를 갱신합니다.
+        if total_times[y] < total_times[x] + setup_time[y]:
+            total_times[y] = total_times[x] + setup_time[y]
 
-    for nxt in graph[cur]:
-        finish_time[nxt] = max(finish_time[nxt], finish_time[cur] + t[nxt])
+        # 해당 노드의 indegree를 1만큼 감소시켜줍니다.
+        indegree[y] -= 1
 
-        indegree[nxt] -= 1
+        # 비로소 indegree가 0이 되었다면
+        # queue에 새로 넣어줍니다.
+        if not indegree[y]:
+            q.append(y)
 
-        if indegree[nxt] == 0:
-            q.append(nxt)
-
-for f_time in finish_time:
-    print(f_time)
+# n개의 노드가 각 건물이 완성되기까지
+# 걸리는 최소 시간을 차례대로 출력합니다.
+for i in range(1, n + 1):
+    print(total_times[i])
