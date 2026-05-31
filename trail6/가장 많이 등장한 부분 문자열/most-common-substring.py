9a6346c3bf6a2a349f21ee3bf89a8f7e3588194c
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-06-01 08:41:14 +0900

    [Passed] 가장 많이 등장한 부분 문자열 (115ms, 29MB)

--- trail6/가장 많이 등장한 부분 문자열/most-common-substring.py
+++ trail6/가장 많이 등장한 부분 문자열/most-common-substring.py
@@ -1,41 +1,65 @@
-l, S = input().split()
+# 변수 선언 및 입력:
+l, text = tuple(input().split())
 l = int(l)
 
-# Please write your code here.
-from collections import defaultdict
+n = len(text)
 
-n = len(S)
+# 2개의 polynomial rolling 해싱을 위한 p, m 값을 정의합니다.
+p = [31, 37]
+m = [int(1e9) + 7, int(1e9) + 9]
 
-MOD1 = 10 ** 9 + 7
-MOD2 = 10 ** 9 + 9
-BASE = 911382323
+# p^i, 값을 m으로 나눈 나머지를 관리합니다.
+p_pow = [
+    [0] * (n + 1)
+    for _ in range(2)
+]
 
-pow1 = [1] * (n + 1)
-pow2 = [1] * (n + 1)
+# 각 문자열이 몇번 나왔는지 정리합니다.
+freq = {}
 
-for i in range(n):
-    pow1[i + 1] = (pow1[i] * BASE) % MOD1
-    pow2[i + 1] = (pow2[i] * BASE) % MOD2
+ans = 0
 
-hash1 = [0] * (n + 1)
-hash2 = [0] * (n + 1)
 
-for i, ch in enumerate(S):
-    num = ord(ch) - ord('a') + 1
-    hash1[i + 1] = (hash1[i] * BASE + num) % MOD1
-    hash2[i + 1] = (hash2[i] * BASE + num) % MOD2
+# 소문자 알파벳을 수로 변경합니다.
+def to_int(c):
+    return ord(c) - ord('a') + 1
 
-cnt = defaultdict(int)
-answer = 0
 
-for i in range(n - l + 1):
-    h1 = (hash1[i + l] - hash1[i] * pow1[l]) % MOD1
-    h2 = (hash2[i + l] - hash2[i] * pow2[l]) % MOD2
+# p_pow 값을 계산합니다.
+# p_pow[i] = p^i % m
+for k in range(2):
+    # p_pow[i] = p^i % m
+    p_pow[k][0] = 1
+    for i in range(1, n + 1):
+        p_pow[k][i] = (p_pow[k][i - 1] * p[k]) % m[k]
 
-    key = (h1, h2)
-    cnt[key] += 1
+# text에서 구간 [0, l - 1]에 해당하는 해싱값을 계산합니다.
+t_h = [0, 0]
+for k in range(2):
+    for i in range(l):
+        t_h[k] = (t_h[k] + to_int(text[i]) * p_pow[k][l - 1 - i]) % m[k]
 
-    if cnt[key] > answer:
-        answer = cnt[key]
+# 해싱된 값을 map에 저장합니다.
+freq[(t_h[0], t_h[1])] = freq.get((t_h[0], t_h[1]), 0) + 1
+ans = max(ans, freq[(t_h[0], t_h[1])])
 
-print(answer)
+# text에서
+# 길이가 l인 부분문자열을 전부 잡아보며
+# 해싱값이 일치하는 위치를 찾습니다.
+ans = -1
+for i in range(1, n - l + 1):
+    for k in range(2):
+        # 이전 [i - 1, i + l - 2]에 해당하는 해싱값은 t_h에 있습니다.
+        # 이전 값(t_h)은 (text[i - 1] * p^(l - 1) + text[i] * p^(l - 2) + ... + text[i + l - 2] * 1) % m입니다.
+        # 이제 t_h * p - text[i - 1] * p^l + text[i + l - 1]를 계산하면
+        # 새로 계산을 원하는 해싱값인 (text[i] * p^(l - 1) + text[i + 1] * p(l - 2) + ... + text[i + l - 1] * 1) % m이 됩니다.
+        t_h[k] = (t_h[k] * p[k] - to_int(text[i - 1]) * p_pow[k][l] + to_int(text[i + l - 1])) % m[k]
+        # t_h값을 양수로 변환해줍니다.
+        if t_h[k] < 0:
+            t_h[k] += m[k]
+
+    # 해싱된 값을 map에 저장합니다.
+    freq[(t_h[0], t_h[1])] = freq.get((t_h[0], t_h[1]), 0) + 1
+    ans = max(ans, freq[(t_h[0], t_h[1])])
+
+print(ans)
