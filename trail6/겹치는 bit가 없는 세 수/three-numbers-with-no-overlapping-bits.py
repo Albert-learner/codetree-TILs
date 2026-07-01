a0e1f5baddf40c8a0ad8bf2642d64c02a00e0f14
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-07-02 08:40:41 +0900

    [Passed] 겹치는 bit가 없는 세 수 (55ms, 16MB)

--- trail6/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.py
+++ trail6/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.py
@@ -1,12 +1,17 @@
+# 변수 선언 및 입력:
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-from itertools import combinations
+# 모든 숫자에 대해 겹치지 않는 쌍을 찾습니다.
+# 맞을 경우 합을 더해줍니다.
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            # 숫자들의 비트가 겹치지 않는다면
+            # 세 숫자의 합과, 세 숫자의 or연산의 값이 같습니다.
+            if arr[i] + arr[j] + arr[k] == (arr[i] | arr[j] | arr[k]):
+                ans = max(ans, arr[i] + arr[j] + arr[k])
 
-max_cst = 0
-for a, b, c in combinations(arr, 3):
-    if (a & b) == 0 and (b & c) == 0 and (a & c) == 0:
-        max_cst = max(max_cst, a + b + c)
-
-print(max_cst)
+# 조건을 만족하는 경우 중 얻을 수 있는 최대 합을 출력합니다.
+print(ans)
