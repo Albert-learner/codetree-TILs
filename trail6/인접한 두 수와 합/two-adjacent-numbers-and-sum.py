29f9dcb24871bf82ef8447371b01a3dd3ffcaf2a
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-07-01 09:16:49 +0900

    [Passed] 인접한 두 수와 합 (81ms, 18MB)

--- trail6/인접한 두 수와 합/two-adjacent-numbers-and-sum.py
+++ trail6/인접한 두 수와 합/two-adjacent-numbers-and-sum.py
@@ -1,30 +1,45 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-prefix = [0] * (n + 1)
-for i in range(n):
-    prefix[i + 1] = prefix[i] + arr[i]
+prefix_sum = [0 for _ in range(n)]
 
-def range_sum(l, r):
-    return prefix[r + 1] - prefix[l]
+# dp[i][j] : [i, j] 구간에 있는 수들을 전부 하나로 합치는 동아
+#            얻을 수 있는 최대 점수를 기록합니다.
+dp = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
 
-dp = [[0] * n for _ in range(n)]
+# [i, j] 구간에 있는 수들의 합을 반환합니다.
+def GetSum(i, j):
+    return prefix_sum[j] - prefix_sum[i] + arr[i]
+   
+# 누적합을 이용하기 위해 미리 계산을 진행합니다.
+prefix_sum[0] = arr[0]
+for i in range(1, n):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
 
-for length in range(2, n + 1):
-    for l in range(n - length + 1):
-        r = l + length - 1
-        total = range_sum(l, r)
-        best = 0
+# dp는 미리 구해져 있는 작은 문제를 가지고 큰 문제를 풀어야 하므로
+# 이러한 유형의 경우 구간을 점점 넓혀가면서 dp값을 채워야만 합니다. 
+# 따라서 구간의 크기를 2부터 n까지 증가하게 미리 정해줍니다.
+for gap in range(2, n + 1):
+    # 구간의 시작위치 i를 정해줍니다.
+    for i in range(n - gap + 1):
+        # 구간의 크기와 시작 위치가 정해져 있기에
+        # 끝 위치는 자동으로 정해집니다.
+        j = i + gap - 1
 
-        for k in range(l, r):
-            left_sum = range_sum(l, k)
-            right_sum = total - left_sum
+        # [i, j]가 되기 위해
+        # 최종적으로 합쳐지는 두 수가
+        # 각각 [i, k], [k + 1, j]로부터 온 결과였을 경우의
+        # 최적의 비용을 구해 가능한 경우 중 최솟값을 갱신해줍니다.
+        # [i, k]까지 합쳐서 나오는 수는 arr[i] + .. + arr[k]이고
+        # [k + 1, j]까지 합쳐서 나오는 수는 a[k + 1] + .. + arr[j]이므로
+        # 두 수를 합쳤을 때 나오는 점수는 abs(sum[i..k] - sum[k+1..j])입니다.
+        # 시간을 줄이기 위해 누적합을 이용하여 O(1)에 계산합니다.
+        for k in range(i, j):
+            cost = dp[i][k] + dp[k + 1][j] + abs(GetSum(i, k) - GetSum(k + 1, j))
+            dp[i][j] = max(dp[i][j], cost)
 
-            score = dp[l][k] + dp[k + 1][r] + abs(left_sum - right_sum)
-            if score > best:
-                best = score
-
-        dp[l][r] = best
-
+# 모든 수를 합치면서 얻는 최대 점수를 출력합니다.
 print(dp[0][n - 1])
