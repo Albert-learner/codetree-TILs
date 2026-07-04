efb2aa927097220edb6b5e98d79f9640a7d3f330
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-07-04 13:04:23 +0900

    [Passed] 겹치지 않는 쌍의 수 (78ms, 18MB)

--- trail6/겹치지 않는 쌍의 수/number-of-non-overlapping-pairs.py
+++ trail6/겹치지 않는 쌍의 수/number-of-non-overlapping-pairs.py
@@ -1,26 +1,24 @@
 n = int(input())
-m = []
-groups = []
 
-for _ in range(n):
-    nums = list(map(int, input().split()))
-    m.append(nums[0])
-    groups.append(nums[1:])
+arr = [0 for _ in range(n)]
 
-# Please write your code here.
-masks = []
+# 각 그룹의 정보를 입력받습니다.
+for i in range(n):
+    infos = map(int, input().split()[1:])
+    for info in infos:
+        # 각 그룹의 정보를 비트로 관리합니다.
+        # x번 사람이 있을 경우
+        # (1 << x)에 해당하는 비트가 1이 됩니다.
+        arr[i] |= (1 << info)
 
-for group in groups:
-    mask = 0
-    for person in group:
-        mask |= 1 << (person - 1)
-    masks.append(mask)
-
-answer = 0
-
+# 모든 그룹에 대해 겹치지 않는 모든 쌍의 개수를 찾습니다.
+ans = 0
 for i in range(n):
     for j in range(i + 1, n):
-        if masks[i] & masks[j] == 0:
-            answer += 1
+        # 두 그룹에 속한 사람 번호가 겹치지 않는다는 뜻은
+        # 두 그룹을 and연산했을 때 0이 됨을 의미합니다.
+        if (arr[i] & arr[j]) == 0:
+            ans += 1
 
-print(answer)
+# 조건을 만족하는 그룹의 순서쌍의 개수를 출력합니다.
+print(ans)
