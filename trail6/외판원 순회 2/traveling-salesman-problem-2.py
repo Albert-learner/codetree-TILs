34dc50c0edbeec3ab1a39fb5dfce4c3fce724a56
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-07-05 14:59:30 +0900

    [Passed] 외판원 순회 2 (418ms, 31MB)

--- trail6/외판원 순회 2/traveling-salesman-problem-2.py
+++ trail6/외판원 순회 2/traveling-salesman-problem-2.py
@@ -1,43 +1,75 @@
-n = int(input())
-dist = [list(map(int, input().split())) for _ in range(n)]
-
-# Please write your code here.
 import sys
 
-INF = sys.maxsize
+INT_MAX = sys.maxsize
 
-dp = [[INF] * n for _ in range(1 << n)]
+# 변수 선언 및 입력:
+n = int(input())
+cost = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
+# dp[i][j] : 
+# 0번을 시작으로 겹치지 않게 방문한 위치를
+# x1, x2, ..., xk라 헀을 때 
+# 2^x1 + 2^x2 + ... + 2^xk 값이 i이고 (bitmask된 정수값이 i)
+# 그래서 현재 서 있는 위치가 j가 되었을 때
+# 가능한 최소 비용 
+dp = [
+    [0] * n
+    for _ in range(1 << n)
+]
+
+   
+# 최소 비용을 구하는 문제이기에
+# 초기값으로 아주 큰 값을 넣어줍니다.
+for i in range(1 << n):
+    for j in range(n):
+        dp[i][j] = INT_MAX
+
+# 초기조건은
+# 0번 지점을 시작으로 하며 현재 0번 위치에 서있으며
+# 지금까지 들어간 비용이 0원인 상태인
+# dp[1][0] = 0이 됩니다.
 dp[1][0] = 0
 
-for mask in range(1 << n):
-    for cur in range(n):
-        if dp[mask][cur] == INF:
+# 뿌려주는 방식의 dp를 진행합니다.
+# dp[i][j]가 계산이 되어있다는 가정하에서
+# 그 다음 상태값을 갱신합니다.
+for i in range(1 << n):
+    for j in range(n):
+        # j번 지점을 방문한게 정의상 불가능 하다면
+        # 패스합니다.
+        if ((i >> j) & 1) == 0:
             continue
 
-        if ((mask >> cur) & 1) == 0:
-            continue
-
-        for nxt in range(n):
-            if (mask >> nxt) & 1:
+        # 현재 j번에서 그 다음 위치로 k번 지점을 가게 되는 경우
+        # 상태값을 계산하여 최솟값을 갱신해줍니다.
+        for k in range(n):
+            # k번 지점을 이미 방문한 적이 있다면
+            # 중복 방문은 조건상 불가하므로 패스합니다.
+            if ((i >> k) & 1) == 1:
                 continue
-
-            if dist[cur][nxt] == 0:
+            
+            # j번에서 k번으로 가는 길이 없다면
+            # 패스합니다.
+            if cost[j][k] == 0:
                 continue
-
-            next_mask = mask | (1 << nxt)
-            dp[next_mask][nxt] = min(
-                dp[next_mask][nxt],
-                dp[mask][cur] + dist[cur][nxt]
+            
+            dp[i + (1 << k)][k] = min(
+                dp[i + (1 << k)][k], dp[i][j] + cost[j][k]
             )
 
-full = (1 << n) - 1
-answer = INF
-
-for last in range(1, n):
-    if dist[last][0] == 0:
+# 문제 특성상 최종적으로 0번으로 다시 돌아와야 끝이 나기에
+# 최종적으로 i번 위치에서 0번으로 이동을 끝냈을 때 가능한 최소 비용을 계산하여
+# 가능한 비용 중 최솟값을 갱신합니다.
+ans = INT_MAX
+for i in range(1, n):
+    # i번에서 1번으로 가는 길이 없다면 패스합니다.
+    if cost[i][0] == 0:
         continue
+    
+    # 최솟값을 갱신합니다.
+    ans = min(ans, dp[(1 << n) - 1][i] + cost[i][0])
 
-    answer = min(answer, dp[full][last] + dist[last][0])
-
-print(answer)
+print(ans)
