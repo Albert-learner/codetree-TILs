56cf0bb95b1207e075fe768692db5e9bd0f9bc3e
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2026-07-08 08:17:24 +0900

    [Passed] 물체 놓기 2 (144ms, 20MB)

--- trail6/물체 놓기 2/place-object-2.py
+++ trail6/물체 놓기 2/place-object-2.py
@@ -2,33 +2,34 @@ n, k = map(int, input().split())
 cost = [int(input()) for _ in range(n)]
 board = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-INF = 10 ** 18
-size = 1 << n
+INF = float('inf')
 
-dp = [INF] * size
-dp[0] = 0
+dp = [INF] * (1 << n)
+dp[0] = 0  # 아무것도 없는 초기 상태
 
-for mask in range(size):
+for mask in range(1 << n):
     if dp[mask] == INF:
         continue
 
-    for nxt in range(n):
-        if mask & (1 << nxt):
+    # 다음에 추가할 정점 j
+    for j in range(n):
+        if (mask >> j) & 1:  # 이미 물체가 있으면 skip
             continue
 
-        add_cost = cost[nxt]
+        next_mask = mask | (1 << j)
 
-        for cur in range(n):
-            if mask & (1 << cur):
-                add_cost = min(add_cost, board[cur][nxt])
+        # 방법 1: j에 직접 물체를 놓기
+        dp[next_mask] = min(dp[next_mask], dp[mask] + cost[j])
 
-        new_mask = mask | (1 << nxt)
-        dp[new_mask] = min(dp[new_mask], dp[mask] + add_cost)
+        # 방법 2: mask 안의 정점 i에서 j로 간선 연결
+        for i in range(n):
+            if (mask >> i) & 1:  # i에 물체가 있을 때
+                dp[next_mask] = min(dp[next_mask], dp[mask] + board[i][j])
 
-answer = INF
-for mask in range(size):
-    if mask.bit_count() >= k:
-        answer = min(answer, dp[mask])
+# k개 이상인 mask 중 최솟값
+ans = INF
+for mask in range(1 << n):
+    if bin(mask).count('1') >= k:
+        ans = min(ans, dp[mask])
 
-print(answer)
+print(ans)
